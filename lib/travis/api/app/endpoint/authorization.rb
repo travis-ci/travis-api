01e19e2888110af9c24c9b1d6e3bea2fbba1f096
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-09-18 22:21:38 +0200

    use id rather than login for user

--- lib/travis/api/app/endpoint/authorization.rb
+++ lib/travis/api/app/endpoint/authorization.rb
@@ -142,7 +142,7 @@ class Travis::Api::App
         def user_for_github_token(token)
           data   = GH.with(token: token.to_s) { GH['user'] }
           scopes = parse_scopes data.headers['x-oauth-scopes']
-          user   = User.find_by_login(data['login'])
+          user   = User.find_by_github_id(data['id'])
 
           halt 403, 'not a Travis user'   if user.nil?
           halt 403, 'insufficient access' unless acceptable? scopes
