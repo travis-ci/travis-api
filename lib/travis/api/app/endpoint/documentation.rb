e9474652a87b281df164fd6f288b2a4b79291989
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-09-20 12:03:08 +0200

    add github ribbon to docs

--- lib/travis/api/app/endpoint/documentation.rb
+++ lib/travis/api/app/endpoint/documentation.rb
@@ -95,6 +95,13 @@ __END__
   </head>
 
   <body onload="prettyPrint()">
+
+    <a href="https://github.com/travis-ci/travis-api">
+      <img style="position: absolute; top: 0; right: 0; border: 0;"
+        src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
+        alt="Fork me on GitHub">
+    </a>
+
     <div class="container">
       <div class="row">
         <header class="span12">
