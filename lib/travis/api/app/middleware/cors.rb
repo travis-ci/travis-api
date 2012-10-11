f4d467c99892ad713b311595d5807236b37e40fa
Sven Fuchs <me@svenfuchs.com>
2012-10-11 13:27:58 +0200

    whitelist Cache-Control, Expires, Last-Modified for cors

--- lib/travis/api/app/middleware/cors.rb
+++ lib/travis/api/app/middleware/cors.rb
@@ -10,12 +10,12 @@ class Travis::Api::App
       before do
         headers['Access-Control-Allow-Origin']      = "*"
         headers['Access-Control-Allow-Credentials'] = "true"
-        headers['Access-Control-Expose-Headers']    = "Content-Type"
+        headers['Access-Control-Expose-Headers']    = "Content-Type, Cache-Control, Expires, Etag, Last-Modified"
       end
 
       options // do
         headers['Access-Control-Allow-Methods'] = "HEAD, GET, POST, PATCH, PUT, DELETE"
-        headers['Access-Control-Allow-Headers'] = "Content-Type, Authorization, Accept"
+        headers['Access-Control-Allow-Headers'] = "Content-Type, Authorization, Accept, If-None-Match, If-Modified-Since"
       end
     end
   end
