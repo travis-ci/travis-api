440dacece3360402fb643cb90e6def8bf5a8c7fa
Piotr Sarnacki <drogus@gmail.com>
2012-12-03 02:34:15 +0100

    Add endpoints for key regeneration
    Enpoints are:
    
        POST /repos/:id/key
    
    and
    
        POST /repos/:owner/:name/key
    
    Both versions will replace current ssl key for given repo with the new
    one.

--- spec/integration/v2/repositories_spec.rb
+++ spec/integration/v2/repositories_spec.rb
@@ -6,6 +6,38 @@ describe 'Repos' do
   let(:repo)    { Repository.by_slug('svenfuchs/minimal').first }
   let(:headers) { { 'HTTP_ACCEPT' => 'application/vnd.travis-ci.2+json' } }
 
+  describe 'with authenticated user' do
+    let(:user)    { User.where(login: 'svenfuchs').first }
+    let(:token)   { Travis::Api::App::AccessToken.create(user: user, app_id: -1) }
+    let(:headers) { { 'HTTP_ACCEPT' => 'application/vnd.travis-ci.2+json', 'HTTP_AUTHORIZATION' => "token #{token}" } }
+
+    before { user.permissions.create!(:repository_id => repo.id, :push => true) }
+
+    it 'POST /repos/:id/key' do
+      expect {
+        response = post "/repos/#{repo.id}/key", {}, headers
+      }.to change { repo.reload.key.private_key }
+    end
+
+    it 'POST /repos/:owner/:name/key' do
+      expect {
+        response = post "/repos/#{repo.slug}/key", {}, headers
+      }.to change { repo.reload.key.private_key }
+    end
+  end
+
+  describe 'without authenticated user' do
+    it 'POST /repos/:id/key' do
+      response = post "/repos/#{repo.id}/key", {}, headers
+      response.should be_not_found
+    end
+
+    it 'POST /repos/:owner/:name/key' do
+      response = post "/repos/#{repo.id}/key", {}, headers
+      response.should be_not_found
+    end
+  end
+
   it 'GET /repos/:id/key' do
     response = get "/repos/#{repo.id}/key", {}, headers
     response.should deliver_json_for(repo.key, version: 'v2')
