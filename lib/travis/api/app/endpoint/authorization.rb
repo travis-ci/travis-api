7b4fe5dab3ac4566d3c5a37f4cabea32b462613b
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-12-06 15:52:49 +0100

    no inspect

--- lib/travis/api/app/endpoint/authorization.rb
+++ lib/travis/api/app/endpoint/authorization.rb
@@ -432,7 +432,7 @@ if(window.parent == window) {
 @@ post_payload
 <body onload='document.forms[0].submit()'>
   <form action="<%= uri %>" method='post'>
-    <input type='hidden' name='token'   value='<%= token.inspect %>'>
+    <input type='hidden' name='token'   value='<%= token %>'>
     <input type='hidden' name='user'    value='<%= user.to_json %>'>
     <input type='hidden' name='storage' value='sessionStorage'>
   </form>
