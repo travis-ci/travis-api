88363423f44e793e5263a66ace67fe60aff291c9
Piotr Sarnacki <drogus@gmail.com>
2012-12-09 23:46:12 +0100

    Fix whitelist auth check for localhost

--- lib/travis/api/app/endpoint/authorization.rb
+++ lib/travis/api/app/endpoint/authorization.rb
@@ -256,7 +256,7 @@ class Travis::Api::App
           return unless uri = Addressable::URI.parse(target_origin)
           if uri.host =~ /\A(.+\.)?travis-ci\.(com|org)\Z/
             uri.scheme == 'https'
-          elsif uri == 'localhost' or uri == '127.0.0.1'
+          elsif uri.host == 'localhost' or uri.host == '127.0.0.1'
             uri.port > 1023
           end
         end
