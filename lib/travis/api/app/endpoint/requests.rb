92a3d17ebf098a1bab0671517d8735be26993d74
Sven Fuchs <me@svenfuchs.com>
2013-01-17 00:30:51 +0100

    use travis-core/sf-restart-event

--- lib/travis/api/app/endpoint/requests.rb
+++ lib/travis/api/app/endpoint/requests.rb
@@ -4,7 +4,7 @@ class Travis::Api::App
   class Endpoint
     class Requests < Endpoint
       post '/' do
-        respond_with service(:requeue_request, params)
+        respond_with service(:reset_model, params)
       end
     end
   end
