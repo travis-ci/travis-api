ee11e571b7547c4cb54607ec314d7287bfdf6980
Sven Fuchs <me@svenfuchs.com>
2013-01-22 16:18:43 +0100

    pass the api payload type for workers because we now do not return AR scopes any more

--- lib/travis/api/app/endpoint/workers.rb
+++ lib/travis/api/app/endpoint/workers.rb
@@ -4,11 +4,11 @@ class Travis::Api::App
   class Endpoint
     class Workers < Endpoint
       get '/' do
-        respond_with service(:find_workers, params)
+        respond_with service(:find_workers, params), type: :workers
       end
 
       get '/:id' do
-        respond_with service(:find_worker, params)
+        respond_with service(:find_worker, params), type: :worker
       end
     end
   end
