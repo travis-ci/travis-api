b81644acba2e8542d64887e66b8a9fdddaaed9c1
Sven Fuchs <me@svenfuchs.com>
2013-01-26 20:41:18 +0100

    comment out put to /artifacts/:id for now

--- lib/travis/api/app/endpoint/artifacts.rb
+++ lib/travis/api/app/endpoint/artifacts.rb
@@ -10,9 +10,11 @@ class Travis::Api::App
         respond_with service(:find_artifact, params)
       end
 
-      put '/:id' do |id|
-        respond_with service(:update_artifact, params)
-      end
+      # TODO needs auth, required for live log archiving on log:aggregted in travis-tasks
+      #
+      # put '/:id' do |id|
+      #   respond_with service(:update_artifact, params)
+      # end
     end
   end
 end
