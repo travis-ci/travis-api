c3249a498cb8a714a1fa0ac9091add294db88ce0
Mathias Meyer <meyer@paperplanes.de>
2013-05-09 13:44:08 +0200

    Fix Metriks reference.

--- lib/travis/api/app/middleware/metriks.rb
+++ lib/travis/api/app/middleware/metriks.rb
@@ -5,6 +5,7 @@ class Travis::Api::App
     class Metriks < Middleware
       before do
         env['metriks.request.start'] = Time.now.utc
+        ::Metriks.meter("api.requests").mark
       end
 
       after do
@@ -12,10 +13,10 @@ class Travis::Api::App
           time = Time.now.utc - env['metriks.request.start']
           pattern = headers['X-Pattern'].gsub(/[:\/]/, ".")
           metric = "api.request.endpoint.#{pattern}"
-          Metriks.timer(metric).update(time)
-          Metriks.meter("api.request.#{request.method}")
+          ::Metriks.timer(metric).update(time)
+          ::Metriks.meter("api.request.#{request.method}").mark
         end
-        Metriks.meter("api.request.status.#{response.status.to_s[0]}")
+        ::Metriks.meter("api.request.status.#{response.status.to_s[0]}").mark
       end
     end
   end
