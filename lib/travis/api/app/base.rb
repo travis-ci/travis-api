94922cd62504b8c1f6e7d07ef32f6354a00dcc5b
Piotr Sarnacki <drogus@gmail.com>
2013-09-02 12:29:13 +0200

    Start new relic manually

--- lib/travis/api/app/base.rb
+++ lib/travis/api/app/base.rb
@@ -9,6 +9,8 @@ class Travis::Api::App
 
     configure :production do
       require 'newrelic_rpm'
+      ::NewRelic::Agent.manual_start()
+      ::NewRelic::Agent.after_fork(:force_reconnect => true)
     end
 
     error NotImplementedError do
