783d197e7e865ed900f8caa5edc9d790b62652a5
Piotr Sarnacki <drogus@gmail.com>
2013-09-10 11:56:59 +0200

    Tune GC for unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,3 +9,11 @@ require 'fileutils'
 before_fork do |server,worker|
   FileUtils.touch('/tmp/app-initialized')
 end
+
+before_exec do |server|
+  ENV['RUBY_HEAP_MIN_SLOTS']=800000
+  ENV['RUBY_GC_MALLOC_LIMIT']=59000000
+  ENV['RUBY_HEAP_SLOTS_INCREMENT']=10000
+  ENV['RUBY_HEAP_SLOTS_GROWTH_FACTOR']=1
+  ENV['RUBY_HEAP_FREE_MIN']=100000
+end
