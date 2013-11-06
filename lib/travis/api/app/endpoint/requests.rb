4deddc5843bbc9deb515a593b401f46b7c2acccf
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-11-06 01:22:54 +0100

    add deprecation notice

--- lib/travis/api/app/endpoint/requests.rb
+++ lib/travis/api/app/endpoint/requests.rb
@@ -3,6 +3,7 @@ require 'travis/api/app'
 class Travis::Api::App
   class Endpoint
     class Requests < Endpoint
+      # DEPRECATED: this will be removed by 1st of December
       post '/' do
         Metriks.meter("api.request.restart").mark
         respond_with service(:reset_model, params)
