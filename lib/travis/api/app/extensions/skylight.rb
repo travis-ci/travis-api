749ee8b943c6c0d74013d25fc8217d27cc17d2e0
Henrik Hodne <henrik@hodne.io>
2014-04-09 04:14:19 -0500

    skylight: only load extension if skylight is available

--- lib/travis/api/app/extensions/skylight.rb
+++ lib/travis/api/app/extensions/skylight.rb
@@ -1,14 +1,15 @@
 require 'travis/api/app'
-require 'skylight'
 
 class Travis::Api::App
   module Extensions
     module Skylight
       def route(verb, path, *)
         condition do
-          trace = ::Skylight::Instrumenter.instance.current_trace
-          endpoint = settings.name.to_s.split("::", 5).last.gsub(/::/, "/").downcase
-          trace.endpoint = "#{verb} /#{endpoint}#{path}"
+          if ENV['SKYLIGHT_APPLICATION']
+            trace = ::Skylight::Instrumenter.instance.current_trace
+            endpoint = settings.name.to_s.split("::", 5).last.gsub(/::/, "/").downcase
+            trace.endpoint = "#{verb} /#{endpoint}#{path}"
+          end
         end
 
         super
