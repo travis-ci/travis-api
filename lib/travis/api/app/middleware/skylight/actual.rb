fb74fcc711be0f23b8191258f258b75a2c73c2f1
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2015-01-14 14:28:07 +0100

    debug that

--- lib/travis/api/app/middleware/skylight/actual.rb
+++ lib/travis/api/app/middleware/skylight/actual.rb
@@ -12,7 +12,7 @@ class Travis::Api::App
       after do
         instrumenter   = ::Skylight::Instrumenter.instance
         trace          = instrumenter.current_trace if instrumenter
-        trace.endpoint = "#{request.request_method} #{endpoint}" || "unknown" if trace
+        p trace.endpoint = "#{request.request_method} #{endpoint}" || "unknown" if trace
       end
 
       def endpoint
