d7ee8b4d3bb3f562e9d1c162c8cbda950c1b5d5d
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-01-23 14:49:47 +0100

    enable Async and configure the Sidekiq client

--- lib/travis/sidekiq.rb
+++ lib/travis/sidekiq.rb
@@ -5,10 +5,15 @@ require 'travis/api/workers/build_cancellation'
 require 'travis/support/amqp'
 
 Travis::Database.connect
+Travis::Async.enabled = true
 Travis::Amqp.config = Travis.config.amqp
 Travis::Metrics.setup
 Travis::Notification.setup
 
 Sidekiq.configure_server do |config|
   config.redis = Travis.config.redis.merge(namespace: Travis.config.sidekiq.namespace)
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = Travis.config.redis.merge(size: 1, namespace: Travis.config.sidekiq.namespace)
 end
