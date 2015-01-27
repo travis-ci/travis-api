517a3b04f79f808afad3c03ba6849ca94adc807a
carlad <carla@travis-ci.org>
2015-01-27 16:22:30 +0100

    Revert "Revert "Merge branch 'master' of github.com:travis-ci/travis-api""
    This reverts commit 171b9c57c79b74e46a285e102e47a69a49719d77.

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: bundle exec ./script/server
 console: bundle exec ./script/console
+sidekiq: bundle exec sidekiq -c 5 -r ./lib/travis/sidekiq.rb -q build_cancellations, -q build_restarts, -q job_cancellations, -q job_restarts
---
+++ lib/travis/sidekiq.rb
@@ -0,0 +1,22 @@
+#$: << './lib'
+require 'sidekiq'
+require 'travis'
+require 'travis/api/workers/build_cancellation'
+require 'travis/api/workers/build_restart'
+require 'travis/api/workers/job_cancellation'
+require 'travis/api/workers/job_restart'
+require 'travis/support/amqp'
+
+Travis::Database.connect
+Travis::Async.enabled = true
+Travis::Amqp.config = Travis.config.amqp
+Travis::Metrics.setup
+Travis::Notification.setup
+
+Sidekiq.configure_server do |config|
+  config.redis = Travis.config.redis.merge(namespace: Travis.config.sidekiq.namespace)
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = Travis.config.redis.merge(size: 1, namespace: Travis.config.sidekiq.namespace)
+end
