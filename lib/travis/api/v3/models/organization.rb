3680fcf3894feb74c860acff00fccd02ff48201a
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2015-04-17 12:16:11 +0200

    v3: add subscription model

--- lib/travis/api/v3/models/organization.rb
+++ lib/travis/api/v3/models/organization.rb
@@ -3,5 +3,10 @@ module Travis::API::V3
     has_many :memberships
     has_many :users, through: :memberships
     has_many :repositories, as: :owner
+    has_one  :subscription, as: :owner
+
+    def subscription
+      super if Features.use_subscriptions?
+    end
   end
 end
