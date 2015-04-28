bbba06a85fb059fc6deb40f96127d17d27014222
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2015-04-28 12:43:54 +0200

    API v3: fix owner query

--- lib/travis/api/v3/services/owner/repositories.rb
+++ lib/travis/api/v3/services/owner/repositories.rb
@@ -3,7 +3,7 @@ module Travis::API::V3
     result_type :repositories
 
     def run!
-      unfiltered = query(:repositories).for_owner(find)
+      unfiltered = query(:repositories).for_owner(find(:owner))
       access_control.visible_repositories(unfiltered)
     end
   end
