fb8e6825db79ba6dc204366d4febcb29909e8952
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2015-04-30 16:25:14 +0200

    v3: expose build.previous_state

--- spec/v3/services/branch/find_spec.rb
+++ spec/v3/services/branch/find_spec.rb
@@ -8,23 +8,24 @@ describe Travis::API::V3::Services::Repository::Find do
     before     { get("/v3/repo/#{repo.id}/branch/master") }
     example    { expect(last_response).to be_ok           }
     example    { expect(JSON.load(body)).to be ==         {
-      "@type"         => "branch",
-      "@href"         => "/v3/repo/#{repo.id}/branch/master",
-      "name"          => "master",
-      "repository"    => {
-        "@type"       => "repository",
-        "@href"       => "/v3/repo/#{repo.id}",
-        "id"          => repo.id,
-        "slug"        => "svenfuchs/minimal"},
-      "last_build"    => {
-        "@type"       => "build",
-        "@href"       => "/v3/build/#{repo.last_build.id}",
-        "id"          => repo.last_build.id,
-        "number"      => "3",
-        "state"       => "configured",
-        "duration"    => nil,
-        "event_type"  => "push",
-        "started_at"  => "2010-11-12T13:00:00Z",
-        "finished_at" => nil}}}
+      "@type"            => "branch",
+      "@href"            => "/v3/repo/#{repo.id}/branch/master",
+      "name"             => "master",
+      "repository"       => {
+        "@type"          => "repository",
+        "@href"          => "/v3/repo/#{repo.id}",
+        "id"             => repo.id,
+        "slug"           => "svenfuchs/minimal"},
+      "last_build"       => {
+        "@type"          => "build",
+        "@href"          => "/v3/build/#{repo.last_build.id}",
+        "id"             => repo.last_build.id,
+        "number"         => "3",
+        "state"          => "configured",
+        "duration"       => nil,
+        "event_type"     => "push",
+        "previous_state" => "passed",
+        "started_at"     => "2010-11-12T13:00:00Z",
+        "finished_at"    => nil}}}
   end
 end
