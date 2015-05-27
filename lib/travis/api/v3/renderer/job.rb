2455cf3c4225b27df07aa998388378d96cae1261
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2015-05-27 15:17:26 +0200

    add job renderer

---
+++ lib/travis/api/v3/renderer/job.rb
@@ -0,0 +1,15 @@
+require 'travis/api/v3/renderer/model_renderer'
+
+module Travis::API::V3
+  class Renderer::Build < Renderer::ModelRenderer
+    representation(:minimal,  :id, :number, :state, :duration, :started_at, :finished_at, :allow_failure, :queue)
+    representation(:standard, *representations[:minimal], :repository, :build, :commit)
+
+    def queue
+      {
+        :@type => 'queue'.freeze,
+        :name  => model.queue
+      }
+    end
+  end
+end
