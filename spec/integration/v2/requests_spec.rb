66f5d71d1a5ed5cbdee6be48a36fbe4e64d06ea9
Piotr Sarnacki <drogus@gmail.com>
2015-06-15 12:20:04 +0200

    Remove triggering builds in V2

--- spec/integration/v2/requests_spec.rb
+++ spec/integration/v2/requests_spec.rb
@@ -26,31 +26,4 @@ describe 'Requests' do
       response.should deliver_json_for(request, version: 'v2', type: 'request')
     end
   end
-
-  describe 'POST /requests' do
-    let(:payload) { { request: { repository: { owner_name: repo.owner_name, name: repo.name } } } }
-
-    before do
-      Travis::Features.stubs(:owner_active?).returns(true)
-    end
-
-    it 'schedules a request' do
-      response = post '/requests', payload, headers
-      expect(response.status).to eq 200
-    end
-
-    it 'requires activation' do
-      Travis::Features.stubs(:owner_active?).returns(false)
-      response = post '/requests', payload, headers
-      json = JSON.parse(response.body)
-      expect(json['result']).to eq 'not_active'
-    end
-
-    it 'throttles requests' do
-      Travis::Api::App::Services::ScheduleRequest::Throttle.any_instance.stubs(:throttled?).returns(true)
-      response = post '/requests', payload, headers
-      json = JSON.parse(response.body)
-      expect(json['result']).to eq 'throttled'
-    end
-  end
 end
