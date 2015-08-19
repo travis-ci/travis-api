18a90c2561409b2d7a5f25713fa98d87b50540a5
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2015-08-19 13:04:54 +0200

    v3: if the API is not marked as public (travis-ci.com), then do not allow read access to organizations the user is not a member of (note: private repos were never included)

--- spec/v3/services/organization/find_spec.rb
+++ spec/v3/services/organization/find_spec.rb
@@ -19,4 +19,17 @@ describe Travis::API::V3::Services::Organization::Find do
       "avatar_url"   => nil
     }}
   end
+
+  describe 'existing org, private api' do
+    before  { Travis.config.private_api = true      }
+    before  { get("/v3/org/#{org.id}")              }
+    after   { Travis.config.private_api = false     }
+    example { expect(last_response).to be_not_found }
+    example { expect(JSON.load(body)).to be ==      {
+      "@type"         => "error",
+      "error_type"    => "not_found",
+      "error_message" =>"organization not found (or insufficient access)",
+      "resource_type" => "organization"
+    }}
+  end
 end
