462b2aed0da78aca530474b436129a1980d1407f
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2015-08-21 19:11:27 +0200

    v3: allow filtering builds by branch

--- lib/travis/api/v3/queries/builds.rb
+++ lib/travis/api/v3/queries/builds.rb
@@ -1,6 +1,7 @@
 module Travis::API::V3
   class Queries::Builds < Query
     params :state, :event_type, :previous_state, prefix: :build
+    params :name, prefix: :branch, method_name: :branch_name
 
     def find(repository)
       filter(repository.builds)
@@ -9,7 +10,8 @@ module Travis::API::V3
     def filter(list)
       list = list.where(state:          list(state))          if state
       list = list.where(previous_state: list(previous_state)) if previous_state
-      list = list.where(event_type:     list(state))          if event_type
+      list = list.where(event_type:     list(event_type))     if event_type
+      list = list.where(branch:         list(branch_name))    if branch_name
 
       list = list.includes(:commit).includes(branch: :last_build).includes(:repository)
       list = list.includes(branch: { last_build: :commit }) if includes? 'build.commit'.freeze
--- lib/travis/api/v3/services/builds/find.rb
+++ lib/travis/api/v3/services/builds/find.rb
@@ -1,6 +1,7 @@
 module Travis::API::V3
   class Services::Builds::Find < Service
     params :state, :event_type, :previous_state, prefix: :build
+    params "branch.name"
     paginate
 
     def run!
