cbc5329b3d1439f61b5fd561b70a3edfcc8094d5
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2015-08-21 18:26:21 +0200

    v3: avoid N+1 query in builds endpoint

--- lib/travis/api/v3/queries/builds.rb
+++ lib/travis/api/v3/queries/builds.rb
@@ -1,7 +1,12 @@
 module Travis::API::V3
   class Queries::Builds < Query
     def find(repository)
-      repository.builds
+      filter(repository.builds)
+    end
+
+    def filter(list)
+      # filtering by branch, type, etc would go here
+      list.includes(:commit).includes(branch: :last_build)
     end
   end
 end
