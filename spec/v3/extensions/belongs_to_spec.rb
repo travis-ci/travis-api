070be827df841fa8b0bacb9abf4df7bb682233ea
carlad <carla@travis-ci.com>
2015-09-02 16:29:40 +0200

    revert extensions spec expectation

--- spec/v3/extensions/belongs_to_spec.rb
+++ spec/v3/extensions/belongs_to_spec.rb
@@ -12,7 +12,7 @@ describe Travis::API::V3::Extensions::BelongsTo do
     after      { repo.destroy; user.destroy }
 
     example { expect(repo.owner).to be_a(Travis::API::V3::Models::User) }
-    example { expect(Travis::API::V3::Models::Repository.find(repo.id).owner).to be_a(Travis::API::V3::Models::User)  }
+    example { expect(::Repository.find(repo.id).owner).to be_a(::User)  }
     example { expect(user.repositories).to include(repo)                }
   end
 end
