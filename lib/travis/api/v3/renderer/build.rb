2891dd640a27c572ce7c5aecdd23a36a1143107c
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2015-09-03 15:03:12 +0200

    v3: use cached_matrix_ids for build.jobs if only job ids are being loaded

--- lib/travis/api/v3/renderer/build.rb
+++ lib/travis/api/v3/renderer/build.rb
@@ -3,6 +3,26 @@ require 'travis/api/v3/renderer/model_renderer'
 module Travis::API::V3
   class Renderer::Build < Renderer::ModelRenderer
     representation(:minimal,  :id, :number, :state, :duration, :event_type, :previous_state, :started_at, :finished_at, :jobs)
-    representation(:standard, *representations[:minimal], :repository, :branch, :commit )
+    representation(:standard, *representations[:minimal], :repository, :branch, :commit)
+
+    def jobs
+      return model.jobs if include_full_jobs?
+      model.job_ids.map { |id| job(id) }
+    end
+
+    private def include_full_jobs?
+      return true if include? 'build.job'.freeze
+      return true if include.any?  { |i| i.start_with? 'job.'.freeze }
+      return true if included.any? { |i| i.is_a? Models::Job and i.source_id == model.id }
+    end
+
+    private def job(id)
+      {
+        "@type"           => "job",
+        :@href            => Renderer.href(:job, script_name: script_name, id: id),
+        "@representation" => "minimal",
+        "id"              => id
+      }
+    end
   end
 end
