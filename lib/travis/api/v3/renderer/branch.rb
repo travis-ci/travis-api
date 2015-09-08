2c5ad649f5f56070f64a18a3ee3e36484ee4532d
carlad <carla@travis-ci.com>
2015-09-08 16:26:43 +0200

    v3 add :exists_on_github to standard branch representation

--- lib/travis/api/v3/renderer/branch.rb
+++ lib/travis/api/v3/renderer/branch.rb
@@ -3,6 +3,6 @@ require 'travis/api/v3/renderer/model_renderer'
 module Travis::API::V3
   class Renderer::Branch < Renderer::ModelRenderer
     representation(:minimal,  :name, :last_build)
-    representation(:standard, :name, :repository, :last_build)
+    representation(:standard, :name, :repository, :last_build, :exists_on_github)
   end
 end
