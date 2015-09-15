4a0932df1eaba7bb74564ca0986397b72afcf9dc
carlad <carla@travis-ci.com>
2015-09-15 11:45:16 +0200

    prefix ids with correct tables when ordering

--- lib/travis/api/v3/models/branch.rb
+++ lib/travis/api/v3/models/branch.rb
@@ -2,7 +2,7 @@ module Travis::API::V3
   class Models::Branch < Model
     belongs_to :repository
     belongs_to :last_build, class_name: 'Travis::API::V3::Models::Build'.freeze
-    has_many   :builds,  foreign_key: [:repository_id, :branch], primary_key: [:repository_id, :name], order: 'id DESC'.freeze, conditions: { event_type: 'push' }
-    has_many   :commits, foreign_key: [:repository_id, :branch], primary_key: [:repository_id, :name], order: 'id DESC'.freeze
+    has_many   :builds,  foreign_key: [:repository_id, :branch], primary_key: [:repository_id, :name], order: 'builds.id DESC'.freeze, conditions: { event_type: 'push' }
+    has_many   :commits, foreign_key: [:repository_id, :branch], primary_key: [:repository_id, :name], order: 'commits.id DESC'.freeze
   end
 end
