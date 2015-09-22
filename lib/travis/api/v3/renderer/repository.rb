af7fdec34027733c37de11ac7167f5c30f220c27
carlad <carla@travis-ci.com>
2015-09-22 16:16:01 +0200

    v3 remove :last_build attrubite from repository renderer

--- lib/travis/api/v3/renderer/repository.rb
+++ lib/travis/api/v3/renderer/repository.rb
@@ -3,7 +3,7 @@ require 'travis/api/v3/renderer/model_renderer'
 module Travis::API::V3
   class Renderer::Repository < Renderer::ModelRenderer
     representation(:minimal,  :id, :slug, :default_branch)
-    representation(:standard, :id, :name, :slug, :description, :github_language, :active, :private, :owner, :last_build, :default_branch)
+    representation(:standard, :id, :name, :slug, :description, :github_language, :active, :private, :owner, :default_branch)
 
     def active
       !!model.active
