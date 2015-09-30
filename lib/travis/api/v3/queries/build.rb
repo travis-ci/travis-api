a2deeb1d96f6f48c983428c5e0ef518b83263c18
carlad <carla@travis-ci.com>
2015-09-30 15:49:48 +0200

    v3 update payload

--- lib/travis/api/v3/queries/build.rb
+++ lib/travis/api/v3/queries/build.rb
@@ -7,6 +7,7 @@ module Travis::API::V3
       raise WrongParams, 'missing build.id'.freeze
     end
 
+    # TODO this must match restart method below
     def cancel(user)
       payload = {id: id, user_id: user.id, source: 'api'}
       perform_async(:build_cancellation, type: 'api'.freeze, payload: JSON.dump(payload))
@@ -14,8 +15,8 @@ module Travis::API::V3
     end
 
     def restart(user)
-      payload = {id: id, user_id: user.id, source: 'api'}
-      perform_async(:build_restart, type: 'api'.freeze, payload: payload)
+      payload = { id: id, user_id: user.id, source: 'api' }
+      perform_async(:build_restart, payload: payload)
       payload
     end
   end
