e03f79513a0e3bfc9a6071fa2e9056e5b890b43f
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2015-10-08 13:57:49 +0200

    reinstate nginx, use it in development, add smare concurrency settings

--- config/puma-config.rb
+++ config/puma-config.rb
@@ -2,8 +2,7 @@ root = File.expand_path('../..', __FILE__)
 
 rackup "#{root}/config.ru"
 
-bind 'unix:///tmp/nginx.socket'
-
+bind "unix://#{ENV["tmp_dir"]}/nginx.socket"
 environment ENV['RACK_ENV'] || 'development'
 
 threads 0, 16
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,19 +1,15 @@
 # http://michaelvanrooijen.com/articles/2011/06/01-more-concurrency-on-a-single-heroku-dyno-with-the-new-celadon-cedar-stack/
 
-worker_processes 4 # amount of unicorn workers to spin up
-timeout 30         # restarts workers that hang for 15 seconds
+worker_processes Integer(ENV.fetch('WEB_CONCURRENCY')) # amount of unicorn workers to spin up
+timeout 30 # restarts workers that hang for 30 seconds
 
-listen '/tmp/nginx.socket', backlog: 1024
+listen File.expand_path("nginx.socket", ENV["tmp_dir"]), backlog: 1024
 
 require 'fileutils'
-before_fork do |server,worker|
-  FileUtils.touch('/tmp/app-initialized')
-end
+before_fork do |server, worker|
+  # preload travis so we can have copy on write
+  require 'travis/api/app'
 
-before_exec do |server|
-  ENV['RUBY_HEAP_MIN_SLOTS']=800000
-  ENV['RUBY_GC_MALLOC_LIMIT']=59000000
-  ENV['RUBY_HEAP_SLOTS_INCREMENT']=10000
-  ENV['RUBY_HEAP_SLOTS_GROWTH_FACTOR']=1
-  ENV['RUBY_HEAP_FREE_MIN']=100000
+  # signal to nginx we're ready
+  FileUtils.touch("#{ENV["tmp_dir"]}/app-initialized")
 end
