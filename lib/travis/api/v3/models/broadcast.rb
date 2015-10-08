0eb564388c42c4ae785166a6154891f54b0f427a
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2015-10-08 18:43:22 +0200

    v3: broadcasts - filter inactive broadcasts

--- lib/travis/api/v3/models/broadcast.rb
+++ lib/travis/api/v3/models/broadcast.rb
@@ -1,5 +1,14 @@
 module Travis::API::V3
   class Models::Broadcast < Model
+    EXPIRY_TIME = 14.days
+
     belongs_to :recipient, polymorphic: true
+    scope :active,   -> { where('created_at >= ? AND (expired IS NULL OR expired <> ?)', EXPIRY_TIME.ago, true) }
+    scope :inactive, -> { where('created_at < ? OR (expired = ?)', EXPIRY_TIME.ago, true) }
+
+    def active?
+      return false if expired?
+      created_at >= EXPIRY_TIME.ago
+    end
   end
 end
