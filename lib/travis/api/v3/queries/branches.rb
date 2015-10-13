949a8765508c5842d94bf2c04f270a547b2f9735
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2015-10-13 13:08:27 +0200

    v3: allow sorting branches by them being default branch or not

--- lib/travis/api/v3/queries/branches.rb
+++ lib/travis/api/v3/queries/branches.rb
@@ -3,13 +3,26 @@ module Travis::API::V3
     params :exists_on_github, prefix: :branch
 
     sortable_by :name,
-      last_build: "builds.started_at".freeze,
-      exists_on_github: "(case when branches.exists_on_github then 1 else 2 end)".freeze
+      last_build:       "builds.started_at".freeze,
+      exists_on_github: sort_condition(:exists_on_github),
+      default_branch:   sort_condition(name: "repositories.default_branch")
 
     default_sort "last_build:desc"
 
     def find(repository)
-      sort filter(repository.branches)
+      sort(filter(repository.branches), repository: repository)
+    end
+
+    def sort_by(collection, field, repository: nil, **options)
+      return super unless field == "default_branch".freeze
+
+      if repository
+        options[:sql] = sort_condition(name: quote(repository.default_branch_name))
+      else
+        collection    = collection.joins(:repository)
+      end
+
+      super(collection, field, **options)
     end
 
     def filter(list)
--- spec/v3/services/branches/find_spec.rb
+++ spec/v3/services/branches/find_spec.rb
@@ -252,6 +252,28 @@ describe Travis::API::V3::Services::Branches::Find do
     }
   end
 
+  describe "sorting by default_branch" do
+    before  { get("/v3/repo/#{repo.id}/branches?sort_by=default_branch&limit=1") }
+    example { expect(last_response).to be_ok }
+    example { expect(parsed_body["@pagination"]).to be == {
+        "limit"            => 1,
+        "offset"           => 0,
+        "count"            => 1,
+        "is_first"         => true,
+        "is_last"          => true,
+        "next"             => nil,
+        "prev"             => nil,
+        "first"            => {
+          "@href"          => "/v3/repo/#{repo.id}/branches?sort_by=default_branch&limit=1",
+          "offset"         => 0,
+          "limit"          => 1 },
+        "last"             => {
+          "@href"          => "/v3/repo/#{repo.id}/branches?sort_by=default_branch&limit=1",
+          "offset"         => 0,
+          "limit"          => 1 }}
+    }
+  end
+
   describe "sorting by unknown sort field" do
     before  { get("/v3/repo/#{repo.id}/branches?sort_by=name:desc,foo&limit=1") }
     example { expect(last_response).to be_ok }
