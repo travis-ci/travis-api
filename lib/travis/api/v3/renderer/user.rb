083355227f4bd29811ca5955220fc7ec9124377d
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2015-10-21 16:49:54 +0200

    v3: add developer program feature flag

--- lib/travis/api/v3/renderer/user.rb
+++ lib/travis/api/v3/renderer/user.rb
@@ -3,5 +3,11 @@ require 'travis/api/v3/renderer/owner'
 module Travis::API::V3
   class Renderer::User < Renderer::Owner
     representation(:standard, :is_syncing, :synced_at)
+    representation(:additional, :developer_program)
+
+    def developer_program
+      return true if Travis::Features.owner_active?(:developer_program, @model)
+      @model.organizations.any? { |o| Travis::Features.owner_active?(:developer_program, o) }
+    end
   end
 end
