3d171394a67397d68855dd94f0adb261aa3d068e
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2015-10-22 09:13:55 +0200

    v3: list more resources in the service index

--- lib/travis/api/v3/service_index.rb
+++ lib/travis/api/v3/service_index.rb
@@ -39,7 +39,11 @@ module Travis::API::V3
         }
 
         all = routes.resources + [
-          Routes::Resource.new(:account), # dummy as there are only accounts routes right now
+          Routes::Resource.new(:account),   # dummy as there are only accounts routes right now
+          Routes::Resource.new(:broadcast), # dummy as there are only broadcasts routes right now
+          Routes::Resource.new(:commit),    # dummy as commits can only be embedded
+          Routes::Resource.new(:jobs),      # dummy as there are no direct jobs routes
+          Routes::Resource.new(:request),   # dummy as there are only requests routes right now
           Routes::Resource.new(:error),
           Routes::Resource.new(:home,     attributes: [:config, :errors, :resources], actions: home_actions),
           Routes::Resource.new(:resource, attributes: [:actions, :attributes, :representations, :access_rights]),
