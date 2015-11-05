c08f8243fb7fe755e684258af83d0845e31de404
Lennard Wolf <lennardwolf@live.de>
2015-11-05 16:08:08 +0100

    fix .../crons/create endpoint

--- lib/travis/api/v3/services/crons/create.rb
+++ lib/travis/api/v3/services/crons/create.rb
@@ -1,14 +1,13 @@
 module Travis::API::V3
   class Services::Crons::Create < Service
-
+    result_type :cron
 
     def run!
-      #raise LoginRequired unless access_control.logged_in? or access_control.full_access?
+      raise LoginRequired unless access_control.logged_in? or access_control.full_access?
       raise NotFound      unless repository = find(:repository)
-      #access_control.permissions(cron).create!
+      access_control.permissions(cron).create!
 
       Models::Cron.create(repository: repository)
-      query.find(find(:repository))
       end
 
   end
