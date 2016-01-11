68ba4b9fce6acbaf11fc6a060966e98d07033a41
Steffen Kötte <steffen.koette@gmail.com>
2016-01-11 14:54:02 +0100

    check for correct interval allow only one cronjob per branch

--- lib/travis/api/v3/services/cron/create.rb
+++ lib/travis/api/v3/services/cron/create.rb
@@ -7,7 +7,14 @@ module Travis::API::V3
       raise LoginRequired unless access_control.logged_in? or access_control.full_access?
       raise NotFound      unless repository = find(:repository)
       raise NotFound      unless branch = find(:branch, repository)
+      raise ArgumentError, 'interval must be "daily", "weekly" or "monthly"' unless ["daily", "weekly", "monthly"].include?(params["interval"])
       access_control.permissions(repository).create_cron!
+
+      if branch.cron
+        access_control.permissions(branch.cron).delete!
+        branch.cron.destroy
+      end
+
       Models::Cron.create(branch: branch,
                           interval: params["interval"],
                           disable_by_build: params["disable_by_build"] ? params["disable_by_build"] : false)
--- lib/travis/api/v3/services/crons/find.rb
+++
@@ -1,9 +0,0 @@
-module Travis::API::V3
-  class Services::Crons::Find < Service
-    paginate
-
-    def run!
-      query.find(find(:branch, find(:repository)))
-    end
-  end
-end
