2d00e1696270a641970cc673cc3646258199ef09
carlad <carla@travis-ci.com>
2016-01-11 14:43:23 +0100

    remove query and permission

--- lib/travis/api/v3/permissions/lint.rb
+++
@@ -1,9 +0,0 @@
-require 'travis/api/v3/permissions/generic'
-
-module Travis::API::V3
-  class Permissions::Lint < Permissions::Generic
-    def lint?
-      write?
-    end
-  end
-end
--- lib/travis/api/v3/services/lint/lint.rb
+++ lib/travis/api/v3/services/lint/lint.rb
@@ -1,7 +1,12 @@
 module Travis::API::V3
   class Services::Lint::Lint < Service
     def run!
-      lint
+      request.body.rewind
+      content  = params[:content] || request.body.read
+      parsed   = Travis::Yaml.parse(content)
+      warnings = parsed.nested_warnings.map { |k, m| { key: k, message: m } }
+      payload = { lint: { warnings: warnings } }.to_json
+      payload
     end
   end
 end
