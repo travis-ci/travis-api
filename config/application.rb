b7ba7813b92594c3fe766dafa903b44411c97f55
Aly Fulton <aly@travis-ci.com>
2016-01-29 14:33:11 -0500

    Remove ActiveRecord
    this makes it so Rails actually runs the application now!

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,13 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# don't know which ones I need yet but rails/all has activerecord
+# TODO: Figure out which of these we need
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'action_mailer/railtie'
+require 'active_job/railtie'
+require 'rails/test_unit/railtie'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,6 +28,6 @@ module TravisAdmin
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
   end
 end
