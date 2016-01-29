a551499b1f73cce88afaf0972b8eb04dc8638ebb
Aly Fulton <aly@travis-ci.com>
2016-01-29 15:27:57 -0500

    Create example ActiveResource Models
    Nothing good in there yet though.

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < Travis::Base
+
+  # I don't think this has to match the way API has it exactly but putting this here for now.
+  # TODO: Trim this down if necessary.
+  has_many :memberships,   dependent: :destroy
+  has_many :permissions,   dependent: :destroy
+  has_many :emails,        dependent: :destroy
+  has_many :tokens,        dependent: :destroy
+  has_many :organizations, through:   :memberships
+  has_many :repositories,  as:        :owner
+  has_one  :subscription,  as:        :owner
+end
