16864ef2af4c57cec5ae5112d8bcaabd9142739a
Aly Fulton <aly@travis-ci.com>
2016-02-11 14:39:43 -0500

    Add User spec
    Just getting started. Added a find by ID spec.

---
+++ test/models/user_spec.rb
@@ -0,0 +1,10 @@
+describe User, '.find' do
+  context 'there is a user with id 125283' do
+    it 'finds user 125283' do
+      ActiveResource::HttpMock.respond_to do |mock|
+        mock.get '/user/125283', {'Accept' => 'application/json'}
+      end
+    end
+    User.find(125283).should == 'sinthetix'
+  end
+end
