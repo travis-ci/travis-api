b055dbcded4e6e9c8e0e856719bc70ecf2c4844e
Steffen Kötte <steffen.koette@gmail.com>
2016-02-25 15:09:36 +0100

    exclude canceled builds from build duration overview

--- spec/v3/services/overview/get_build_duration_spec.rb
+++ spec/v3/services/overview/get_build_duration_spec.rb
@@ -23,10 +23,11 @@ describe Travis::API::V3::Services::Overview::GetBuildDuration do
     builds = []
     before     {
       Travis::API::V3::Models::Build.where(repository_id: repo.id).each do |build| build.destroy end
-      builds.push Travis::API::V3::Models::Build.create(repository_id: repo.id, created_at: DateTime.now - 5, duration: 600,  number: 1, state: 'passed', branch_name: repo.default_branch.name)
-      builds.push Travis::API::V3::Models::Build.create(repository_id: repo.id, created_at: DateTime.now - 4, duration: 1200, number: 2, state: 'failed', branch_name: repo.default_branch.name)
-      builds.push Travis::API::V3::Models::Build.create(repository_id: repo.id, created_at: DateTime.now - 2, duration: 10,   number: 3, state: 'passed', branch_name: repo.default_branch.name)
-      builds.push Travis::API::V3::Models::Build.create(repository_id: repo.id, created_at: DateTime.now    , duration: 6000, number: 4, state: 'failed', branch_name: repo.default_branch.name)
+      builds.push Travis::API::V3::Models::Build.create(repository_id: repo.id, created_at: DateTime.now - 5, duration: 600,  number: 1, state: 'passed',   branch_name: repo.default_branch.name)
+      builds.push Travis::API::V3::Models::Build.create(repository_id: repo.id, created_at: DateTime.now - 4, duration: 1200, number: 2, state: 'failed',   branch_name: repo.default_branch.name)
+      builds.push Travis::API::V3::Models::Build.create(repository_id: repo.id, created_at: DateTime.now - 2, duration: 10,   number: 3, state: 'passed',   branch_name: repo.default_branch.name)
+      builds.push Travis::API::V3::Models::Build.create(repository_id: repo.id, created_at: DateTime.now - 1, duration: 0,    number: 4, state: 'canceled', branch_name: repo.default_branch.name)
+      builds.push Travis::API::V3::Models::Build.create(repository_id: repo.id, created_at: DateTime.now    , duration: 6000, number: 4, state: 'failed',  branch_name: repo.default_branch.name)
       get("/v3/repo/#{repo.id}/overview/build_duration") }
     example    { expect(last_response).to be_ok }
     example    { expect(parsed_body).to be == {
@@ -39,17 +40,17 @@ describe Travis::API::V3::Services::Overview::GetBuildDuration do
           "state" => "failed",
           "duration" => 6000
         },
-        { "id" => builds[-2].id,
+        { "id" => builds[-3].id,
           "number" => "3",
           "state" => "passed",
           "duration" => 10
         },
-        { "id" => builds[-3].id,
+        { "id" => builds[-4].id,
           "number" => "2",
           "state" => "failed",
           "duration" => 1200
         },
-        { "id" => builds[-4].id,
+        { "id" => builds[-5].id,
           "number" => "1",
           "state" => "passed",
           "duration" => 600
