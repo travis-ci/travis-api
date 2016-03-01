d1935e978455835e1a827f5442c975b7e5a2bd65
Steffen Kötte <steffen.koette@gmail.com>
2016-03-01 10:07:49 +0100

    optimize recent build history db query

--- lib/travis/api/v3/services/overview/recent_build_history.rb
+++ lib/travis/api/v3/services/overview/recent_build_history.rb
@@ -3,16 +3,19 @@ module Travis::API::V3
 
     def run!
       repo = find(:repository)
-      builds = Models::Build.where(:repository_id => repo.id, :branch => repo.default_branch_name).where("created_at > ?", Date.today - 9)
+
+      builds = Models::Build.where(:repository_id => repo.id, :branch => repo.default_branch_name).where("created_at > ?", Date.today - 9).group(:state, "date_trunc('day', created_at)").count
 
       hash = {}
       hash.default_proc = proc do |hash, key|
         hash[key] = Hash.new(0)
       end
 
-      for build in builds do
-        hash[build.created_at.to_date][build.state] += 1
-      end
+      builds.each {|key, value|
+        state      = key[0]
+        created_at = key[1]
+        hash[created_at.to_date][state] = value
+      }
 
       [{recent_build_history: hash}]
     end
