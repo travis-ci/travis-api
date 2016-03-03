ca09547452c824b45d348c016add90d4ff7a2078
Hiro Asari <asari.ruby@gmail.com>
2016-03-02 14:44:14 -1000

    Access job via attr_reader

--- lib/travis/api/v3/services/job/debug.rb
+++ lib/travis/api/v3/services/job/debug.rb
@@ -2,9 +2,11 @@ module Travis::API::V3
   class Services::Job::Debug < Service
     params "quiet"
 
+    attr_reader :job
+
     def run
       raise LoginRequired unless access_control.logged_in? or access_control.full_access?
-      raise NotFound      unless job = find(:job)
+      raise NotFound      unless @job = find(:job)
       access_control.permissions(job).debug!
 
       job.config.merge! debug_data
@@ -14,11 +16,11 @@ module Travis::API::V3
       accepted(job: job, state_change: :created)
     end
 
-    def debug_data(j)
+    def debug_data
       {
         debug: {
           stage: 'before_install',
-          previous_state: j.state,
+          previous_state: job.state,
           created_by: access_control.user.login,
           quiet: params["quiet"] || false
         }
