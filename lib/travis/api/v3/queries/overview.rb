02acef0d7db0d8e749c7d7c9640fdf8a25350ff5
Steffen Kötte <steffen.koette@gmail.com>
2016-03-04 11:50:08 +0100

    refactor streak to use one SQL query less move AR to query

--- lib/travis/api/v3/queries/overview.rb
+++ lib/travis/api/v3/queries/overview.rb
@@ -12,5 +12,14 @@ module Travis::API::V3
     def recent_build_history(repo)
       Models::Build.where(:repository_id => repo.id, :branch => repo.default_branch_name).where("created_at > ?", Date.today - 9).group("date_trunc('day', created_at)", :state).count
     end
+
+    def streak_last_failing_build_id(repo)
+      last_failing_build = Models::Build.where(:repository_id => repo.id, :branch => repo.default_branch_name, :state => ['failed', 'canceled', 'errored'], :event_type => ['push', 'cron']).order("id DESC").select(:id).first
+      (last_failing_build != nil) ? last_failing_build.id : 0
+    end
+
+    def streak(repo)
+      Models::Build.select('COUNT(*) AS "count", MIN(created_at) AS "created_at", "event_type"').where(:repository_id => repo.id, :branch => repo.default_branch_name, :state => 'passed', :event_type => ['push', 'cron']).where("id > ?", streak_last_failing_build_id(repo)).group(:event_type).to_a
+    end
   end
 end
--- lib/travis/api/v3/services/overview/streak.rb
+++ lib/travis/api/v3/services/overview/streak.rb
@@ -2,15 +2,21 @@ module Travis::API::V3
   class Services::Overview::Streak < Service
 
     def run!
-      repo = find(:repository)
-      last_failing_build = Models::Build.where(:repository_id => repo.id, :branch => repo.default_branch_name, :state => ['failed', 'canceled', 'errored'], :event_type => ['push', 'cron']).order("id DESC").select(:id).first
+      result = query.streak(find(:repository))
 
-      fail_id = (last_failing_build != nil) ? last_failing_build.id : 0
+      start_of_streak = DateTime::Infinity.new
+      build_count = 0
 
-      first_build_of_streak = Models::Build.where(:repository_id => repo.id, :branch => repo.default_branch_name, :state => 'passed', :event_type => ['push', 'cron']).where("id > ?", fail_id).order("id ASC").select(:created_at).first
-      build_count = Models::Build.where(:repository_id => repo.id, :branch => repo.default_branch_name, :state => 'passed', :event_type => 'push').where("id > ?", fail_id).count
+      for builds in result
+        if builds.created_at < start_of_streak
+          start_of_streak = builds.created_at
+        end
+        if builds.event_type == "push"
+          build_count = builds.count.to_i
+        end
+      end
 
-      day_count = (build_count > 0) ? ((Time.now - first_build_of_streak.created_at)/(60*60*24)).floor : 0
+      day_count = (build_count > 0) ? ((Time.now - start_of_streak)/(60*60*24)).floor : 0
 
       [{streak: {days: day_count, builds: build_count}}]
     end
