00aaf32b1e6d15fcab21582300c64760a01769da
carlad <carla@travis-ci.com>
2016-04-15 17:22:41 +0200

    add branch_name for request

--- lib/travis/api/v3/models/request.rb
+++ lib/travis/api/v3/models/request.rb
@@ -1,10 +1,20 @@
 module Travis::API::V3
   class Models::Request < Model
+    BRANCH_REF = %r{refs/heads/(.*?)$}
+
     belongs_to :commit
     belongs_to :repository
     belongs_to :owner, polymorphic: true
     has_many   :builds
     serialize  :config
     serialize  :payload
+
+    def branch_name
+      ref =~ BRANCH_REF and $1
+    end
+
+    def ref
+      payload['ref'] if payload
+    end
   end
 end
