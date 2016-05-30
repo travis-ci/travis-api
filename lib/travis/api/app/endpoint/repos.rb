574dfab7728bc9e206e5544d20e0c7a5d1b82501
Renée Hendricksen <renee@travis-ci.org>
2016-05-30 18:07:54 +0200

    fix the error - it's reporting to Sentry - Yay

--- lib/travis/api/app/endpoint/repos.rb
+++ lib/travis/api/app/endpoint/repos.rb
@@ -15,7 +15,6 @@ class Travis::Api::App
       #
       # json(:repositories)
       get '/' do
-        we want an error
         prefer_follower do
           params['ids'] = params['ids'].split(',') if params['ids'].respond_to?(:split)
           respond_with service(:find_repos, params)
