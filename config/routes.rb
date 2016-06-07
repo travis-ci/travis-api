e9445969359f52dd12238b5fe9299841f690a9a4
Aly Fulton <aly@travis-ci.com>
2016-06-07 14:08:26 -0400

    fixed user route Rails magic and pluralizations is the best and worst. :)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :users
+  resources :user
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
