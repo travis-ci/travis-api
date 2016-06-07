1b6459921f9718a03efd2c115a6bc27d35cfbc2f
Aly Fulton <aly@travis-ci.com>
2016-06-07 16:30:21 -0400

    fix pluralization and factorygirl

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :user
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
