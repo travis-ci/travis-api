b70084afa3c1fcdaa20bce18765e57fb7a567da1
Katrin Marschner <katrin.marschner@gmail.com>
2016-06-07 16:36:37 +0200

    start of user view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
