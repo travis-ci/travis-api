f098d4092f5d51f8651a86d75ce49433bf4892ea
Katrin Marschner <katrin.marschner@gmail.com>
2016-06-14 15:25:57 +0200

    fix failing tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe User, type: :model do
 
   describe '.find' do
     it 'finds user by ID' do
-      expect(user.find(125283)).to_not be_nil
+      expect(User.find(125283)).to_not be_nil
     end
 
     it 'retrieves login name' do
-      expect(user.find(125283).login).to eql 'sinthetix'
+      expect(User.find(125283).login).to eql 'sinthetix'
     end
   end
 end
