bfbecf58b34005c0b3c73bd23fd16bb9c689d439
Katrin Marschner <katrin.marschner@gmail.com>
2016-06-14 16:21:49 +0200

    change user route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :users
+  get 'user/:id' => 'users#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
