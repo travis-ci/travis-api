93e2cfbac65d0d8e4312da3d19bb73b5398d9d72
Katrin Marschner <katrin.marschner@gmail.com>
2016-06-15 08:24:37 +0200

    small fixes
    remove unnecessary content from membership factory, fix indentation,
    remove comments

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,8 +1,4 @@
 class Repository < ActiveRecord::Base
-  # has_many :commits
-  # has_many :requests
-  # has_many :branches
-  # has_many :builds
   has_many :jobs
   has_many :permissions
   has_many :users,   through:     :permissions
