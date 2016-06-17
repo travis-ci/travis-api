3e6ed7240e65c9a0240977bed5bf8a0f7152105f
Katrin Marschner <katrin.marschner@gmail.com>
2016-06-17 12:49:55 +0200

    add organization info
    sections Basic Information, Github Information, Users;
    links to users, adapted the routes;
    correct copy/paste mistake in show.html.erb

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -3,5 +3,6 @@ class OrganizationsController < ApplicationController
     @organization = Organization.find(params[:id])
     @repositories = @organization.repositories
     @pending_jobs = Job.from_repositories(@repositories).not_finished
+    @users = @organization.users
   end
 end
