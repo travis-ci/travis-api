8d472a4f8989401940a115c94824aba51d0eac9a
Aly Fulton <aly@travis-ci.com>
2016-06-20 23:39:48 -0400

    change English a little bit for flash messages

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,7 +1,7 @@
 class OrganizationsController < ApplicationController
   def show
     @organization = Organization.find_by(id: params[:id])
-    return redirect_to root_path, alert: "There is no Organization for that ID" if @organization.nil?
+    return redirect_to root_path, alert: "There is no organization associated with that ID." if @organization.nil?
 
     @repositories = @organization.repositories
     @pending_jobs = Job.from_repositories(@repositories).not_finished
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
-    return redirect_to root_path, alert: "There is no User for that ID" if @user.nil?
+    return redirect_to root_path, alert: "There is no user associated with that ID." if @user.nil?
 
     @repositories = @user.permitted_repositories
     @pending_jobs = Job.from_repositories(@repositories).not_finished
