88d900004232953f43039e0ac9584b6df5a0e147
Joe Corcoran <joe@corcoran.io>
2016-06-21 13:17:44 +0200

    Refactor repeated pattern into extracted method
    Seems like we'll be checking login status before finding a resource
    a lot, so here's a standard way to do it.

--- lib/travis/api/v3/services/env_vars/create.rb
+++ lib/travis/api/v3/services/env_vars/create.rb
@@ -4,8 +4,7 @@ module Travis::API::V3
     params :id, :name, :value, :public, prefix: :env_var
 
     def run!
-      raise LoginRequired unless access_control.logged_in? or access_control.full_access?
-      raise NotFound      unless repository = find(:repository)
+      repository = check_login_and_find(:repository)
       env_var = query(:env_vars).create(repository)
       result(:env_var, env_var, status: 201)
     end
