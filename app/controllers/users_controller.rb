f64b0c4d22756d22ce96dfa6ba8bd50d9668945f
Aly Fulton <aly@travis-ci.com>
2016-06-20 23:24:49 -0400

    add flash alerts to user and org controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(id: params[:id])
+    return redirect_to root_path, alert: "There is no User for that ID" if @user.nil?
+
     @repositories = @user.permitted_repositories
     @pending_jobs = Job.from_repositories(@repositories).not_finished
   end
