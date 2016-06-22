7305797300d9f885249ee88d616126d3f119e510
Katrin Marschner <katrin.marschner@gmail.com>
2016-06-22 15:40:38 +0200

    add tests for slug and permissions_sorted

--- spec/factories/repositories.rb
+++ spec/factories/repositories.rb
@@ -1,5 +1,17 @@
 FactoryGirl.define do
   factory :repository do
     name 'travis-admin'
+
+    factory :repo_with_users do
+      after(:create) do |repo|
+        user_admin = create(:user)
+        user_pull = create(:user)
+        user_push = create(:user)
+
+        create(:permission, repository_id: repo.id, user_id: user_admin.id, admin: true)
+        create(:permission, repository_id: repo.id, user_id: user_pull.id, pull: true)
+        create(:permission, repository_id: repo.id, user_id: user_push.id, push: true)
+      end
+    end
   end
 end
--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -1,4 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Repository, type: :model do
+  describe '#slug' do
+    let!(:repository) { create(:repository, owner_name: 'travis') }
+
+    it 'combines owner_name with repository name' do
+      expect(repository.slug).to eql 'travis/travis-admin'
+    end
+  end
+
+  describe '#permissions_sorted' do
+    let!(:repository_with_users) { create(:repo_with_users) }
+
+    it 'returns Hash with users sorted according to their permissions' do
+      expect(repository_with_users.permissions_sorted).to be_a(Hash)
+
+      expect(repository_with_users.permissions_sorted[:admin]).to be_a(Array)
+      expect(repository_with_users.permissions_sorted[:admin]).not_to be_empty
+
+      expect(repository_with_users.permissions_sorted[:pull]).to be_a(Array)
+      expect(repository_with_users.permissions_sorted[:pull]).not_to be_empty
+
+      expect(repository_with_users.permissions_sorted[:push]).to be_a(Array)
+      expect(repository_with_users.permissions_sorted[:push]).not_to be_empty
+    end
+
+    it 'contains an Array with all admin users' do
+      user = repository_with_users.permissions_sorted[:admin].first
+      expect(user.permissions.first.admin?).to be true
+    end
+
+    it 'contains an Array with all pull access users' do
+      user = repository_with_users.permissions_sorted[:pull].first
+      expect(user.permissions.first.pull?).to be true
+    end
+
+    it 'contains an Array with all push access users' do
+      user = repository_with_users.permissions_sorted[:push].first
+      expect(user.permissions.first.push?).to be true
+    end
+  end
 end
