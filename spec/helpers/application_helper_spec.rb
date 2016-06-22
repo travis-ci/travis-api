7a0e236f5c86942af6a17dcfcce0e6f390106e61
Katrin Marschner <katrin.marschner@gmail.com>
2016-06-22 16:45:42 +0200

    tests for format_duration, format_short_duration

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe UsersHelper, type: :helper do
+  describe 'format_duration' do
+    it 'returns nicely formated time' do
+      expect(helper.format_duration(30)).to eq('30 sec')
+      expect(helper.format_duration(60)).to eq('1 min')
+      expect(helper.format_duration(3600)).to eq('1 hrs')
+      expect(helper.format_duration(4865)).to eq('1 hrs 21 min 5 sec')
+    end
+  end
+
+  describe 'format_short_duration' do
+    it 'returns nicely formated time' do
+      expect(helper.format_short_duration(30)).to eq('30s')
+      expect(helper.format_short_duration(60)).to eq('1m')
+      expect(helper.format_short_duration(3600)).to eq('1h')
+      expect(helper.format_short_duration(4865)).to eq('1h 21m 5s')
+    end
+  end
+end
