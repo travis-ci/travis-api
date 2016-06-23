a5eded1b29a5e4ffc02cd396f3aaeca18959387a
Katrin Marschner <katrin.marschner@gmail.com>
2016-06-23 15:30:50 +0200

    faster loading by using includes

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,6 +3,7 @@ class RepositoriesController < ApplicationController
     @repository = Repository.find_by(id: params[:id])
     return redirect_to root_path, alert: "There is no repository associated with that ID." if @repository.nil?
 
-    @builds = @repository.builds.order('id DESC')
+    @builds = @repository.builds.includes(:commit).order('id DESC')
+    @requests = @repository.requests.includes(:builds)
   end
 end
