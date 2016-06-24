fceaac2388be56b28a3534ac6e5877c4193338d1
Aly Fulton <aly@travis-ci.com>
2016-06-23 23:35:30 -0400

    fix time formatter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,12 @@
 module ApplicationHelper
-  def format_duration(seconds, hrs_suffix: "hrs", min_suffix: "min", sec_suffix: "sec")
-    return "none" if seconds.nil?
-    Time.at(seconds.to_i).utc.strftime("%H #{hrs_suffix} %M #{min_suffix} %S #{sec_suffix}")
+  def format_duration(seconds, hrs_suffix: " hrs", min_suffix: " min", sec_suffix: " sec")
+    hours, minutes, seconds = Time.at(seconds.to_i).utc.strftime("%H:%M:%S").split(':').map(&:to_i)
+
+    [].tap do |parts|
+      parts << "#{hours}#{hrs_suffix}" unless hours.zero?
+      parts << "#{minutes}#{min_suffix}" unless minutes.zero?
+      parts << "#{seconds}#{sec_suffix}" unless seconds.zero?
+    end.join(' ')
   end
 
   def format_short_duration(seconds)
