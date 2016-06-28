f4a64f7ff6b94fa1712f633f15ae2a09ca4db0a8
Katrin Marschner <katrin.marschner@gmail.com>
2016-06-28 17:33:09 +0200

    basics for job view

--- app/models/build.rb
+++ app/models/build.rb
@@ -3,4 +3,5 @@ class Build < ActiveRecord::Base
   belongs_to :repository
   belongs_to :commit
   belongs_to :request
+  has_many   :jobs,     as: :source
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -2,9 +2,17 @@ class Job < ActiveRecord::Base
   self.inheritance_column = :_type_disabled
 
   belongs_to :repository
+  belongs_to :commit
   belongs_to :owner, polymorphic: true
+  belongs_to :build, foreign_key: 'source_id'
+
 
   scope :from_repositories, -> (repositories) { where(repository_id: repositories.map(&:id)) }
   scope :not_finished, -> { where(state: %w[started received queued created]).sort_by {|job|
                                      %w[started received queued created].index(job.state.to_s) } }
+  scope :finished, -> { where(state: %w[passed failed errored canceled]).order('id DESC') }
+
+  def duration
+    started_at && finished_at ? finished_at - started_at : nil
+  end
 end
