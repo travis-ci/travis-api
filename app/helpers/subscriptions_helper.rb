bac2e16a0cf56adf535726072a3d55d4172c624c
Aly Fulton <aly@travis-ci.com>
2016-06-28 21:41:12 -0400

    add format helper

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -2,4 +2,8 @@ module SubscriptionsHelper
   def format_price(amount)
     number_to_currency(amount.to_f/100)
   end
+
+  def format_subscription(name)
+    name.selected_plan.gsub(/-/, ' ').remove('travis ci')
+  end
 end
