a6aa9a709d7bceb08c8f9f717c0df7a75e568739
Katrin Marschner <katrin.marschner@gmail.com>
2016-07-01 17:23:18 +0200

    find the current plan

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,7 +3,7 @@ class SubscriptionsController < ApplicationController
     @subscription = Subscription.find_by(id: params[:id])
     return redirect_to root_path, alert: "There is no subscription associated with that ID." if @subscription.nil?
 
-    @plan = @subscription.plans.last
+    @plan = @subscription.plans.current
     @invoices = @subscription.invoices.order('id DESC')
   end
 end
