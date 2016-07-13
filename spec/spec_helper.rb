e9d481f46f8acb42c92105d1983f653e58c06076
Aly Fulton <aly@travis-ci.com>
2016-07-12 20:28:09 -0400

    disable net connect

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require 'webmock/rspec'
 
 RSpec.configure do |config|
 
+  WebMock.disable_net_connect!(allow_localhost: true)
+  
   config.expect_with :rspec do |expectations|
 
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
