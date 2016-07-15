20384a3ea54eb9258685f718019be5fd338f91ef
Katrin Marschner <katrin.marschner@gmail.com>
2016-07-15 09:28:44 +0200

    strip mock endpoints down to bare minimum

--- spec/features/disable_repository_spec.rb
+++ spec/features/disable_repository_spec.rb
@@ -8,7 +8,7 @@ RSpec.feature "Disable a Repository", :js => true, :type => :feature do
     visit "/repository/#{repository.id}"
 
     WebMock.stub_request(:post, "https://api-fake.travis-ci.com/repo/#{repository.id}/disable").
-      with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'token', 'Content-Length'=>'0', 'Content-Type'=>'application/json', 'Travis-Api-Version'=>'3', 'User-Agent'=>'Faraday v0.9.2'}).
+      with(:headers => {'Authorization'=>'token', 'Content-Type'=>'application/json', 'Travis-Api-Version'=>'3'}).
       to_return(:status => 200, :body => "", :headers => {})
 
     find_link('Disable').trigger('click')
