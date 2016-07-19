5044c4c879fb012cc3cc4eb79b5f29910471746d
Aly Fulton <aly@travis-ci.com>
2016-07-19 19:27:35 -0400

    add empty hashes for config

--- spec/features/cancel_job_spec.rb
+++ spec/features/cancel_job_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Cancel a Job", :js => true, :type => :feature do
-  let!(:job) { create(:job, started_at: '2016-06-29 11:06:01', finished_at: nil) }
+  let!(:job) { create(:job, started_at: '2016-06-29 11:06:01', finished_at: nil, config: {}) }
 
   scenario "User cancels a job" do
     visit "/job/#{job.id}"
