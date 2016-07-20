c9b92444ed2869d12c8eedb07e7dd2eccb94e856
Aly Fulton <aly@travis-ci.com>
2016-07-20 17:54:49 -0400

    modify cancel service tests

--- spec/features/cancel_build_spec.rb
+++ spec/features/cancel_build_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Cancel a Build", :js => true, :type => :feature do
-  let!(:build) { create(:build, started_at: '2016-06-29 11:06:01', finished_at: nil, config: {}) }
+  let!(:build) { create(:build, started_at: '2016-06-29 11:06:01', finished_at: nil, state: 'started', config: {}) }
 
   scenario "User cancels a build" do
     visit "/build/#{build.id}"
@@ -13,6 +13,7 @@ RSpec.feature "Cancel a Build", :js => true, :type => :feature do
     click_button('Cancel')
 
     expect(page).to have_text('Build successfully canceled.')
+    expect(page).to_not have_text('started')
     expect(page).to have_text('canceled', count: 2)
   end
 end
--- spec/features/cancel_job_spec.rb
+++ spec/features/cancel_job_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Cancel a Job", :js => true, :type => :feature do
-  let!(:job) { create(:job, started_at: '2016-06-29 11:06:01', finished_at: nil, config: {}) }
+  let!(:job) { create(:job, started_at: '2016-06-29 11:06:01', finished_at: nil, state: 'started', config: {}) }
 
   scenario "User cancels a job" do
     visit "/job/#{job.id}"
@@ -13,6 +13,7 @@ RSpec.feature "Cancel a Job", :js => true, :type => :feature do
     click_button('Cancel')
 
     expect(page).to have_text('Job successfully canceled.')
+    expect(page).to_not have_text('started')
     expect(page).to have_text('canceled', count: 2)
   end
 end
