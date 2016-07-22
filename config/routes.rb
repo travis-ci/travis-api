7e8135ac4ad09940de793ee1e2e9e2fbfead8c5b
Aly Fulton <aly@travis-ci.com>
2016-07-22 19:08:36 -0400

    trying to resolve conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'home#home'
 
   get 'user/:id' => 'users#show', as: :user
+  post 'user/:id/sync' => 'users#sync', as: :sync_user
+  post 'user/sync_all' => 'users#sync_all', as: :sync_all
 
   get 'organization/:id' => 'organizations#show', as: :organization
 
@@ -10,16 +12,8 @@ Rails.application.routes.draw do
   post 'repository/:id/disable' => 'repositories#disable', as: :disable_repository
 
   get 'request/:id' => 'requests#show', as: :request
-
   get 'build/:id' => 'builds#show', as: :build
-  post 'build/:id/cancel' => 'builds#cancel', as: :cancel_build
-  post 'build/:id/restart' => 'builds#restart', as: :restart_build
-
   get 'job/:id' => 'jobs#show', as: :job
-  post 'job/:id/cancel' => 'jobs#cancel', as: :cancel_job
-  post 'job/:id/restart' => 'jobs#restart', as: :restart_job
-
   get 'subscription/:id' => 'subscriptions#show', as: :subscription
-  
   get 'broadcast' => 'broadcasts#index', as: :broadcast
 end
