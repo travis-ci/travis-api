12537284efa05b5c63ccba876c8aa29b5d813cfa
Katrin Marschner <katrin.marschner@gmail.com>
2016-07-25 14:27:16 +0200

    use current_user from SSO::Helpers for welcome message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
+require_relative '../models/user'
+
 class ApplicationController < ActionController::Base
+  include Travis::SSO::Helpers
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user
 end
