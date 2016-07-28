d8af7634bb7ff398ad39ec1b8bbcb482da387f9d
Katrin Marschner <katrin.marschner@gmail.com>
2016-07-28 14:50:09 +0200

    set default value when setting is missing
    also deletes settings that are not updated via API v3 right now

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,7 +2,7 @@ class Setting
   include ActiveModel::Model
 
   BINARY = %w[builds_only_with_travis_yml build_pushes build_pull_requests]
-  INTEGER = %w[maximum_number_of_builds timeout_hard_limit timeout_log_silence api_builds_rate_limit]
+  INTEGER = %w[maximum_number_of_builds]
 
   attr_reader :repository
 
@@ -10,7 +10,16 @@ class Setting
     @repository = repository
   end
 
+  def defaults
+    {
+      "builds_only_with_travis_yml" => false,
+      "build_pushes" => true,
+      "build_pull_requests" => true,
+      "maximum_number_of_builds" => 0
+    }
+  end
+
   def get
-    @repository.settings || {}
+    defaults.merge(@repository.settings)
   end
 end
