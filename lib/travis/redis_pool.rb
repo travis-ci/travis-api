04c98df21809edc8378f9851552bef17155b84f0
Aly Fulton <aly@travis-ci.com>
2016-07-29 18:53:10 -0400

    take sven's redispool from coreless gatekeeper

---
+++ lib/travis/redis_pool.rb
@@ -0,0 +1,33 @@
+require 'connection_pool'
+require 'redis'
+require 'metriks'
+
+module Travis
+  class RedisPool
+    attr_reader :pool
+
+    def initialize(options = {})
+      pool_options = options.delete(:pool) || {}
+      pool_options[:size] ||= 10
+      pool_options[:timeout] ||= 10
+      @pool = ConnectionPool.new(pool_options) do
+        ::Redis.new(options)
+      end
+    end
+
+    def method_missing(name, *args, &block)
+      # TODO for some reason this blocks during tests
+      # timer = Metriks.timer('redis.pool.wait').time
+      pool.with do |redis|
+        # timer.stop
+        if redis.respond_to?(name)
+          # Metriks.timer("redis.operations").time do
+          redis.send(name, *args, &block)
+          # end
+        else
+          super
+        end
+      end
+    end
+  end
+end
