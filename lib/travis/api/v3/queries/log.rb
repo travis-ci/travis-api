aaaf699bcf98123f2360972122a4de551be75b0d
carlad <carla@travis-ci.com>
2016-07-29 13:51:24 +0200

    add comment

--- lib/travis/api/v3/queries/log.rb
+++ lib/travis/api/v3/queries/log.rb
@@ -1,6 +1,8 @@
 module Travis::API::V3
   class Queries::Log < Query
     def find(job)
+      #TODO check for the log in the DB, and then fetch it from S3 if not.
+      
       job.log
     end
   end
