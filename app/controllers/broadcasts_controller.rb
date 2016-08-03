ed7ffac6b8382f21e4162c59c13a2746a97f7d0b
Katrin Marschner <katrin.marschner@gmail.com>
2016-08-03 12:37:27 +0200

    make redirect case statement a private method

--- app/controllers/broadcasts_controller.rb
+++ app/controllers/broadcasts_controller.rb
@@ -14,20 +14,24 @@ class BroadcastsController < ApplicationController
       flash[:error] = "Could not create broadcast."
     end
 
-    case @broadcast.recipient_type
-    when 'User'
-      redirect_to user_path(@recipient, anchor: "broadcast")
-    when 'Organization'
-      redirect_to organization_path(@recipient, anchor: "broadcast")
-    when 'Repository'
-      redirect_to repository_path(@recipient, anchor: "broadcast")
-    else
-      redirect_to broadcast_path
-    end
+    redirect_to_broadcast_view(@broadcast.recipient_type)
   end
 
   private
     def broadcast_params
       params.require(:broadcast).permit(:recipient_type, :recipient_id, :message, :category)
+    end
+
+    def redirect_to_broadcast_view(recipient_type)
+      case recipient_type
+      when 'User'
+        redirect_to user_path(@recipient, anchor: "broadcast")
+      when 'Organization'
+        redirect_to organization_path(@recipient, anchor: "broadcast")
+      when 'Repository'
+        redirect_to repository_path(@recipient, anchor: "broadcast")
+      else
+        redirect_to broadcast_path
+      end
     end
 end
