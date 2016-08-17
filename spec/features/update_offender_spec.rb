8db082c8d3e88cce5c949772b90bfc976a7f54e0
Katrin Marschner <katrin.marschner@gmail.com>
2016-08-17 17:57:12 +0200

    add check for checked checkbox to feature test

--- spec/features/update_offender_spec.rb
+++ spec/features/update_offender_spec.rb
@@ -8,8 +8,12 @@ RSpec.feature "Update Offender", :js => true, :type => :feature do
     click_on("Account")
 
     find("#offender_offenders").trigger('click')
-    find_button('Update').trigger('click')
+    find_button("Update").trigger('click')
 
     expect(page).to have_text("Abuse settings for Klaus (klaus_maus) updated.")
+
+    # rethink this (is not working without)
+    click_on("Account")
+    expect(page.has_checked_field?("offender[offenders]")).to be true
   end
 end
