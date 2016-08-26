faaa361904d679129b3e944e2f932d1c7a7d6c6e
Aly Fulton <aly@travis-ci.com>
2016-08-26 19:09:44 -0400

    clean up whitespace

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,7 +1,7 @@
 class SubscriptionsController < ApplicationController
   def show
     @subscription = Subscription.find_by(id: params[:id])
-    return redirect_to root_path, alert: "There is no subscription associated with that ID." if @subscription.nil?
+    return redirect_to root_path, alert: 'There is no subscription associated with that ID.' if @subscription.nil?
 
     @plan = @subscription.plans.current
     @invoices = @subscription.invoices.order('id DESC')
@@ -9,15 +9,11 @@ class SubscriptionsController < ApplicationController
 
   def update
     @subscription = Subscription.find_by(id: params[:id])
-
     @subscription.attributes = subscription_params.slice('billing_email', 'vat_id', 'valid_to(1i)', 'valid_to(2i)', 'valid_to(3i)').reject
-
     changes = @subscription.changes
-
     @subscription.save
 
     flash[:notice] = "Updated #{@subscription.owner.login}'s subscription: #{changes.map {|attr, change| "#{attr} changed from #{change.first.inspect} to #{change.last.inspect}"}.join(", ")}"
-
     redirect_to @subscription
   end
 
