06c79f86d3bdb16dcf603bee550911bf222a3139
carlad <carla@travis-ci.com>
2016-08-31 16:41:45 +0200

    fix specs for job and jobs now that there's a new job permission

--- spec/v3/services/job/find_spec.rb
+++ spec/v3/services/job/find_spec.rb
@@ -22,7 +22,8 @@ describe Travis::API::V3::Services::Job::Find, set_app: true do
         "read"            => true,
         "cancel"          => false,
         "restart"         => false,
-        "debug"           => false },
+        "debug"           => false,
+        "delete_log"      => false },
       "id"                => job.id,
       "number"            => job.number,
       "state"             => job.state,
@@ -82,6 +83,7 @@ describe Travis::API::V3::Services::Job::Find, set_app: true do
     let(:headers) {{ 'HTTP_AUTHORIZATION' => "token #{token}"                        }}
     before        { Travis::API::V3::Models::Permission.create(repository: repo, user: repo.owner, pull: true) }
     before        { repo.update_attribute(:private, true)                             }
+    before        { Travis::API::V3::Permissions::Job.any_instance.stubs(:delete_log?).returns(true) }
     before        { get("/v3/job/#{job.id}", {}, headers)                             }
     after         { repo.update_attribute(:private, false)                            }
     example       { expect(last_response).to be_ok                                    }
@@ -93,7 +95,8 @@ describe Travis::API::V3::Services::Job::Find, set_app: true do
         "read"            => true,
         "cancel"          => false,
         "restart"         => false,
-        "debug"           => false },
+        "debug"           => false,
+        "delete_log"      => true },
       "id"                => job.id,
       "number"            => job.number,
       "state"             => job.state,
