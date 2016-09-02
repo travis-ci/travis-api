8413401501d7b0d832c075baf1f637b8c9cc28d0
Katrin Marschner <katrin.marschner@gmail.com>
2016-09-02 10:12:05 +0200

    fix lib autoloading

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module TravisAdmin
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.autoload_paths += ['lib']
+    config.enable_dependency_loading = true
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
