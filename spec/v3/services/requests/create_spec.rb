3ddcb7093a7926d88dfcb637a6d7fda699ae824d
Sven Fuchs <me@svenfuchs.com>
2016-09-22 13:28:20 +0200

    Do not query for request.result when ratelimiting triggering builds
    Gatekeeper 2.0 changed this from `accepted` to now `approved`. After
    some discussion with Carla and Joe we decided to consider this column
    more interal, and remove the condition. It also seems more "right" to
    ratelimit all requests, not only in cases where they are approved by
    Gatekeeper.

--- spec/v3/services/requests/create_spec.rb
+++ spec/v3/services/requests/create_spec.rb
@@ -218,8 +218,8 @@ describe Travis::API::V3::Services::Requests::Create, set_app: true do
     end
 
     describe "when request limit is reached" do
-      before { 10.times { repo.requests.create(event_type: 'api', result: 'accepted') } }
-      before { post("/v3/repo/#{repo.id}/requests", params, headers)                    }
+      before { 10.times { repo.requests.create(event_type: 'api') } }
+      before { post("/v3/repo/#{repo.id}/requests", params, headers) }
 
       example { expect(last_response.status).to be == 429 }
       example { expect(JSON.load(body).to_s).to include(
@@ -242,7 +242,7 @@ describe Travis::API::V3::Services::Requests::Create, set_app: true do
     describe "overrides default request limit if included in repository.settings" do
       before { repo.update_attribute(:settings, { api_builds_rate_limit: 12 }.to_json) }
 
-      before { 10.times { repo.requests.create(event_type: 'api', result: 'accepted') } }
+      before { 10.times { repo.requests.create(event_type: 'api') } }
       before { post("/v3/repo/#{repo.id}/requests", {}, headers) }
 
       example { expect(last_response.status).to be == 202 }
