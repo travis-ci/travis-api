37cf1fee639dbed292be468caf80b9e2a606a8fd
Katrin Marschner <katrin.marschner@gmail.com>
2016-09-27 18:21:49 +0200

    escape slashes for search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
   def search
     unless params[:q].blank?
-      @results = Elasticsearch::Model.search(params[:q], [User, Organization, Repository, Job, Build, Request]).records.to_a
+      query = params[:q].gsub(/([#{Regexp.escape('/')}])/, '\\\\\1')
+      @results = Elasticsearch::Model.search(query, [User, Organization, Repository, Job, Build, Request]).records.to_a
     end
   end
 end
