044755d5681b4c1e8f038b5871b7ea38a0be8fd5
Katrin Marschner <katrin.marschner@gmail.com>
2016-10-05 16:38:13 +0200

    try combining elasticsearch and active record lookups

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,8 @@ class SearchController < ApplicationController
   def search
     unless params[:q].blank?
       query = params[:q].gsub(/([#{Regexp.escape('/')}])/, '\\\\\1')
+      # TO DO: query should be further modified to extract ids from urls, also doesn't search for build/job slugs
+
       payload = {
         from: 0, size: 20,
         query: {
@@ -16,7 +18,13 @@ class SearchController < ApplicationController
           }
         }
       }
-      @results = Elasticsearch::Model.search(payload, [User, Organization, Repository]).records.to_a
+      elasticsearch = Elasticsearch::Model.search(payload, [User, Organization, Repository]).records.to_a
+
+      build   = Build.find_by(id: query)
+      job     = Job.find_by(id: query)
+      request = Request.find_by(id: query)
+
+      @results = elasticsearch.unshift(build, job, request).compact
     end
   end
 end
