68afcb6e60362f862479dda422e6566a2262c0e9
Aly Fulton <aly@travis-ci.com>
2016-10-06 16:33:49 -0400

    capybara wants it here

--- spec/features/update_offender_spec.rb
+++ spec/features/update_offender_spec.rb
@@ -7,12 +7,14 @@ RSpec.feature "Update Offender", :js => true, :type => :feature do
     allow(Travis::DataStores.topaz).to receive(:builds_provided_for)
 
     visit "/users/#{user.id}"
-    click_on('User') #Capybara seems to be demanding this
+
 
     find("#offender_offenders").trigger('click')
     find_button("update-abuse-status").trigger('click')
 
     expect(page).to have_text("Abuse settings for Klaus (klaus_maus) updated.")
+
+    click_on('User') #Capybara seems to be demanding this
 
     expect(page.has_checked_field?("offender[offenders]")).to be true
   end
