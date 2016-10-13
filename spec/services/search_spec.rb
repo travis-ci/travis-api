2713be19566989763b3ea8698636ee19e0920b51
Katrin Marschner <katrin.marschner@gmail.com>
2016-10-13 17:26:02 +0200

    rewrite service tests

--- spec/services/search_spec.rb
+++ spec/services/search_spec.rb
@@ -12,160 +12,191 @@ RSpec.describe Services::Search do
   let!(:build)         { create(:build, id: 6397, repository: repository, number: 567) }
   let!(:job)           { create(:job, id: 35465, repository: repository, number: 567.1) }
 
-  def search(query)
-    Services::Search.new(query).call
-  end
+  let(:search) { Services::Search.new(query).call }
 
   context "explicit search (search by type and id)" do
-    it "finds repository with id=416 for 'repo 416'" do
-      expect(search('repo 416')).to eq([repository])
+    describe "search for 'repo 416' finds repository with id=416" do
+      let(:query) { 'repo 416' }
+      it { expect(search).to eq([repository]) }
     end
 
-    it "finds repository with id=416 for 'repository 416'" do
-      expect(search('repository 416')).to eq([repository])
+    describe "search for 'repository 416' finds repository with id=416" do
+      let(:query) { 'repository 416' }
+      it { expect(search).to eq([repository]) }
     end
 
-    it "finds request with id=4567 for 'request 4567'" do
-      expect(search('request 4567')).to eq([request])
+    describe "search for 'request 4567' finds request with id=4567" do
+      let(:query) { 'request 4567' }
+      it { expect(search).to eq([request]) }
     end
 
-    it "finds build with id=6397 for 'build 6397'" do
-      expect(search('build 6397')).to eq([build])
+    describe "search for 'build 6397' finds build with id=6397" do
+      let(:query) { 'build 6397' }
+      it { expect(search).to eq([build]) }
     end
 
-    it "finds job with id=35465 for 'job 35465'" do
-      expect(search('job 35465')).to eq([job])
+    describe "search for 'job 35465' finds job with id=35465" do
+      let(:query) { 'job 35465' }
+      it { expect(search).to eq([job]) }
     end
 
-    it "finds user with id=162 for 'user 162'" do
-      expect(search('user 162')).to eq([user])
+    describe "search for 'user 162' finds user with id=162" do
+      let(:query) { 'user 162' }
+      it { expect(search).to eq([user]) }
     end
 
-    it "finds user with github_id=12324 for 'user 12324'" do
-      expect(search('user 12324')).to eq([user2])
+    describe "search for 'user 12324' finds user with github_id=12324" do
+      let(:query) { 'user 12324' }
+      it { expect(search).to eq([user2]) }
     end
 
-    it "finds organization with id=46 for 'organization 46'" do
-      expect(search('organization 46')).to eq([organization])
+    describe "search for 'organization 46' finds organization with id=46" do
+      let(:query) { 'organization 46' }
+      it { expect(search).to eq([organization]) }
     end
 
-    it "finds user with github_id=9267 for 'organization 9267'" do
-      expect(search('organization 9267')).to eq([organization2])
+    describe "search for 'organization 9267' finds user with github_id=9267" do
+      let(:query) { 'organization 9267' }
+      it { expect(search).to eq([organization2]) }
     end
   end
 
   context "search for login" do
-    it "finds user with login='lisbethmarianne' for 'lisbethmarianne'" do
-      expect(search('lisbethmarianne')).to eq([user])
+    describe "search for 'lisbethmarianne' finds user with login='lisbethmarianne'" do
+      let(:query) { 'lisbethmarianne' }
+      it { expect(search).to eq([user]) }
     end
 
-    it "finds organization with login='rubymonstas' for 'rubymonstas'" do
-      expect(search('rubymonstas')).to eq([organization])
+    describe "search for 'rubymonstas' finds organization with login='rubymonstas'" do
+      let(:query) { 'rubymonstas' }
+      it { expect(search).to eq([organization]) }
     end
 
-    it "finds organization with login='rubymonstas' for 'Rubymonstas'" do
-      expect(search('Rubymonstas')).to eq([organization])
+    describe "search for 'Rubymonstas' finds organization with login='rubymonstas'" do
+      let(:query) { 'Rubymonstas' }
+      it { expect(search).to eq([organization]) }
     end
 
-    it "finds organization with login='travis-ci' as well as repo with name='travis-ci' for 'travis-ci'" do
-      expect(search('travis-ci')).to include organization2
-      expect(search('travis-ci')).to include repository2
+    describe "search for 'travis-ci' finds organization with login='travis-ci' as well as repo with name='travis-ci'" do
+      let(:query) { 'travis-ci' }
+      it { expect(search).to include organization2 }
+      it { expect(search).to include repository2 }
     end
   end
 
   context "search for name" do
-    it "finds user with name='Katrin' for 'Katrin'" do
-      expect(search('Katrin')).to eq([user])
+    describe "search for 'Katrin' finds user with name='Katrin'" do
+      let(:query) { 'Katrin' }
+      it { expect(search).to eq([user]) }
     end
 
-    it "finds organization with name='Ruby Monstas' for 'Ruby Monstas'" do
-      expect(search('Ruby Monstas')).to eq([organization])
+    describe "search for 'Ruby Monstas' finds organization with name='Ruby Monstas'" do
+      let(:query) { 'Ruby Monstas' }
+      it { expect(search).to eq([organization]) }
     end
 
-    it "finds organization with name='Ruby Monstas' for 'ruby monstas'" do
-      expect(search('ruby monstas')).to eq([organization])
+    describe "search for 'ruby monstas' finds organization with name='Ruby Monstas'" do
+      let(:query) { 'ruby monstas' }
+      it { expect(search).to eq([organization]) }
     end
 
-    it "finds repository with name='diversitytickets' for 'diversitytickets'" do
-      expect(search('diversitytickets')).to eq([repository])
+    describe "search for 'diversitytickets' finds repository with name='diversitytickets'" do
+      let(:query) { 'diversitytickets' }
+      it { expect(search).to eq([repository]) }
     end
   end
 
   context "search for email" do
-    it "finds user with email='katrin@example.com' for 'katrin@example.com'" do
-      expect(search('katrin@example.com')).to eq([user])
+    describe "search for 'katrin@example.com' finds user with email='katrin@example.com'" do
+      let(:query) { 'katrin@example.com' }
+      it { expect(search).to eq([user]) }
     end
 
-    it "finds user with secondary email='lisbethmarianne@example.com' for 'lisbethmarianne@example.com'" do
-      expect(search('lisbethmarianne@example.com')).to eq([user])
+    describe "search for 'lisbethmarianne@example.com' finds user with secondary email='lisbethmarianne@example.com'" do
+      let(:query) { 'lisbethmarianne@example.com' }
+      it { expect(search).to eq([user]) }
     end
   end
 
   context "search for homepage" do
-    it "finds organization with homepage='http://rubymonstas.org/' for 'http://rubymonstas.org/'" do
-      expect(search('http://rubymonstas.org/')).to eq([organization])
+    describe "search for 'http://rubymonstas.org/' finds organization with homepage='http://rubymonstas.org/'" do
+      let(:query) { 'http://rubymonstas.org/' }
+      it { expect(search).to eq([organization]) }
     end
   end
 
   context "search for repository slug" do
-    it "finds repository with slug='rubymonstas/diversitytickets' for 'rubymonstas/diversitytickets'" do
-      expect(search('rubymonstas/diversitytickets')).to eq([repository])
+    describe "search for 'rubymonstas/diversitytickets' finds repository with slug='rubymonstas/diversitytickets'" do
+      let(:query) { 'rubymonstas/diversitytickets' }
+      it { expect(search).to eq([repository]) }
     end
   end
 
   context "search for build slug" do
-    it "finds build with slug='rubymonstas/diversitytickets#567' for 'rubymonstas/diversitytickets#567'" do
-      expect(search('rubymonstas/diversitytickets#567')).to eq([build])
+    describe "search for 'rubymonstas/diversitytickets#567' finds build with slug='rubymonstas/diversitytickets#567'" do
+      let(:query) { 'rubymonstas/diversitytickets#567' }
+      it { expect(search).to eq([build]) }
     end
   end
 
   context "search for job slug" do
-    it "finds job with slug='rubymonstas/diversitytickets#567.1' for 'rubymonstas/diversitytickets#567.1'" do
-      expect(search('rubymonstas/diversitytickets#567.1')).to eq([job])
+    describe "search for 'rubymonstas/diversitytickets#567.1' finds job with slug='rubymonstas/diversitytickets#567.1'" do
+      let(:query) { 'rubymonstas/diversitytickets#567.1' }
+      it { expect(search).to eq([job]) }
     end
   end
 
   context "search with github url" do
-    it "finds user with login='lisbethmarianne' for 'https://github.com/lisbethmarianne'" do
-      expect(search('https://github.com/lisbethmarianne')).to eq([user])
+    describe "search for 'https://github.com/lisbethmarianne' finds user with login='lisbethmarianne'" do
+      let(:query) { 'https://github.com/lisbethmarianne' }
+      it { expect(search).to eq([user]) }
     end
 
-    it "finds user with login='lisbethmarianne' for 'https://github.com/LisbethMarianne'" do
-      expect(search('https://github.com/LisbethMarianne')).to eq([user])
+    describe "search for 'https://github.com/LisbethMarianne' finds user with login='lisbethmarianne'" do
+      let(:query) { 'https://github.com/LisbethMarianne' }
+      it { expect(search).to eq([user]) }
     end
 
-    it "finds organization with login='rubymonstas' for 'https://github.com/rubymonstas'" do
-      expect(search('https://github.com/rubymonstas')).to eq([organization])
+    describe "search for 'https://github.com/rubymonstas' finds organization with login='rubymonstas'" do
+      let(:query) { 'https://github.com/rubymonstas' }
+      it { expect(search).to eq([organization]) }
     end
 
-    it "finds repository with slug='rubymonstas/diversitytickets' for 'https://github.com/rubymonstas/diversitytickets'" do
-      expect(search('https://github.com/rubymonstas/diversitytickets')).to eq([repository])
+    describe "search for 'https://github.com/rubymonstas/diversitytickets' finds repository with slug='rubymonstas/diversitytickets'" do
+      let(:query) { 'https://github.com/rubymonstas/diversitytickets' }
+      it { expect(search).to eq([repository]) }
     end
   end
 
   context "search with travis-ci url" do
-    it "finds user with login='lisbethmarianne' for 'https://travis-ci.com/profile/lisbethmarianne'" do
-      expect(search('https://travis-ci.com/profile/lisbethmarianne')).to eq([user])
+    describe "search for 'https://travis-ci.com/profile/lisbethmarianne' finds user with login='lisbethmarianne'" do
+      let(:query) { 'https://travis-ci.com/profile/lisbethmarianne' }
+      it { expect(search).to eq([user]) }
     end
 
-    it "finds user with login='lisbethmarianne' for 'https://travis-ci.com/profile/LisbethMarianne'" do
-      expect(search('https://travis-ci.com/profile/LisbethMarianne')).to eq([user])
+    describe "search for 'https://travis-ci.com/profile/LisbethMarianne' finds user with login='lisbethmarianne'" do
+      let(:query) { 'https://travis-ci.com/profile/LisbethMarianne' }
+      it { expect(search).to eq([user]) }
     end
 
-    it "finds organization with login='rubymonstas' for 'https://travis-ci.com/profile/rubymonstas'" do
-      expect(search('https://travis-ci.com/profile/rubymonstas')).to eq([organization])
+    describe "search for 'https://travis-ci.com/profile/rubymonstas' finds organization with login='rubymonstas'" do
+      let(:query) { 'https://travis-ci.com/profile/rubymonstas' }
+      it { expect(search).to eq([organization]) }
     end
 
-    it "finds repository with slug='rubymonstas/diversitytickets' for 'https://travis-ci.com/rubymonstas/diversitytickets'" do
-      expect(search('https://travis-ci.com/rubymonstas/diversitytickets')).to eq([repository])
+    describe "search for 'https://travis-ci.com/rubymonstas/diversitytickets' finds repository with slug='rubymonstas/diversitytickets'" do
+      let(:query) { 'https://travis-ci.com/rubymonstas/diversitytickets' }
+      it { expect(search).to eq([repository]) }
     end
 
-    it "finds build with id=6397 for 'https://travis-ci.com/rubymonstas/diversitytickets/builds/6397'" do
-      expect(search('https://travis-ci.com/rubymonstas/diversitytickets/builds/6397')).to eq([build])
+    describe "search for 'https://travis-ci.com/rubymonstas/diversitytickets/builds/6397' finds build with id=6397" do
+      let(:query) { 'https://travis-ci.com/rubymonstas/diversitytickets/builds/6397' }
+      it { expect(search).to eq([build]) }
     end
 
-    it "finds job with id=35465 for 'https://travis-ci.com/rubymonstas/diversitytickets/jobs/35465'" do
-      expect(search('https://travis-ci.com/rubymonstas/diversitytickets/jobs/35465')).to eq([job])
+    describe "search for 'https://travis-ci.com/rubymonstas/diversitytickets/jobs/35465' finds job with id=35465" do
+      let(:query) { 'https://travis-ci.com/rubymonstas/diversitytickets/jobs/35465' }
+      it { expect(search).to eq([job]) }
     end
   end
 end
