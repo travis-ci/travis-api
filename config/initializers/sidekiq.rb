e5d628a6a81405319dffad8a3d19eca474038487
Katrin Marschner <katrin.marschner@gmail.com>
2016-10-26 13:03:38 +0200

    change sidekiq settings

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 if Rails.env.production?
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDIS_URL'] }
+    config.redis = { url: Travis::Config.load.redis.url }
   end
 
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL'] }
+    config.redis = { url: Travis::Config.load.redis.url }
   end
 end
