62de6e9c74d0bb5940aa112e9879561dc4752488
Aly Fulton <aly@travis-ci.com>
2016-10-28 20:49:41 -0400

    major factory overall for subscriptions

--- spec/features/update_subscription_spec.rb
+++ spec/features/update_subscription_spec.rb
@@ -1,12 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'update subscription information', :js => true, :type => :feature do
-  let!(:user) { create :user, login: 'travis-ci' }
-  let!(:organization) { create :organization, login: 'travis-pro' }
-  let!(:user_subscription) { create :subscription, owner: user, cc_token: 'tok_1076247Biz', valid_to: 1.week.from_now, vat_id: 'DE999999999', billing_email: 'contact@travis-ci.com', selected_plan: 'travis-ci-five-builds'}
-  let!(:user_plan) { create :plan, amount: 249, subscription: user_subscription }
-  let!(:org_subscription) { create :subscription, owner: organization, cc_token: 'tok_1079247Biz', valid_to: 1.week.from_now, vat_id: 'DE999999999', billing_email: 'contact@travis-ci.com', selected_plan: 'travis-ci-five-builds'}
-  let!(:org_plan) { create :plan, amount: 249, subscription: org_subscription }
+  let!(:user) { create :user_with_active_subscription}
 
   before { allow(Travis::DataStores.topaz).to receive(:builds_provided_for) }
 
@@ -16,33 +11,26 @@ RSpec.feature 'update subscription information', :js => true, :type => :feature 
     fill_in('subscription_valid_to', with: 2.weeks.from_now.to_date)
     find_button('Update').trigger('click')
 
-    expect(page).to have_text("Updated travis-ci's subscription: valid_to changed from #{1.week.from_now.to_date} to #{2.week.from_now.to_date}")
+    expect(page).to have_text("Updated travisbot's subscription: valid_to changed from #{1.week.from_now.to_date} to #{2.week.from_now.to_date}")
     expect(page).to have_field('subscription_valid_to', with: 2.weeks.from_now.to_date)
   end
 
-  scenario 'Update VAT ID and billing email for Organization' do
-    visit "/organizations/#{organization.id}#subscription"
+  scenario 'Update VAT ID and billing email' do
+    visit "/users/#{user.id}#subscription"
     click_on('Subscription')
     fill_in('subscription_vat_id', :with => 'DE999999998')
     fill_in('subscription_billing_email', :with => 'contact@travis-ci.org')
     find_button('Update').trigger('click')
 
-    expect(page).to have_text("Updated travis-pro's subscription: billing_email changed from contact@travis-ci.com to contact@travis-ci.org, vat_id changed from DE999999999 to DE999999998")
+    expect(page).to have_text("Updated travisbot's subscription: billing_email changed from contact@travis-ci.com to contact@travis-ci.org, vat_id changed from DE999999999 to DE999999998")
     expect(page).to have_field('subscription_vat_id', with: 'DE999999998')
     expect(page).to have_field('subscription_billing_email', with: 'contact@travis-ci.org')
   end
 
-  scenario 'No changes made to User subscription' do
+  scenario 'No changes made to subscription' do
     visit "/users/#{user.id}#subscription"
     find_button('Update').trigger('click')
     expect(page).to have_text ('No subscription changes were made.')
     expect(page).to have_field('subscription_vat_id', with: 'DE999999999')
-  end
-
-  scenario 'No changes made to Organization subscription' do
-    visit "/organizations/#{organization.id}#subscription"
-    find_button('Update').trigger('click')
-    expect(page).to have_text ('No subscription changes were made.')
-    expect(page).to have_field('subscription_vat_id', with: 'DE999999998')
   end
 end
