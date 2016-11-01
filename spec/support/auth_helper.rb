54f6f2a875fbc8cc9cfa850972a804e585b6278a
Katrin Marschner <katrin.marschner@gmail.com>
2016-11-01 11:10:23 +0100

    remove unused AuthHelper

--- spec/support/auth_helper.rb
+++
@@ -1,6 +0,0 @@
-module AuthHelper
-  def admin_login
-    username, password = ENV['ADMIN_NAME'], ENV['ADMIN_PASSWORD']
-    @request.headers['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(username, password)
-  end
-end
