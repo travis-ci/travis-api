4b2285f40435aa804b51a036d34839350efbd4e7
Katrin Marschner <katrin.marschner@gmail.com>
2016-11-03 13:37:55 +0100

    add loading indicator

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   $("body").on("click", '.pagination a', function(e){
     e.preventDefault();
+    $(event.target).closest('section').find('.loading').append("<span class='loading-indicator'><i></i><i></i><i></i></span>");
     $.getScript(this.href);
     return false;
   });
