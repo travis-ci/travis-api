db21ccf3abe519b7d052b2d1fb61a54c5f722afc
Renée Hendricksen <renee@travis-ci.org>
2016-11-20 23:52:12 -0500

    put the gcs mock in the correct place.

--- spec/v3/services/caches/delete_spec.rb
+++ spec/v3/services/caches/delete_spec.rb
@@ -115,21 +115,7 @@ describe Travis::API::V3::Services::Caches::Delete, set_app: true do
         to_return(:status => 200, :body => xml_content, :headers => {})
       stub_request(:delete, "https://bucket.s3.amazonaws.com/#{repo.id}/#{result[0]["branch"]}/#{result[0]["slug"]}").
         to_return(:status => 200, :body => xml_content, :headers => {})
-      Fog.mock!
-      Travis.config.cache_options.gcs = { json_key: 'key', google_project: 'pj', bucket_name: 'bucket' }
-      storage = Fog::Storage::Google.new({
-        google_json_key_string: 'key',
-        google_project: 'pj'
-      })
-
-      ## FIXME:
-      bucket = storage.directories.create(key: "#{repo.id}/branch")
-      file = bucket.files.create(
-        key: "some file",
-        body: "something to test"
-      )
     end
-    after { Fog::Mock.reset }
 
     before     { delete("/v3/repo/#{repo.id}/caches") }
     example    { expect(last_response).to be_ok }
@@ -187,7 +173,23 @@ describe Travis::API::V3::Services::Caches::Delete, set_app: true do
       stub_request(:get, "https://www.googleapis.com/storage/v1/b/travis-cache-staging-org-gce/?prefix=#{repo.id}/").
          to_return(:status => 200, :body => xml_content, :headers => {})
 
+      ## FIXME: figure out mock for gcs
+      # Fog.mock!
+      # Travis.config.cache_options.gcs = { json_key: 'key', google_project: 'pj', bucket_name: 'bucket' }
+      # storage = Fog::Storage::Google.new({
+      #   google_json_key_string: 'key',
+      #   google_project: 'pj'
+      # })
+      #
+      # ## FIXME:
+      # bucket = storage.directories.create(key: "#{repo.id}/branch")
+      # file = bucket.files.create(
+      #  key: "some file",
+      #  body: "something to test"
+      # )
     end
+    # after { Fog::Mock.reset }
+
     before     { delete("/v3/repo/#{repo.id}/caches") }
     skip    do
       expect(JSON.load(body)).to be == {
