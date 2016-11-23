5a8b439225dc7b5ca31a5470f2bfa4d72c3c0a39
Igor Wiedler <igor@travis-ci.org>
2016-11-23 20:16:27 +0100

    remove jruby cases, as we are not using or testing against jruby

--- spec/support/active_record.rb
+++ spec/support/active_record.rb
@@ -8,10 +8,7 @@ FileUtils.mkdir_p('log')
 
 # TODO why not make this use Travis::Database.connect ?
 config = Travis.config.database.to_h
-config.merge!('adapter' => 'jdbcpostgresql', 'username' => ENV['USER']) if RUBY_PLATFORM == 'java'
-
 logs_config = Travis.config.logs_database.to_h
-logs_config.merge!('adapter' => 'jdbcpostgresql', 'username' => ENV['USER']) if RUBY_PLATFORM == 'java'
 
 ActiveRecord::Base.default_timezone = :utc
 ActiveRecord::Base.logger = Logger.new('log/test.db.log')
