aac8fe0539a9ef82446a61621a205af3f824d6b1
Renée Hendricksen <renee@travis-ci.org>
2016-11-25 14:39:38 -0500

    working delete tests

--- spec/v3/services/caches/delete_spec.rb
+++ spec/v3/services/caches/delete_spec.rb
@@ -3,14 +3,28 @@ describe Travis::API::V3::Services::Caches::Delete, set_app: true do
   let(:build) { repo.builds.first }
   let(:jobs)  { Travis::API::V3::Models::Build.find(build.id).jobs }
   let(:s3_bucket_name) { "travis-cache-staging-org" }
+  let(:storage) { Fog::Storage.new({
+    aws_access_key_id: "key",
+    aws_secret_access_key: "secret",
+    provider: "AWS"
+    })
+  }
+  let(:bucket){ storage.directories.create(key: s3_bucket_name) }
+  let(:file1){ bucket.files.create(
+    key: "#{repo.github_id}/ha-bug-rm_rf/cache-linux-precise-lkjdhfsod8fu4tc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855--rvm-2.2.5--gemfile-Gemfile.tgz",
+    body: "cached file for one branch.")
+  }
+  let(:file2){ bucket.files.create(
+    key: "#{repo.github_id}/master/cache-linux-precise-e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855--rvm-default--gemfile-Gemfile.tgz",
+    body: "Cached file for the awesome master branch.")
+  }
   let(:result) { [
     {
       "@type"=>"cache",
       "@representation"=>"standard",
       "repository_id"=>1,
-      "size"=>20308738,
+      "size"=>file1.content_length,
       "branch"=>"ha-bug-rm_rf",
-      "last_modified"=>"2009-10-12T17:50:30Z",
       "slug"=>"cache-linux-precise-lkjdhfsod8fu4tc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855--rvm-2.2.5--gemfile-Gemfile.tgz",
       "repo"=>{
         "@type"=>"repository",
@@ -25,9 +39,8 @@ describe Travis::API::V3::Services::Caches::Delete, set_app: true do
       "@type"=>"cache",
       "@representation"=>"standard",
       "repository_id"=>1,
-      "size"=>64994,
+      "size"=>file2.content_length,
       "branch"=>"master",
-      "last_modified"=>"2009-10-12T17:50:30Z",
       "slug"=>"cache-linux-precise-e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855--rvm-default--gemfile-Gemfile.tgz",
       "repo"=>{
         "@type"=>"repository",
@@ -40,99 +53,27 @@ describe Travis::API::V3::Services::Caches::Delete, set_app: true do
       }
     ]
   }
-  let(:xml_content) {
-    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-    <ListBucketResult xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\">
-    <Name>bucket</Name>
-    <Prefix/>
-    <Marker/>
-    <MaxKeys>1000</MaxKeys>
-    <IsTruncated>false</IsTruncated>
-      <Contents>
-          <Key>#{repo.github_id}/ha-bug-rm_rf/cache-linux-precise-lkjdhfsod8fu4tc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855--rvm-2.2.5--gemfile-Gemfile.tgz</Key>
-          <LastModified>2009-10-12T17:50:30.000Z</LastModified>
-          <ETag>&quot;hgb9dede5f27731c9771645a39863328&quot;</ETag>
-          <Size>20308738</Size>
-          <StorageClass>STANDARD</StorageClass>
-          <Owner>
-              <ID>75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a</ID>
-              <DisplayName>mtd@amazon.com</DisplayName>
-          </Owner>
-      </Contents>
-      <Contents>
-         <Key>#{repo.github_id}/master/cache-linux-precise-e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855--rvm-default--gemfile-Gemfile.tgz</Key>
-           <LastModified>2009-10-12T17:50:30.000Z</LastModified>
-           <ETag>&quot;1b2cf535f27731c974343645a3985328&quot;</ETag>
-           <Size>64994</Size>
-           <StorageClass>STANDARD_IA</StorageClass>
-           <Owner>
-              <ID>75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a</ID>
-              <DisplayName>mtd@amazon.com</DisplayName>
-          </Owner>
-      </Contents>
-    </ListBucketResult>"
-  }
 
-  let(:xml_content_single_repo) {
-    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-    <ListBucketResult xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\">
-    <Name>bucket</Name>
-    <Prefix/>
-    <Marker/>
-    <MaxKeys>1000</MaxKeys>
-    <IsTruncated>false</IsTruncated>
-      <Contents>
-          <Key></Key>
-          <LastModified>2009-10-12T17:50:30.000Z</LastModified>
-          <ETag>&quot;hgb9dede5f27731c9771645a39863328&quot;</ETag>
-          <Size>20308738</Size>
-          <StorageClass>STANDARD</StorageClass>
-          <Owner>
-              <ID>75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a</ID>
-              <DisplayName>mtd@amazon.com</DisplayName>
-          </Owner>
-      </Contents>
-    </ListBucketResult>"
-  }
-
-  let(:empty_xml_content) {
-    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-    <ListBucketResult xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\">
-    <Name>bucket</Name>
-    <Prefix/>
-    <Marker/>
-    <MaxKeys>1000</MaxKeys>
-    <IsTruncated>false</IsTruncated>
-    </ListBucketResult>"
-  }
   before { repo.default_branch.save! }
+  before     do
+    Fog.mock!
+    Travis.config.cache_options.s3 = { access_key_id: 'key', secret_access_key: 'secret', bucket_name: s3_bucket_name }
+    Timecop.return
+    Timecop.freeze(Time.now.utc)
+    result[0]["last_modified"] = Time.now.utc.strftime("%FT%TZ")
+    result[1]["last_modified"] = Time.now.utc.strftime("%FT%TZ")
+  end
+  after do
+    Fog::Mock.reset
+    Timecop.return
+  end
 
   describe "delete all on s3" do
-    before     do
-      Fog.mock!
-      Travis.config.cache_options.s3 = { access_key_id: 'key', secret_access_key: 'secret', bucket_name: 'bucket' }
-      storage = Fog::Storage.new({
-        aws_access_key_id: "key",
-        aws_secret_access_key: "secret",
-        provider: "AWS"
-      })
-      bucket = storage.directories.create(key: 'bucket')
-      file = bucket.files.create(
-        key: "#{repo.github_id}/ha-bug-rm_rf/cache-linux-precise-lkjdhfsod8fu4tc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855--rvm-2.2.5--gemfile-Gemfile.tgz",
-        body: "<LastModified>2009-10-12T17:50:30.000Z</LastModified>
-        <ETag>&quot;hgb9dede5f27731c9771645a39863328&quot;</ETag>
-        <Size>20308738</Size>
-        <StorageClass>STANDARD</StorageClass>"
-      )
-    end
-    after { Fog::Mock.reset }
-
     before     { delete("/v3/repo/#{repo.id}/caches") }
     example    { expect(last_response).to be_ok }
     example    do
       expect(JSON.load(body)).to be == {
         "@type"=>"caches",
-        "@href"=>"/v3/repo/#{repo.id}/caches",
         "@representation"=>"standard",
         "caches"=> result
       }
@@ -140,16 +81,10 @@ describe Travis::API::V3::Services::Caches::Delete, set_app: true do
   end
 
   describe "delete for branch" do
-    before     do
-      stub_request(:get, "https://#{s3_bucket_name}.s3.amazonaws.com/?prefix=#{repo.id}/#{result[0]["branch"]}").
-         to_return(:status => 200, :body => xml_content_single_repo, :headers => {})
-    end
-
     example do
       delete("/v3/repo/#{repo.id}/caches", { branch: result[0]["branch"] } )
       expect(JSON.load(body)).to be == {
         "@type"=>"caches",
-        "@href"=>"/v3/repo/1/caches?branch=#{result[0]["branch"]}",
         "@representation"=>"standard",
         "caches"=> [result[0]]
       }
@@ -157,16 +92,10 @@ describe Travis::API::V3::Services::Caches::Delete, set_app: true do
   end
 
   describe "delete for match" do
-    before     do
-      stub_request(:get, "https://#{s3_bucket_name}.s3.amazonaws.com/?prefix=#{repo.id}/#{result[0]["branch"]}").
-         to_return(:status => 200, :body => xml_content, :headers => {})
-    end
-
     example do
       delete("/v3/repo/#{repo.id}/caches", { branch: result[0]["branch"], match: 'dhfsod8fu4' } )
       expect(JSON.load(body)).to be == {
         "@type"=>"caches",
-        "@href"=>"/v3/repo/1/caches?branch=#{result[0]["branch"]}&match=dhfsod8fu4",
         "@representation"=>"standard",
         "caches"=> [result[0]]
       }
@@ -174,6 +103,72 @@ describe Travis::API::V3::Services::Caches::Delete, set_app: true do
   end
 
   describe "delete all on gcs" do
+    let(:xml_content) {
+      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+      <ListBucketResult xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\">
+      <Name>#{s3_bucket_name}</Name>
+      <Prefix/>
+      <Marker/>
+      <MaxKeys>1000</MaxKeys>
+      <IsTruncated>false</IsTruncated>
+        <Contents>
+            <Key>#{repo.github_id}/ha-bug-rm_rf/cache-linux-precise-lkjdhfsod8fu4tc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855--rvm-2.2.5--gemfile-Gemfile.tgz</Key>
+            <LastModified>2009-10-12T17:50:30.000Z</LastModified>
+            <ETag>&quot;hgb9dede5f27731c9771645a39863328&quot;</ETag>
+            <Size>20308738</Size>
+            <StorageClass>STANDARD</StorageClass>
+            <Owner>
+                <ID>75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a</ID>
+                <DisplayName>mtd@amazon.com</DisplayName>
+            </Owner>
+        </Contents>
+        <Contents>
+           <Key>#{repo.github_id}/master/cache-linux-precise-e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855--rvm-default--gemfile-Gemfile.tgz</Key>
+             <LastModified>2009-10-12T17:50:30.000Z</LastModified>
+             <ETag>&quot;1b2cf535f27731c974343645a3985328&quot;</ETag>
+             <Size>64994</Size>
+             <StorageClass>STANDARD_IA</StorageClass>
+             <Owner>
+                <ID>75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a</ID>
+                <DisplayName>mtd@amazon.com</DisplayName>
+            </Owner>
+        </Contents>
+      </ListBucketResult>"
+    }
+
+    let(:xml_content_single_repo) {
+      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+      <ListBucketResult xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\">
+      <Name>#{s3_bucket_name}</Name>
+      <Prefix/>
+      <Marker/>
+      <MaxKeys>1000</MaxKeys>
+      <IsTruncated>false</IsTruncated>
+        <Contents>
+            <Key></Key>
+            <LastModified>2009-10-12T17:50:30.000Z</LastModified>
+            <ETag>&quot;hgb9dede5f27731c9771645a39863328&quot;</ETag>
+            <Size>20308738</Size>
+            <StorageClass>STANDARD</StorageClass>
+            <Owner>
+                <ID>75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a</ID>
+                <DisplayName>mtd@amazon.com</DisplayName>
+            </Owner>
+        </Contents>
+      </ListBucketResult>"
+    }
+
+    let(:empty_xml_content) {
+      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+      <ListBucketResult xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\">
+      <Name>#{s3_bucket_name}</Name>
+      <Prefix/>
+      <Marker/>
+      <MaxKeys>1000</MaxKeys>
+      <IsTruncated>false</IsTruncated>
+      </ListBucketResult>"
+    }
+
     before     do
       stub_request(:get, "https://#{s3_bucket_name}.s3.amazonaws.com/?prefix=#{repo.id}/").
          to_return(:status => 200, :body => empty_xml_content, :headers => {})
