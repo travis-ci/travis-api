48046f7665e6e4481a9fb2b5261d3ca82878567f
Igor Wiedler <igor@igor.io>
2016-12-06 23:01:54 +0100

    disconnect activerecord after connect

--- spec/active_record_postgres_variables_spec.rb
+++ spec/active_record_postgres_variables_spec.rb
@@ -3,6 +3,10 @@ describe "ActiveRecordPostgresVariables" do
   let(:database_options) { Travis.config.database.merge(variables: variables) }
   let(:base) { Class.new(ActiveRecord::Base) }
 
+  after do
+    base.remove_connection
+  end
+
   it "passes on variables to postgres connections" do
     base.establish_connection(database_options)
     expect { base.table_exists? }.to raise_error(ActiveRecord::StatementInvalid, /unrecognized configuration parameter "foo"/)
