d5af9a347e6b631c6eed6f903180cadc49381d06
Curtis Ekstrom <code@fauxton.io>
2016-12-06 13:21:06 +0100

    Add missing v2 fields to v3 build representation
    This adds both `pull_request_number` and `pull_request_title`.

--- spec/v3/services/branch/find_spec.rb
+++ spec/v3/services/branch/find_spec.rb
@@ -31,6 +31,8 @@ describe Travis::API::V3::Services::Branch::Find, set_app: true do
         "duration"       => nil,
         "event_type"     => "push",
         "previous_state" => "passed",
+        "pull_request_number" => nil,
+        "pull_request_title" => nil,
         "started_at"     => "2010-11-12T13:00:00Z",
         "finished_at"    => nil }}}
   end
