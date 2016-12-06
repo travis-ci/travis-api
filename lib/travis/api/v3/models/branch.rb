c64e0d9273f8a3cdfb7d57bddedf96074b18eac1
Igor Wiedler <igor@travis-ci.org>
2016-12-06 14:49:18 +0100

    move conditions: to scope

--- lib/travis/api/v3/models/branch.rb
+++ lib/travis/api/v3/models/branch.rb
@@ -2,7 +2,7 @@ module Travis::API::V3
   class Models::Branch < Model
     belongs_to :repository
     belongs_to :last_build, class_name: 'Travis::API::V3::Models::Build'.freeze
-    has_many   :builds,  -> { order('builds.id DESC'.freeze) }, foreign_key: [:repository_id, :branch], primary_key: [:repository_id, :name], conditions: { event_type: 'push' }
+    has_many   :builds,  -> { where(event_type: 'push').order('builds.id DESC'.freeze) }, foreign_key: [:repository_id, :branch], primary_key: [:repository_id, :name]
     has_many   :commits, -> { order('commits.id DESC'.freeze) }, foreign_key: [:repository_id, :branch], primary_key: [:repository_id, :name]
     has_one    :cron,   dependent: :destroy
 
