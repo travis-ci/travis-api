3a9a5f168714f19e61d3053904a9996f2798c46e
Renée Hendricksen <renee@travis-ci.org>
2016-12-05 22:58:41 -0500

    working find

--- lib/travis/api/v3/queries/beta_features.rb
+++ lib/travis/api/v3/queries/beta_features.rb
@@ -2,8 +2,13 @@ module Travis::API::V3
   class Queries::BetaFeatures < Query
 
     def find(user)
-      user_beta_features = user.beta_features
-      beta_features = BetaFeature.where.not(id: user_beta_features.map(&:beta_feature_id))
+      user_beta_features = user.user_beta_features
+      ids = user_beta_features.pluck(:beta_feature_id)
+      if ids.empty?
+        beta_features = Models::BetaFeature.all
+      else
+        beta_features = Models::BetaFeature.where('id NOT IN (?)', ids)
+      end
       beta_features + user_beta_features
     end
   end
