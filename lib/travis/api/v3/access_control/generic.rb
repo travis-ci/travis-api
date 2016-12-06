b1ade6c8d46e5f660cd42b4048bb125a08d65487
carlad <carla@travis-ci.com>
2016-12-06 18:56:45 +0100

    remove more features check

--- lib/travis/api/v3/access_control/generic.rb
+++ lib/travis/api/v3/access_control/generic.rb
@@ -76,11 +76,11 @@ module Travis::API::V3
     end
 
     def cron_visible?(cron)
-      Travis::Features.owner_active?(:cron, cron.branch.repository.owner) and visible? cron.branch.repository
+      visible? cron.branch.repository
     end
 
     def cron_writable?(cron)
-      Travis::Features.owner_active?(:cron, cron.branch.repository.owner) and writable? cron.branch.repository
+      writable? cron.branch.repository
     end
 
     def job_visible?(job)
