77122f9d08e90a129411aaf0eabad1760337ea30
Renée Hendricksen <renee@travis-ci.org>
2016-12-07 21:18:43 -0500

    use the beta_feature id and set the type

--- lib/travis/api/v3/renderer/user_beta_feature.rb
+++ lib/travis/api/v3/renderer/user_beta_feature.rb
@@ -2,5 +2,7 @@ module Travis::API::V3
   class Renderer::UserBetaFeature < Renderer::ModelRenderer
     representation :standard, :id, :name, :description, :enabled, :feedback_url
     representation :staff_only, :standard, :staff_only
+
+    type           :beta_feature
   end
 end
