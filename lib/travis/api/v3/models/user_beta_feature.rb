7e1007ab9ff4a9f6e2793821de03a0384d0b3424
Renée Hendricksen <renee@travis-ci.org>
2016-12-07 21:25:03 -0500

    use the beta_feature id in the renderer for user_beta_feature

--- lib/travis/api/v3/models/user_beta_feature.rb
+++ lib/travis/api/v3/models/user_beta_feature.rb
@@ -3,7 +3,7 @@ module Travis::API::V3
     belongs_to :user
     belongs_to :beta_feature
 
-    delegate :id, :name, :description, :feedback_url, :staff_only, to: :beta_feature
+    delegate :name, :description, :feedback_url, :staff_only, to: :beta_feature
 
     def enabled
       !!self[:enabled]
