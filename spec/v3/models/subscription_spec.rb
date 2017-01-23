f5458ab6ed03b3382bf69834f32e53429ab03919
carlad <carla@travis-ci.org>
2017-01-23 12:51:28 +0100

    remove comments

--- spec/v3/models/subscription_spec.rb
+++ spec/v3/models/subscription_spec.rb
@@ -1,6 +1,5 @@
 describe Travis::API::V3::Models::Subscription do
   let!(:subscription) { Travis::API::V3::Models::Subscription.create}
-  # Factory.create(:request, event_type: event_type) }
 
   describe "Subscription inactive" do
     before { subscription.update_attributes(cc_token: nil, valid_to: Time.now - 100)}
