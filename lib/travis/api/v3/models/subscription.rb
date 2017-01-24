885176317283424915856b5dc5e0173fc4f92199
carlad <carla@travis-ci.org>
2017-01-24 11:22:47 +0100

    remove + 1.day

--- lib/travis/api/v3/models/subscription.rb
+++ lib/travis/api/v3/models/subscription.rb
@@ -2,7 +2,7 @@ module Travis::API::V3
   class Models::Subscription < Model
 
     def active?
-      valid_to.present? and valid_to + 1.day >= Time.now
+      valid_to.present? and valid_to >= Time.now
     end
 
   end
