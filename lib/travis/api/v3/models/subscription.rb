041f8802e2d0b85fd4fa1e21bbae4a5c42068226
carlad <carla@travis-ci.org>
2017-01-24 10:30:11 +0100

    update active? to confirm w gatekeeper and omit cc_token check

--- lib/travis/api/v3/models/subscription.rb
+++ lib/travis/api/v3/models/subscription.rb
@@ -2,7 +2,7 @@ module Travis::API::V3
   class Models::Subscription < Model
 
     def active?
-      cc_token? and valid_to.present? and valid_to >= Time.now.utc
+      valid_to.present? and valid_to + 1.day >= Time.now
     end
 
   end
