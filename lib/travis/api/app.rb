40c13188cdcdaeedc23a04a50b7b81b59831dbc1
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2017-02-07 18:30:15 +0100

    add raven back in

--- lib/travis/api/app.rb
+++ lib/travis/api/app.rb
@@ -1,6 +1,8 @@
 require 'rack/contrib/post_body_content_type_parser'
 require 'rack/ssl'
 require 'rack-timeout'
+require 'raven'
+require 'raven/integrations/rack'
 
 module Travis
   module API
@@ -15,11 +17,15 @@ module Travis
         app = self
 
         Rack::Builder.app do
+          if Travis.production?
+            use Raven::Rack
+            use Rack::SSL
+          end
+
           use Travis::API::CORS
           use Rack::Timeout
           use Rack::Deflater
           use Rack::PostBodyContentTypeParser
-          use Rack::SSL if Travis.production?
           use Travis::API::ResponseCleaner
 
           map('/v3') { run app.router }
