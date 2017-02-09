6ceed595ea92d306d93ddccd2ea44a590cc7d5e4
Tyranja <tyranja@cassiopeia.uberspace.de>
2017-02-09 12:53:10 +0100

    Add s3 configs

--- lib/travis/api/v3/queries/log.rb
+++ lib/travis/api/v3/queries/log.rb
@@ -10,7 +10,7 @@ module Travis::API::V3
       raise EntityMissing, 'log not found'.freeze if log.nil?
       #if the log has been archived, go to s3
       if log.archived_at
-        content = s3_config.buckets.find(bucket_name).content
+        content = s3_config.buckets.find(bucket_name).objects.find(prefix).content
         p "#" * 60
         p content
         p bucket_name
