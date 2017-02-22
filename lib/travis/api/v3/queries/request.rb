6cd030fa7cdc8d4375a7cf75e989f350e7267b0e
Sven Fuchs <me@svenfuchs.com>
2017-02-22 17:07:29 +0100

    create a request record on POST to /requests

--- lib/travis/api/v3/queries/request.rb
+++ lib/travis/api/v3/queries/request.rb
@@ -6,9 +6,12 @@ module Travis::API::V3
       raise ServerError, 'repository does not have a github_id'.freeze unless repository.github_id
       raise WrongParams, 'missing user'.freeze                         unless user and user.id
 
+      record = create_request(repository)
+
       payload = {
         repository: { id: repository.github_id, owner_name: repository.owner_name, name: repository.name },
         user:       { id: user.id },
+        id:         record.id,
         message:    message,
         branch:     branch || repository.default_branch_name,
         config:     config || {}
@@ -16,6 +19,18 @@ module Travis::API::V3
 
       perform_async(:build_request, type: 'api'.freeze, credentials: { token: token }, payload: JSON.dump(payload))
       payload
+    end
+
+    private
+
+    def create_request(repository)
+      Models::Request.create!(
+        event_type: :api,
+        state:      :pending,
+        repository: repository,
+        owner:      repository.owner,
+        private:    repository.private
+      )
     end
   end
 end
