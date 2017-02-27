93b61ab8449f5450c5c706d577f7e229b81d1121
Joe Corcoran <joe@corcoran.io>
2017-02-27 12:51:54 +0100

    Rename error

--- lib/travis/api/v3/services/key_pair/find.rb
+++ lib/travis/api/v3/services/key_pair/find.rb
@@ -2,7 +2,7 @@ module Travis::API::V3
   class Services::KeyPair::Find < Service
     def run!
       repository = check_login_and_find(:repository)
-      paid_feature!(repository)
+      private_repo_feature!(repository)
       query.find(repository)
     end
   end
