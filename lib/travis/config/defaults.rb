4a1e4e87cbdb08f265710e9349ddd2dfd789d142
Aakriti Gupta <aakriti@travis-ci.org>
2017-03-03 13:40:54 +0100

    Remove filter on rails project repos - since this was in place when rails repo had our largest build matrix and the system couldn't support it - no longer the case and we plan to introduce a more robust fair usage policy

--- lib/travis/config/defaults.rb
+++ lib/travis/config/defaults.rb
@@ -38,7 +38,6 @@ module Travis
             ssl:           {},
             redis:         { url: 'redis://localhost:6379' },
             repository:    { ssl_key: { size: 4096 } },
-            repository_filter: { include: [/^rails\/rails/], exclude: [/\/rails$/] },
             encryption:    Travis.env == 'development' || Travis.env == 'test' ? { key: 'secret' * 10 } : {},
             sync:          { organizations: { repositories_limit: 1000 } },
             states_cache:  { memcached_servers: 'localhost:11211' },
