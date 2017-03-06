8a9e3c2421cca7b92c83389108156227b94bc19d
carlad <carla@travis-ci.com>
2017-03-06 17:36:42 +0100

    add super - i think we need it

--- lib/travis/api/v3/renderer/cache.rb
+++ lib/travis/api/v3/renderer/cache.rb
@@ -2,10 +2,5 @@ module Travis::API::V3
   class Renderer::Cache < Renderer::ModelRenderer
     representation(:minimal,  :repository_id, :size, :name, :branch, :last_modified)
     representation(:standard, *representations[:minimal], :repo)
-    # 
-    # def self.available_attributes
-    #   [:branch, :name]
-    # end
-
   end
 end
