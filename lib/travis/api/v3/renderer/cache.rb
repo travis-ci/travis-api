2556fc452ad1c9755d9bd98663174e4fffdef474
carlad <carla@travis-ci.com>
2017-03-06 17:20:11 +0100

    override caches attributes in renderer

--- lib/travis/api/v3/renderer/cache.rb
+++ lib/travis/api/v3/renderer/cache.rb
@@ -2,5 +2,10 @@ module Travis::API::V3
   class Renderer::Cache < Renderer::ModelRenderer
     representation(:minimal,  :repository_id, :size, :name, :branch, :last_modified)
     representation(:standard, *representations[:minimal], :repo)
+
+    def self.available_attributes
+      [:branch, :name]
+    end
+
   end
 end
