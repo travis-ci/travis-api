5edc2a01df06480dc3b260272e49f2c2fd517fb3
Igor Wiedler <igor@travis-ci.org>
2017-03-06 14:33:21 +0100

    update link to blog post about running unicorn on heroku

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-# http://michaelvanrooijen.com/articles/2011/06/01-more-concurrency-on-a-single-heroku-dyno-with-the-new-celadon-cedar-stack/
+# https://michael.vanrooijen.io/parallelism-on-heroku/
 
 worker_processes Integer(ENV.fetch('WEB_CONCURRENCY')) # amount of unicorn workers to spin up
 timeout 30 # restarts workers that hang for 30 seconds
