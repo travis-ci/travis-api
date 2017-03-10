56ac118767e80c8e1b053e523848da120250f8ae
carlad <carla@travis-ci.com>
2017-03-10 10:41:02 +0100

    add :branch and :match attributes to caches renderer

--- lib/travis/api/v3/renderer/caches.rb
+++ lib/travis/api/v3/renderer/caches.rb
@@ -2,5 +2,9 @@ module Travis::API::V3
   class Renderer::Caches < Renderer::CollectionRenderer
     type            :caches
     collection_key  :caches
+
+    def self.available_attributes
+      [:branch, :match]
+     end
   end
 end
