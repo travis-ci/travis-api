325cdf103dd6631a2e84736b54672426572cf356
Igor Wiedler <igor@travis-ci.org>
2017-03-14 17:43:36 +0100

    share postgres connection pool between all logs models
    it looks like the status quo is 3 separate pools.

--- lib/travis/api/v3/models/log.rb
+++ lib/travis/api/v3/models/log.rb
@@ -1,7 +1,5 @@
 module Travis::API::V3
-  class Models::Log < Model
-    establish_connection(Travis.config.logs_database.to_h)
-
+  class Models::Log < Travis::LogsModel
     belongs_to :job
     belongs_to :removed_by, class_name: 'User', foreign_key: :removed_by
     has_many  :log_parts, dependent: :destroy, order: 'number ASC'
