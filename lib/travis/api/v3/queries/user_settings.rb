eede9718f5940e171d92cc87c4be7a9458853f15
Sven Fuchs <me@svenfuchs.com>
2017-03-21 21:55:33 +0100

    remove the feature flag for auto-cancel settings

--- lib/travis/api/v3/queries/user_settings.rb
+++ lib/travis/api/v3/queries/user_settings.rb
@@ -1,26 +1,7 @@
 module Travis::API::V3
   class Queries::UserSettings < Query
-    FEATURE_FLAGGED = %i(
-      auto_cancel_pushes
-      auto_cancel_pull_requests
-    )
-
     def find(repo)
-      filter(repo, repo.user_settings)
+      repo.user_settings
     end
-
-    private
-
-      def filter(repo, settings)
-        settings.select { |setting| !flagged?(setting) || active?(repo) }
-      end
-
-      def flagged?(setting)
-        FEATURE_FLAGGED.include?(setting.name)
-      end
-
-      def active?(repo)
-        Travis::Features.owner_active?(:auto_cancel, repo.owner)
-      end
   end
 end
