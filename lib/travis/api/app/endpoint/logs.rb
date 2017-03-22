c30f6050114999a4f48500035aa8d8c810095b3e
Dan Buch <dan@travis-ci.org>
2017-03-21 21:29:31 -0400

    Halt early if no log is found

--- lib/travis/api/app/endpoint/logs.rb
+++ lib/travis/api/app/endpoint/logs.rb
@@ -7,7 +7,9 @@ class Travis::Api::App
       # Fetches a log by its *id*.
       get '/:id' do |id|
         result = service(:find_log, params).run
-        if result && params[:cors_hax] && result.archived? && result.respond_to?(:archived_url)
+        halt 404 if result.nil?
+
+        if params[:cors_hax] && result.archived? && result.respond_to?(:archived_url)
           redirect result.archived_url, 307
         else
           respond_with result
