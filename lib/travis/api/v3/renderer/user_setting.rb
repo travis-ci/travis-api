960b36f576e6ec2182bbacbf24f3ef60b999a23a
Renée Hendricksen <renee@travis-ci.org>
2017-03-28 11:59:21 -0700

    rename the user settings route to settings

--- lib/travis/api/v3/renderer/user_setting.rb
+++ lib/travis/api/v3/renderer/user_setting.rb
@@ -1,12 +1,13 @@
 module Travis::API::V3
   class Renderer::UserSetting < ModelRenderer
+    type :setting
     representation :standard, :name, :value
     representation :minimal, *representations[:standard]
 
     def href
       Renderer.href(:user_setting,
         :"repository.id" => model.repository_id,
-        :"user_setting.name" => name,
+        :"setting.name" => name,
         :"script_name" => script_name
       )
     end
