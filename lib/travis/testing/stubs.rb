aed162103f6e14cd4f1d4b13d65032d2ba2d4ac1
Sven Fuchs <me@svenfuchs.com>
2017-04-07 14:12:20 +0200

    add tag name on v2 /job and /builds

--- lib/travis/testing/stubs.rb
+++ lib/travis/testing/stubs.rb
@@ -121,6 +121,7 @@ module Travis
           commit: '62aae5f70ceee39123ef',
           range: '0cd9ffaab2c4ffee...62aae5f70ceee39123ef',
           branch: 'master',
+          tag_name: nil,
           ref: 'refs/master',
           message: 'the commit message',
           author_name: 'Sven Fuchs',
