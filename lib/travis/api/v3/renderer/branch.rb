7bd91795a2d36aed19925cc50f4b976aa1aeb2c2
carlad <carla@travis-ci.org>
2017-04-19 11:10:10 +0200

    update renderer

--- lib/travis/api/v3/renderer/branch.rb
+++ lib/travis/api/v3/renderer/branch.rb
@@ -4,7 +4,12 @@ module Travis::API::V3
     representation(:standard, :name, :repository, :default_branch, :exists_on_github, :last_build)
 
     def render(representation)
-      super
+      super unless include?(:recent_builds)
+      representation(:standard, :name, :repository, :default_branch, :exists_on_github, :recent_builds)
+    end
+
+    def recent_builds
+      model.builds.first(10)
     end
   end
 end
