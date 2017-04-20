c3b3a30d70f711709e88a67d976e9dbc2e19b1fb
carlad <carla@travis-ci.org>
2017-04-20 14:17:42 +0200

    use include in spec instead of ==

--- spec/v3/services/branch/find_spec.rb
+++ spec/v3/services/branch/find_spec.rb
@@ -1,7 +1,7 @@
 describe Travis::API::V3::Services::Branch::Find, set_app: true do
   let(:repo)  { Travis::API::V3::Models::Repository.where(owner_name: 'svenfuchs', name: 'minimal').first }
   let(:build) { repo.builds.first }
-  let(:recent_builds) { repo.builds.first.branch.builds.first(10) }
+
   before { repo.default_branch.save! }
 
   describe "public repository, existing branch" do
@@ -40,35 +40,6 @@ describe Travis::API::V3::Services::Branch::Find, set_app: true do
   describe "including recent_builds" do
     before     { get("/v3/repo/#{repo.id}/branch/master?include=branch.recent_builds") }
     example    { expect(last_response).to be_ok }
-    example    { expect(JSON.load(body)).to be == {
-      "@type"            => "branch",
-      "@href"            => "/v3/repo/#{repo.id}/branch/master",
-      "@representation"  => "standard",
-      "name"             => "master",
-      "default_branch"   => true,
-      "exists_on_github" => true,
-      "repository"       => {
-        "@type"          => "repository",
-        "@href"          => "/v3/repo/#{repo.id}",
-        "@representation"=> "minimal",
-        "id"             => repo.id,
-        "name"           => "minimal",
-        "slug"           => "svenfuchs/minimal"},
-      "last_build"       => {
-        "@type"          => "build",
-        "@href"          => "/v3/build/#{build.id}",
-        "@representation"=> "minimal",
-        "id"             => build.id,
-        "number"         => build.number,
-        "state"          => build.state,
-        "duration"       => nil,
-        "event_type"     => "push",
-        "previous_state" => "passed",
-        "pull_request_number" => build.pull_request_number,
-        "pull_request_title" => build.pull_request_title,
-        "started_at"     => "2010-11-12T13:00:00Z",
-        "finished_at"    => nil },
-      "recent_builds"    => recent_builds
-            }}
+    example    { expect(JSON.load(body)).to include('recent_builds')}
   end
 end
