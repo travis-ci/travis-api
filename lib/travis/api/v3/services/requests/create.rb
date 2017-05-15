e4320d37472f6526b29e499271a7190fe15062ab
carlad <carla@travis-ci.org>
2017-05-15 10:48:12 +0200

    add prefix for attributes

--- lib/travis/api/v3/services/requests/create.rb
+++ lib/travis/api/v3/services/requests/create.rb
@@ -5,7 +5,7 @@ module Travis::API::V3
     private_constant :TIME_FRAME, :LIMIT
 
     result_type :request
-    params "request", "user", :config, :message, :branch, :token
+    params "request", "user", :config, :message, :branch, :token, prefix: :request
 
     def run
       repository = check_login_and_find(:repository)
