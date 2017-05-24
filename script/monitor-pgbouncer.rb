8fb1992edef39ec4dbd4bd3392adffeba189658b
Igor Wiedler <igor@travis-ci.org>
2017-05-24 11:28:43 +0200

    pgbouncer: move keys array out loop

--- script/monitor-pgbouncer.rb
+++ script/monitor-pgbouncer.rb
@@ -9,11 +9,11 @@ Librato::Metrics.authenticate ENV['LIBRATO_USER'], ENV['LIBRATO_TOKEN']
 
 conn = PGconn.open(url)
 
+keys = %w(cl_active cl_waiting sv_active sv_idle sv_used sv_tested sv_login maxwait)
+
 loop do
   res  = conn.exec('SHOW POOLS')
   res.each do |row|
-    keys = %w(cl_active cl_waiting sv_active sv_idle sv_used sv_tested sv_login maxwait)
-
     queue = Librato::Metrics::Queue.new
     keys.each do |k|
       queue.add "pgbouncer.#{row['database']}.#{k}": { source: librato_source, value: row[k] }
