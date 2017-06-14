d9a85f74baf22c326d855205875de349a28908ae
Joe Corcoran <joe@corcoran.io>
2017-06-14 17:33:19 +0200

    Rename variable

--- lib/travis/api/v3/renderer/build.rb
+++ lib/travis/api/v3/renderer/build.rb
@@ -14,23 +14,23 @@ module Travis::API::V3
     end
 
     def created_by
-      if created_by = model.created_by
+      if creator = model.created_by
         payload = {
           '@type' => model.sender_type.downcase,
-          '@href' => created_by_href(created_by),
+          '@href' => created_by_href(creator),
           '@representation' => 'minimal'.freeze,
-          'id' => created_by.id,
-          'login' => created_by.login
+          'id' => creator.id,
+          'login' => creator.login
         }
-        payload['avatar_url'] = V3::Renderer::AvatarURL.avatar_url(created_by) if include?('created_by.avatar_url')
+        payload['avatar_url'] = V3::Renderer::AvatarURL.avatar_url(creator) if include?('created_by.avatar_url')
         payload
       end
     end
 
-    private def created_by_href(created_by)
-      case created_by
-      when V3::Models::Organization then Renderer.href(:organization, script_name: script_name, id: created_by.id)
-      when V3::Models::User         then Renderer.href(:user, script_name: script_name, id: created_by.id)
+    private def created_by_href(creator)
+      case creator
+      when V3::Models::Organization then Renderer.href(:organization, script_name: script_name, id: creator.id)
+      when V3::Models::User         then Renderer.href(:user, script_name: script_name, id: creator.id)
       end
     end
 
