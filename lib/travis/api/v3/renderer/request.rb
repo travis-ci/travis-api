6370e76c90f2b7418ada50de1e2d94a4f48ee16b
Brandon Ferguson <bnferguson@gmail.com>
2017-06-19 13:15:21 +0200

    Let's include the whole build but not in minimal

--- lib/travis/api/v3/renderer/request.rb
+++ lib/travis/api/v3/renderer/request.rb
@@ -1,6 +1,6 @@
 module Travis::API::V3
   class Renderer::Request < ModelRenderer
-    representation(:minimal,  :id, :build_id, :state, :result, :message)
-    representation(:standard, *representations[:minimal], :repository, :branch_name, :commit, :owner, :created_at, :event_type)
+    representation(:minimal,  :id, :state, :result, :message)
+    representation(:standard, *representations[:minimal], :repository, :branch_name, :commit, :build, :owner, :created_at, :event_type)
   end
 end
