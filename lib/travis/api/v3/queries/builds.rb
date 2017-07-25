0bf690360b8352ca72baee197fe3ed92f358502c
carlad <carla@travis-ci.org>
2017-07-25 12:15:46 +0200

    remove commented out filter

--- lib/travis/api/v3/queries/builds.rb
+++ lib/travis/api/v3/queries/builds.rb
@@ -25,7 +25,6 @@ module Travis::API::V3
 
       relation = relation.includes(:commit).includes(branch: :last_build).includes(:repository)
       relation = relation.includes(branch: { last_build: :commit }) if includes? 'build.commit'.freeze
-      # relation = relation.includes(jobs: [:repository, :commit, :stage]) if includes? 'build.jobs'.freeze or includes? 'job'.freeze
       relation
     end
 
