7af0d2fd61d222dddfba36abfc358aae0cae0796
Ana Rosas <a.rosas10@gmail.com>
2017-09-25 18:42:30 +0200

    Load Travis::Config for Slack

--- lib/services/slack.rb
+++ lib/services/slack.rb
@@ -8,12 +8,12 @@ module Services
     attr_reader :queue
 
     def setup
-      return unless config = Travis.config.slack and queue.nil?
+      return unless config = Travis::Config.load.slack and queue.nil?
       @queue = Queue.new
       Thread.new do
         loop do
           payload = { text: @queue.pop }
-          Travis.config.slack.to_hash.each do |key, value|
+          Travis::Config.load.slack.to_hash.each do |key, value|
             payload[key] = value unless key.to_s == 'url'
           end
           Faraday.post(config.url, JSON.dump(payload))
