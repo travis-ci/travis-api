ae928699b9d051ee36940ea910b5715b811baf30
Piotr Sarnacki <drogus@gmail.com>
2017-10-19 09:53:40 +0200

    Copy obfuscating specs from V2 job specs to config obfuscator

---
+++ spec/v3/config_obfuscator_spec.rb
@@ -0,0 +1,106 @@
+require 'travis/api/v3/config_obfuscator'
+
+describe Travis::API::V3::ConfigObfuscator do
+  describe 'obfuscated config' do
+    let(:repo) { Factory(:repository) }
+    before { repo.regenerate_key! }
+
+    it 'handles nil env' do
+      config = { rvm: '1.8.7', env: nil }
+      result = obfuscator = described_class.new(config, repo.key).obfuscate
+
+      result.should == {
+        rvm: '1.8.7',
+        env: nil
+      }
+    end
+
+    it 'leaves regular vars untouched' do
+      config = { rvm: '1.8.7', env: 'FOO=foo' }
+      result = obfuscator = described_class.new(config, repo.key).obfuscate
+
+      result.should == {
+        rvm: '1.8.7',
+        env: 'FOO=foo'
+      }
+    end
+
+    it 'obfuscates env vars, including accidents' do
+      secure = Travis::SecureConfig.new(repo.key)
+      config = { rvm: '1.8.7',
+                 env: [secure.encrypt('BAR=barbaz'), secure.encrypt('PROBLEM'), 'FOO=foo']
+               }
+      result = obfuscator = described_class.new(config, repo.key).obfuscate
+
+      result.should == {
+        rvm: '1.8.7',
+        env: 'BAR=[secure] [secure] FOO=foo'
+      }
+    end
+
+    it 'handles nil secure var' do
+      secure = Travis::SecureConfig.new(repo.key)
+      config = { rvm: '1.8.7',
+                 env: [{ secure: nil }, { secure: secure.encrypt('FOO=foo') }],
+                 global_env: [{ secure: nil }, { secure: secure.encrypt('BAR=bar') }]
+               }
+      result = obfuscator = described_class.new(config, repo.key).obfuscate
+
+      result.should == {
+        rvm: '1.8.7',
+        env: 'FOO=[secure]',
+        global_env: 'BAR=[secure]'
+      }
+    end
+
+    it 'normalizes env vars which are hashes to strings' do
+      secure = Travis::SecureConfig.new(repo.key)
+      config = { rvm: '1.8.7',
+                 env: [{FOO: 'bar', BAR: 'baz'},
+                        secure.encrypt('BAR=barbaz')]
+               }
+      result = obfuscator = described_class.new(config, repo.key).obfuscate
+
+      result.should == {
+        rvm: '1.8.7',
+        env: 'FOO=bar BAR=baz BAR=[secure]'
+      }
+    end
+
+    it 'removes addons config if it is not a hash' do
+      config = { rvm: '1.8.7',
+                 addons: "foo",
+               }
+      result = obfuscator = described_class.new(config, repo.key).obfuscate
+
+      result.should == {
+        rvm: '1.8.7'
+      }
+    end
+
+    it 'removes addons items which are not safelisted' do
+      config = { rvm: '1.8.7',
+                 addons: { sauce_connect: true, firefox: '22.0' },
+               }
+      result = obfuscator = described_class.new(config, repo.key).obfuscate
+
+      result.should == {
+        rvm: '1.8.7',
+        addons: {
+          firefox: '22.0'
+        }
+      }
+    end
+
+    it 'removes source key' do
+      config = { rvm: '1.8.7',
+                 source_key: '1234'
+               }
+      result = obfuscator = described_class.new(config, repo.key).obfuscate
+
+      result.should == {
+        rvm: '1.8.7',
+      }
+    end
+  end
+end
