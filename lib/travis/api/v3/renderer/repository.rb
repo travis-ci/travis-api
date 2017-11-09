270582a69495e3c20aa6efa85818fb2c3318db37
carlad <carla@travis-ci.org>
2017-11-09 15:17:40 +0100

    put current_build back into representation

--- lib/travis/api/v3/renderer/repository.rb
+++ lib/travis/api/v3/renderer/repository.rb
@@ -2,7 +2,7 @@ module Travis::API::V3
   class Renderer::Repository < ModelRenderer
     representation(:minimal,  :id, :name, :slug)
     representation(:standard, :id, :name, :slug, :description, :github_language, :active, :private, :owner, :default_branch, :starred)
-    representation(:experimental, :id, :name, :slug, :description, :github_language, :active, :private, :owner, :default_branch, :starred, :last_started_build)
+    representation(:experimental, :id, :name, :slug, :description, :github_language, :active, :private, :owner, :default_branch, :starred, :current_build, :last_started_build)
 
     hidden_representations(:experimental)
 
