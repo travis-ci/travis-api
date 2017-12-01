f448c9e8fdd548af62aa48df0550dd3a972f87e4
Igor Wiedler <igor@travis-ci.org>
2017-12-01 15:16:19 +0100

    fix spec/active_record_postgres_variables_spec.rb

--- spec/active_record_postgres_variables_spec.rb
+++ spec/active_record_postgres_variables_spec.rb
@@ -1,10 +1,14 @@
 describe "ActiveRecordPostgresVariables" do
   let(:variables) {{ foo: 'bar' }}
   let(:database_options) { Travis.config.database.to_h.merge(variables: variables) }
-  let(:base) { Class.new(ActiveRecord::Base) }
 
   it "passes on variables to postgres connections" do
-    base.establish_connection(database_options)
-    expect { base.table_exists? }.to raise_error(ActiveRecord::StatementInvalid, /unrecognized configuration parameter "foo"/)
+    ActiveRecord::Base.establish_connection(database_options)
+    expect { ActiveRecord::Base.table_exists? }.to raise_error(ActiveRecord::StatementInvalid, /unrecognized configuration parameter "foo"/)
+  end
+
+  after do
+    ActiveRecord::Base.remove_connection
+    ActiveRecord::Base.establish_connection(Travis.config.database.to_h)
   end
 end
