c6ad68d74b086cb20a77dc99ab419d421dfd3bd5
Igor Wiedler <igor@travis-ci.org>
2017-12-04 15:36:41 +0100

    fix received_at examples to use UTC instead of +0200

--- spec/support/payloads.rb
+++ spec/support/payloads.rb
@@ -591,18 +591,18 @@ GITHUB_OAUTH_DATA = {
 }
 
 WORKER_PAYLOADS = {
-  'job:test:receive' => { 'id' => 1, 'state' => 'received',  'received_at'  => '2011-01-01 00:02:00 +0200', 'worker' => 'ruby3.worker.travis-ci.org:travis-ruby-4' },
-  'job:test:start'   => { 'id' => 1, 'state' => 'started',  'started_at'  => '2011-01-01 00:02:00 +0200', 'worker' => 'ruby3.worker.travis-ci.org:travis-ruby-4' },
+  'job:test:receive' => { 'id' => 1, 'state' => 'received',  'received_at'  => '2011-01-01 00:02:00 +0000', 'worker' => 'ruby3.worker.travis-ci.org:travis-ruby-4' },
+  'job:test:start'   => { 'id' => 1, 'state' => 'started',  'started_at'  => '2011-01-01 00:02:00 +0000', 'worker' => 'ruby3.worker.travis-ci.org:travis-ruby-4' },
   'job:test:log'     => { 'id' => 1, 'log' => '... appended' },
   'job:test:log:1'   => { 'id' => 1, 'log' => 'the '  },
   'job:test:log:2'   => { 'id' => 1, 'log' => 'full ' },
   'job:test:log:3'   => { 'id' => 1, 'log' => 'log'   },
-  'job:test:finish'  => { 'id' => 1, 'state' => 'passed', 'finished_at' => '2011-01-01 00:03:00 +0200', 'log' => 'the full log' },
+  'job:test:finish'  => { 'id' => 1, 'state' => 'passed', 'finished_at' => '2011-01-01 00:03:00 +0000', 'log' => 'the full log' },
   'job:test:reset'   => { 'id' => 1 }
 }
 
 WORKER_LEGACY_PAYLOADS = {
-  'job:test:finished' => { 'id' => 1, 'state' => 'finished', 'finished_at' => '2011-01-01 00:03:00 +0200', 'result' => 0, 'log' => 'the full log' }
+  'job:test:finished' => { 'id' => 1, 'state' => 'finished', 'finished_at' => '2011-01-01 00:03:00 +0000', 'result' => 0, 'log' => 'the full log' }
 }
 
 QUEUE_PAYLOADS = {
