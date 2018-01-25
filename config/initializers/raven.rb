b0493095a0ea7a4de94e5342e8d6c4fd15464b85
Carla Drago <carla@travis-ci.com>
2018-01-25 16:02:11 +0100

    change initializer filename

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -3,6 +3,6 @@ require 'raven'
 if ENV['SENTRY_DSN']
   Raven.configure do |config|
     config.dsn = ENV['SENTRY_DSN']
-    raise Exception.new('Sentry test!')
   end
+  raise Exception.new('Sentry test!')
 end
