57136ad7098111f2b29c578db586ba494d439d44
Sven Fuchs <me@svenfuchs.com>
2018-01-29 13:42:31 +0100

    allow setting public_mode via env var

--- lib/travis/config/defaults.rb
+++ lib/travis/config/defaults.rb
@@ -25,6 +25,7 @@ module Travis
 
     define  host:  'travis-ci.org',
             shorten_host:  'trvs.io',
+            public_mode:   !!ENV['PUBLIC_MODE'],
             tokens:        { internal: 'token' },
             auth:          { target_origin: nil },
             assets:        { host: HOSTS[Travis.env.to_sym] },
