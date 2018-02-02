35ce47154089c9ad71809a4fabad1e4e13fb6bf8
Igor Wiedler <igor@travis-ci.org>
2018-02-02 17:42:16 +0100

    fix code accessing Travis.config.metrics

--- lib/travis/notification.rb
+++ lib/travis/notification.rb
@@ -9,7 +9,7 @@ module Travis
       attr_accessor :publishers
 
       def setup(options = { instrumentation: true })
-        Travis::Instrumentation.setup if options[:instrumentation] && Travis.config.metrics.reporter
+        Travis::Instrumentation.setup if options[:instrumentation] && Travis.config.metrics[:reporter]
         publishers << Publisher::Log.new
         publishers << Publisher::Redis.new if Travis::Features.feature_active?(:notifications_publisher_redis)
       end
