bd726cfbcdc7bdab117ad391615164ce1a31f3e3
Carla Drago <carla@travis-ci.com>
2018-02-06 13:03:31 +0100

    remove comments

--- lib/travis/api/v3/queries/enterprise_license.rb
+++ lib/travis/api/v3/queries/enterprise_license.rb
@@ -4,7 +4,6 @@ module Travis::API::V3
   class Queries::EnterpriseLicense < Query
     def active_users
       redis = Thread.current[:redis] ||= ::Redis.connect(url: Travis.config.redis.url)
-      # redis = ::Redis.connect(url: ENV['REDIS_URL'])
       redis.keys("t:*").count
     end
   end
