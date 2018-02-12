bea2b19bd90e186685bae001a63a11a0a00f53c7
carlad <carlad@users.noreply.github.com>
2018-02-12 18:25:58 +0100

    add suspended check to enterprise active users (#646)
    * add suspended check
    
    * update query
    
    * new build

--- lib/travis/api/v3/queries/enterprise_license.rb
+++ lib/travis/api/v3/queries/enterprise_license.rb
@@ -3,7 +3,7 @@ require 'redis'
 module Travis::API::V3
   class Queries::EnterpriseLicense < Query
     def active_users
-      Travis::API::V3::Models::User.where('github_oauth_token IS NOT NULL')
+      Travis::API::V3::Models::User.where('github_oauth_token IS NOT NULL AND suspended=false')
     end
   end
 end
