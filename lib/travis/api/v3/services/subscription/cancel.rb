e3336c01bf95817ded20222de52fee5206ff4b4d
Ana Rosas <a.rosas10@gmail.com>
2018-02-26 18:46:37 -0600

    Add services for cancel subscription and edit address

--- lib/travis/api/v3/services/subscription/cancel.rb
+++ lib/travis/api/v3/services/subscription/cancel.rb
@@ -1,5 +1,10 @@
 module Travis::API::V3
   class Services::Subscription::Cancel < Service
 
+    def run!
+      current_user_id = access_control.user.id
+      query(:subscription).cancel(current_user_id)
+      accepted(payload: params)
+    end
   end
 end
