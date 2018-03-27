cd49ed7c93cf177343ff3e948f852bbe4cb21463
Tyranja <tyranja@cassiopeia.uberspace.de>
2018-03-27 12:40:34 +0200

    Use the nested params method to declare the attribute

--- lib/travis/api/v3/queries/subscriptions.rb
+++ lib/travis/api/v3/queries/subscriptions.rb
@@ -1,5 +1,9 @@
 module Travis::API::V3
   class Queries::Subscriptions < Query
+    params :plan, :coupon, :organization_id
+    params :first_name, :last_name, :company, :address, :address2, :city, :country, :state, :vat_id, :zip_code, :billing_email, prefix: :billing_info
+    params :card_owner, :expiration_date, :last_digits, prefix: :credit_card_info
+
     def all(user_id)
       client = BillingClient.new(user_id)
       client.all
@@ -11,25 +15,9 @@ module Travis::API::V3
         :plan => params['plan'],
         :coupon => params['coupon'],
         :organization_id => params['organization_id'],
-        :billing_info => {
-          :first_name => params['billing_info.first_name'],
-          :last_name => params['billing_info.last_name'],
-          :company => params['billing_info.company'],
-          :address => params['billing_info.address'],
-          :address2 => params['billing_info.address2'],
-          :city => params['billing_info.city'],
-          :country => params['billing_info.country'],
-          :state => params['billing_info.state'],
-          :vat_id => params['billing_info.vat_id'],
-          :zip_code => params['billing_info.zip_code'],
-          :billing_email => params['billing_info.billing_email']
-        },
-        :credit_card_info => {
-          :card_owner => params['credit_card_info.card_owner'],
-          :expiration_date => params['credit_card_info.expiration_date'],
-          :last_digits => params['credit_card_info.last_digits']
-        }
-      )
+        :billing_info => billing_info_params,
+        :credit_card_info => credit_card_info_params
+        )
     end
   end
 end
