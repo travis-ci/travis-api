6fd99a833137af9813cb5f55aeece025c47d05da
Buck Doyle <b@chromatin.ca>
2018-04-13 14:01:42 -0500

    Add user properties for Intercom (#732)
    This is V2-only but can be applied to V3 when needed.

--- spec/unit/serialize/v2/http/user_spec.rb
+++ spec/unit/serialize/v2/http/user_spec.rb
@@ -21,7 +21,45 @@ describe Travis::Api::Serialize::V2::Http::User do
       'synced_at' => json_format_time(Time.now.utc - 1.hour),
       'correct_scopes' => true,
       'created_at' => json_format_time(Time.now.utc - 2.hours),
+      'first_logged_in_at' => json_format_time(Time.now.utc - 1.5.hours),
       'channels' => ["private-user-1"]
     }
+  end
+
+  context 'when there is an Intercom HMAC secret key' do
+    before do
+      Travis.config.intercom = {
+        hmac_secret_key: 'USER_HASH_SECRET_KEY'
+      }
+    end
+
+    it 'user' do
+      secure_user_hash = OpenSSL::HMAC.hexdigest(
+        'sha256',
+        'USER_HASH_SECRET_KEY',
+        '1'
+      )
+
+      data['user'].should == {
+        'id' => 1,
+        'name' => 'Sven Fuchs',
+        'login' => 'svenfuchs',
+        'email' => 'svenfuchs@artweb-design.de',
+        'gravatar_id' => '402602a60e500e85f2f5dc1ff3648ecb',
+        'avatar_url' => 'https://0.gravatar.com/avatar/402602a60e500e85f2f5dc1ff3648ecb',
+        'locale' => 'de',
+        'is_syncing' => false,
+        'synced_at' => json_format_time(Time.now.utc - 1.hour),
+        'correct_scopes' => true,
+        'created_at' => json_format_time(Time.now.utc - 2.hours),
+        'first_logged_in_at' => json_format_time(Time.now.utc - 1.5.hours),
+        'secure_user_hash' => secure_user_hash,
+        'channels' => ["private-user-1"]
+      }
+    end
+
+    after do
+      Travis.config.intercom = nil
+    end
   end
 end
