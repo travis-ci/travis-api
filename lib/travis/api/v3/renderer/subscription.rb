b60ecdaba41ae149cffa4cbc8ddf34077b1766a5
Ana Rosas <a.rosas10@gmail.com>
2018-04-17 22:31:33 -0500

    Remove invoices from Subscription renderer

--- lib/travis/api/v3/renderer/subscription.rb
+++ lib/travis/api/v3/renderer/subscription.rb
@@ -1,6 +1,6 @@
 module Travis::API::V3
   class Renderer::Subscription < ModelRenderer
-    representation(:standard, :id, :valid_to, :plan, :coupon, :status, :source, :billing_info, :credit_card_info, :owner, :invoices)
+    representation(:standard, :id, :valid_to, :plan, :coupon, :status, :source, :billing_info, :credit_card_info, :owner)
   end
 
   class Renderer::BillingInfo < ModelRenderer
