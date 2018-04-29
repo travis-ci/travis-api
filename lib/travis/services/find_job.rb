37e9810487c5c90ecbb1c59affd050ec8b1c8bcc
Sven Fuchs <me@svenfuchs.com>
2018-04-29 20:33:51 +0200

    reduce find job query for pusher authentication

--- lib/travis/services/find_job.rb
+++ lib/travis/services/find_job.rb
@@ -31,13 +31,20 @@ module Travis
         end
 
         def load_result
-          columns = scope(:job).column_names
-          columns -= %w(config) if params[:exclude_config]
-          columns = columns.map { |c| %Q{"jobs"."#{c}"} }
-          scope = scope(:job).includes(:commit, :config).select(columns)
           job = scope.find_by_id(params[:id])
           job.config = nil if params[:exclude_config]
           job
+        end
+
+        def scope
+          scope = super(:job)
+          return scope.select(params[:columns]) if params[:columns]
+          columns = scope.column_names
+          columns -= %w(config) if params[:exclude_config]
+          columns -= %w(commit) if params[:exclude_commit]
+          columns.map { |c| %("jobs"."#{c}") }
+          scope = scope.includes(:config) unless params[:exclude_config]
+          scope.select(columns)
         end
     end
   end
