9297b3b17b6653f95b899ea972b7478b1866f593
Josh Kalderimis <josh.kalderimis@gmail.com>
2018-05-10 20:16:14 -0700

    stuff is hard

--- spec/v3/services/user/builds_spec.rb
+++ spec/v3/services/user/builds_spec.rb
@@ -20,119 +20,126 @@ describe Travis::API::V3::Services::Builds::ForCurrentUser, set_app: true do
     deploy = build.stages.create(number: 2, name: 'deploy')
     build.jobs[0, 2].each { |job| job.update_attributes!(stage: test) }
     build.jobs[2, 2].each { |job| job.update_attributes!(stage: deploy) }
-
-    Timecop.freeze(Time.now)
-    get(url, {}, headers)
   end
 
   describe "builds for current_user, authenticated as user with access" do
-    example       { expect(last_response).to be_ok                                    }
-    example       { expect(parsed_body).to eql_json({
-      "@type"                 => "builds",
-      "@href"                 => "/v3/user/builds",
-      "@representation"       => "standard",
-      "@pagination"           => {
-        "limit"               => 20,
-        "offset"              => 0,
-        "count"               => 1,
-        "is_first"            => true,
-        "is_last"             => true,
-        "next"                => nil,
-        "prev"                => nil,
-        "first"               => {
-          "@href"             => "/v3/user/builds",
-          "offset"            => 0,
-          "limit"             => 20 },
-        "last"                => {
-          "@href"             => "/v3/user/builds",
-          "offset"            => 0,
-          "limit"             => 20 }},
-      "builds"                => [{
-        "@type"               => "build",
-        "@href"               => "/v3/build/#{build.id}",
-        "@representation"     => "standard",
-        "@permissions"        => {
-          "read"              => true,
-          "cancel"            => false,
-          "restart"           => false },
-        "id"                  => build.id,
-        "number"              => "3",
-        "state"               => "configured",
-        "duration"            => nil,
-        "event_type"          => "push",
-        "previous_state"      => "passed",
-        "pull_request_number" => nil,
-        "pull_request_title"  => nil,
-        "started_at"          => "2010-11-12T13:00:00Z",
-        "finished_at"         => nil,
-        "tag"                 => nil,
-        "stages"              => [{
-           "@type"            => "stage",
-           "@representation"  => "minimal",
-           "id"               => stages[0].id,
-           "number"           => 1,
-           "name"             => "test",
-           "state"            => stages[0].state,
-           "started_at"       => stages[0].started_at,
-           "finished_at"      => stages[0].finished_at},
-          {"@type"            => "stage",
-           "@representation" => "minimal",
-           "id"               => stages[1].id,
-           "number"          => 2,
-           "name"             => "deploy",
-           "state"            => stages[1].state,
-           "started_at"       => stages[1].started_at,
-           "finished_at"      => stages[1].finished_at}],
-        "jobs"                => [
-          {
-          "@type"             => "job",
-          "@href"             => "/v3/job/#{jobs[0].id}",
-          "@representation"   => "minimal",
-          "id"                => jobs[0].id},
-          {
-          "@type"             => "job",
-          "@href"             => "/v3/job/#{jobs[1].id}",
-          "@representation"   => "minimal",
-          "id"                => jobs[1].id},
-          {
-          "@type"             => "job",
-          "@href"             => "/v3/job/#{jobs[2].id}",
-          "@representation"   => "minimal",
-          "id"                => jobs[2].id},
-          {
-          "@type"             => "job",
-          "@href"             => "/v3/job/#{jobs[3].id}",
-          "@representation"   => "minimal",
-          "id"                => jobs[3].id}],
-        "repository"          => {
-          "@type"             => "repository",
-          "@href"             => "/v3/repo/#{repo.id}",
-          "@representation"   => "minimal",
-          "id"                => repo.id,
-          "name"              => "minimal",
-          "slug"              => "svenfuchs/minimal"},
-        "branch"              => {
-          "@type"             => "branch",
-          "@href"             => "/v3/repo/#{repo.id}/branch/master",
-          "@representation"   => "minimal",
-          "name"              => "master"},
-        "commit"              => {
-          "@type"             => "commit",
-          "@representation"   => "minimal",
-          "id"                => 5,
-          "sha"               => "add057e66c3e1d59ef1f",
-          "ref"               => "refs/heads/master",
-          "message"           => "unignore Gemfile.lock",
-          "compare_url"       => "https://github.com/svenfuchs/minimal/compare/master...develop",
-          "committed_at"      => "2010-11-12T12:55:00Z"},
-        "created_by"          => {
-          "@type"             => "user",
-          "@href"             => "/v3/user/1",
-          "@representation"   => "minimal",
-          "id"                => 1,
-          "login"             => "svenfuchs"},
-        "updated_at" => json_format_time_with_ms(build.updated_at),
-      }]
-    })}
+    before do
+      Timecop.freeze(Time.now)
+      get(url, {}, headers)
+    end
+
+    it "has an ok response" do 
+      expect(last_response).to be_ok
+    end
+
+    it "is recognizable as a list of builds" do
+      expect(parsed_body).to eql_json({
+        "@type"                 => "builds",
+        "@href"                 => "/v3/user/builds",
+        "@representation"       => "standard",
+        "@pagination"           => {
+          "limit"               => 20,
+          "offset"              => 0,
+          "count"               => 1,
+          "is_first"            => true,
+          "is_last"             => true,
+          "next"                => nil,
+          "prev"                => nil,
+          "first"               => {
+            "@href"             => "/v3/user/builds",
+            "offset"            => 0,
+            "limit"             => 20 },
+          "last"                => {
+            "@href"             => "/v3/user/builds",
+            "offset"            => 0,
+            "limit"             => 20 }},
+        "builds"                => [{
+          "@type"               => "build",
+          "@href"               => "/v3/build/#{build.id}",
+          "@representation"     => "standard",
+          "@permissions"        => {
+            "read"              => true,
+            "cancel"            => false,
+            "restart"           => false },
+          "id"                  => build.id,
+          "number"              => "3",
+          "state"               => "configured",
+          "duration"            => nil,
+          "event_type"          => "push",
+          "previous_state"      => "passed",
+          "pull_request_number" => nil,
+          "pull_request_title"  => nil,
+          "started_at"          => "2010-11-12T13:00:00Z",
+          "finished_at"         => nil,
+          "tag"                 => nil,
+          "stages"              => [{
+            "@type"            => "stage",
+            "@representation"  => "minimal",
+            "id"               => stages[0].id,
+            "number"           => 1,
+            "name"             => "test",
+            "state"            => stages[0].state,
+            "started_at"       => stages[0].started_at,
+            "finished_at"      => stages[0].finished_at},
+            {"@type"            => "stage",
+            "@representation" => "minimal",
+            "id"               => stages[1].id,
+            "number"          => 2,
+            "name"             => "deploy",
+            "state"            => stages[1].state,
+            "started_at"       => stages[1].started_at,
+            "finished_at"      => stages[1].finished_at}],
+          "jobs"                => [
+            {
+            "@type"             => "job",
+            "@href"             => "/v3/job/#{jobs[0].id}",
+            "@representation"   => "minimal",
+            "id"                => jobs[0].id},
+            {
+            "@type"             => "job",
+            "@href"             => "/v3/job/#{jobs[1].id}",
+            "@representation"   => "minimal",
+            "id"                => jobs[1].id},
+            {
+            "@type"             => "job",
+            "@href"             => "/v3/job/#{jobs[2].id}",
+            "@representation"   => "minimal",
+            "id"                => jobs[2].id},
+            {
+            "@type"             => "job",
+            "@href"             => "/v3/job/#{jobs[3].id}",
+            "@representation"   => "minimal",
+            "id"                => jobs[3].id}],
+          "repository"          => {
+            "@type"             => "repository",
+            "@href"             => "/v3/repo/#{repo.id}",
+            "@representation"   => "minimal",
+            "id"                => repo.id,
+            "name"              => "minimal",
+            "slug"              => "svenfuchs/minimal"},
+          "branch"              => {
+            "@type"             => "branch",
+            "@href"             => "/v3/repo/#{repo.id}/branch/master",
+            "@representation"   => "minimal",
+            "name"              => "master"},
+          "commit"              => {
+            "@type"             => "commit",
+            "@representation"   => "minimal",
+            "id"                => 5,
+            "sha"               => "add057e66c3e1d59ef1f",
+            "ref"               => "refs/heads/master",
+            "message"           => "unignore Gemfile.lock",
+            "compare_url"       => "https://github.com/svenfuchs/minimal/compare/master...develop",
+            "committed_at"      => "2010-11-12T12:55:00Z"},
+          "created_by"          => {
+            "@type"             => "user",
+            "@href"             => "/v3/user/1",
+            "@representation"   => "minimal",
+            "id"                => 1,
+            "login"             => "svenfuchs"},
+          "updated_at" => json_format_time_with_ms(build.updated_at),
+        }]
+      })
+    end
   end
 end
