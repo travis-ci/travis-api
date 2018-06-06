861fc2017a3e3a288b5f441cd4abf301f91a7922
Jan Schulte <hello@unexpected-co.de>
2018-06-06 16:05:22 +0200

    Pass aws region into Fog::Storage constructor

--- lib/travis/api/v3/remote_query.rb
+++ lib/travis/api/v3/remote_query.rb
@@ -71,7 +71,7 @@ module Travis::API::V3
     end
 
     def s3_connection
-      Fog::Storage.new(aws_access_key_id: s3_config[:access_key_id], aws_secret_access_key: s3_config[:secret_access_key], provider: 'AWS')
+      Fog::Storage.new(aws_access_key_id: s3_config[:access_key_id], aws_secret_access_key: s3_config[:secret_access_key], region: s3_config[:region], provider: 'AWS')
     end
 
     def s3_bucket
