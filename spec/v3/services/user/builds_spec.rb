981503a3c70fd69c13419ba9687b589a7a401138
Josh Kalderimis <josh.kalderimis@gmail.com>
2018-06-08 17:33:41 -0700

    missed another place for a spec fix

--- spec/v3/services/user/builds_spec.rb
+++ spec/v3/services/user/builds_spec.rb
@@ -68,6 +68,7 @@ describe Travis::API::V3::Services::Builds::ForCurrentUser, set_app: true do
             "restart"           => false },
           "id"                  => build.id,
           "number"              => "3",
+          "private"             => false,
           "state"               => "configured",
           "duration"            => nil,
           "event_type"          => "push",
