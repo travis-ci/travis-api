55672fb8cf5a14fba4db49242c50cfe9893c90ff
Joe Corcoran <joe@corcoran.online>
2018-06-15 16:15:56 +0200

    Order messages by level, error first

--- spec/v3/services/messages/for_request_spec.rb
+++ spec/v3/services/messages/for_request_spec.rb
@@ -2,7 +2,8 @@ describe Travis::API::V3::Services::Messages::ForRequest, set_app: true do
 
   let(:repo) { Travis::API::V3::Models::Repository.where(owner_name: 'svenfuchs', name: 'minimal').first }
   let(:request) { repo.requests.first }
-  let!(:message) { Travis::API::V3::Models::Message.create!(subject_id: request.id, subject_type: 'Request')}
+  let!(:message) { Travis::API::V3::Models::Message.create!(level: 'info', subject_id: request.id, subject_type: 'Request')}
+  let!(:message_2) { Travis::API::V3::Models::Message.create!(level: 'error', subject_id: request.id, subject_type: 'Request')}
 
   describe "retrieve request messages on a public repository" do
     before     { get("/v3/repo/#{repo.id}/request/#{request.id}/messages")     }
@@ -19,6 +20,9 @@ describe Travis::API::V3::Services::Messages::ForRequest, set_app: true do
                   "representation",
                   "@pagination")
     }
+    example 'ordered messages by level' do
+      expect(JSON.load(body)['messages'].map { |m| m['id'] }).to eq [message_2.id, message.id]
+    end
   end
 
   describe "retrieve request messages on private repository, private API, authenticated as user with access" do
