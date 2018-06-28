79e4628152808097314c988e90529f00720c617a
Buck Doyle <b@chromatin.ca>
2018-06-28 11:37:37 -0500

    Add first_logged_in_at to V3 users

--- lib/travis/api/v3/renderer/user.rb
+++ lib/travis/api/v3/renderer/user.rb
@@ -2,6 +2,6 @@ require 'travis/api/v3/renderer/owner'
 
 module Travis::API::V3
   class Renderer::User < Renderer::Owner
-    representation(:standard, :is_syncing, :synced_at)
+    representation(:standard, :is_syncing, :synced_at, :first_logged_in_at)
   end
 end
