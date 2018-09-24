6a6fa02b19a9e05518cfbed6f3c084058d0a9ab7
Igor Wiedler <igor@travis-ci.org>
2018-09-24 11:16:35 +0200

    slow endpoint for testing timeouts

---
+++ lib/travis/api/app/endpoint/slow.rb
@@ -0,0 +1,12 @@
+require 'travis/api/app'
+
+class Travis::Api::App
+  class Endpoint
+    class Slow < Endpoint
+      if ENV['SLOW_ENDPOINT_ENABLED'] == 'true'
+        get '/' do
+          sleep 60
+        end
+      end
+  end
+end
