143fd12135d91d11464157961217465bc061e65a
Kerri Miller <kerrizor@kerrizor.com>
2018-10-03 15:12:47 -0700

    Add display_migration_ui and DRY spec

--- spec/unit/serialize/v2/http/user_spec.rb
+++ spec/unit/serialize/v2/http/user_spec.rb
@@ -3,13 +3,8 @@ describe Travis::Api::Serialize::V2::Http::User do
 
   let(:user) { stub_user(repository_ids: [1, 4, 8]) }
   let(:data) { described_class.new(user).data }
-
-  before do
-    user.stubs(:github_scopes).returns(['public_repo', 'user:email'])
-  end
-
-  it 'user' do
-    data['user'].should == {
+  let(:expected_data) {
+    {
       'id'                   => 1,
       'name'                 => 'Sven Fuchs',
       'login'                => 'svenfuchs',
@@ -23,7 +18,16 @@ describe Travis::Api::Serialize::V2::Http::User do
       'created_at'           => json_format_time(Time.now.utc - 2.hours),
       'first_logged_in_at'   => json_format_time(Time.now.utc - 1.5.hours),
       'channels'             => ["private-user-1"],
+      'display_migration_ui' => false,
     }
+  }
+
+  before do
+    user.stubs(:github_scopes).returns(['public_repo', 'user:email'])
+  end
+
+  it 'user' do
+    data['user'].should == expected_data
   end
 
   context 'when there is an Intercom HMAC secret key' do
@@ -31,6 +35,7 @@ describe Travis::Api::Serialize::V2::Http::User do
       Travis.config.intercom = {
         hmac_secret_key: 'USER_HASH_SECRET_KEY'
       }
+
     end
 
     it 'user' do
@@ -40,22 +45,9 @@ describe Travis::Api::Serialize::V2::Http::User do
         '1'
       )
 
-      data['user'].should == {
-        'id'                   => 1,
-        'name'                 => 'Sven Fuchs',
-        'login'                => 'svenfuchs',
-        'email'                => 'svenfuchs@artweb-design.de',
-        'gravatar_id'          => '402602a60e500e85f2f5dc1ff3648ecb',
-        'avatar_url'           => 'https://0.gravatar.com/avatar/402602a60e500e85f2f5dc1ff3648ecb',
-        'locale'               => 'de',
-        'is_syncing'           => false,
-        'synced_at'            => json_format_time(Time.now.utc - 1.hour),
-        'correct_scopes'       => true,
-        'created_at'           => json_format_time(Time.now.utc - 2.hours),
-        'first_logged_in_at'   => json_format_time(Time.now.utc - 1.5.hours),
-        'secure_user_hash'     => secure_user_hash,
-        'channels'             => ["private-user-1"],
-      }
+      expected_data["secure_user_hash"] = secure_user_hash
+
+      data['user'].should == expected_data
     end
 
     after do
