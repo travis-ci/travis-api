2c151baf0b393684fd74a6d5fa1e43b5082f215d
Piotr Sarnacki <drogus@gmail.com>
2018-10-19 13:12:36 +0200

    Send an HTTP request to the merge app (#841)
    Before this commit we were trying to talk to a Kafka server directly.
    We decided to change it to HTTP request because of 2 things:
    
      1. API shouldn't be concerned about Kafka details
      2. Setting up Kafka on API was complicated and we couldn't figure it
         out in a timely manner

---
+++ lib/travis/api/v3/models/repository_migration.rb
@@ -0,0 +1,33 @@
+require 'faraday'
+
+module Travis::API::V3::Models
+  class RepositoryMigration
+    class MigrationDisabledError < StandardError; end
+    class MigrationRequestFailed < StandardError; end
+    attr_accessor :repository
+
+    def initialize(repository)
+      self.repository = repository
+    end
+
+    def migrate!
+      if !Travis::Features.owner_active?(:allow_migration, repository.owner)
+        raise MigrationDisabledError
+      end
+
+      token = Travis.config.merge.auth_token
+      url   = Travis.config.merge.api_url
+      connection = Faraday.new(url: url) do |c|
+        c.request :json
+        c.use Faraday::Request::Authorization, 'Token', token
+        c.use OpenCensus::Trace::Integrations::FaradayMiddleware if Travis::Api::App::Middleware::OpenCensus.enabled?
+        c.adapter Faraday.default_adapter
+      end
+      response = connection.post("/api/repository/#{repository.slug}/migrate")
+
+      unless response.success?
+        raise MigrationRequestFailed
+      end
+    end
+  end
+end
