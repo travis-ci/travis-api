db96d11a62261ce1a83ba5f74cf7edf0a58f3be1
Sven Fuchs <me@svenfuchs.com>
2019-03-18 16:17:00 +0100

    restore include=request.yaml_config

--- lib/travis/api/v3/renderer/request.rb
+++ lib/travis/api/v3/renderer/request.rb
@@ -4,7 +4,11 @@ module Travis::API::V3
     representation(:standard, *representations[:minimal], :repository, :branch_name, :commit, :builds, :owner, :created_at, :event_type, :base_commit, :head_commit)
 
     def self.available_attributes
-      super + ['raw_configs']
+      super + %w(raw_configs yaml_config)
+    end
+
+    def yaml_config
+      model.yaml_config&.yaml
     end
 
     def raw_configs
