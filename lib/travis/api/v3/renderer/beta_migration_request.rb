ffe168edf2d5e1bf46258764e0cc977e12be7c6e
Aakriti Gupta <aakriti@travis-ci.org>
2019-04-11 17:45:43 +0200

    Add orgs to beta_migration_reuest renderer

--- lib/travis/api/v3/renderer/beta_migration_request.rb
+++ lib/travis/api/v3/renderer/beta_migration_request.rb
@@ -1,5 +1,9 @@
 module Travis::API::V3
   class Renderer::BetaMigrationRequest < ModelRenderer
-    representation(:standard, :id, :owner_id, :owner_name, :owner_type, :accepted_at)
+    representation(:standard, :id, :owner_id, :owner_name, :owner_type, :accepted_at, :organizations)
+
+    def organizations
+      model.organizations.pluck(:id)
+    end
   end
 end
