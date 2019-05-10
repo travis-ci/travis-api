0afce73edd2dc3489e0a40728ea2c4e6df68f84c
carlad <carla@travis-ci.com>
2019-05-10 14:29:09 +0200

    add downcase

--- lib/travis/api/v3/queries/repository.rb
+++ lib/travis/api/v3/queries/repository.rb
@@ -44,7 +44,7 @@ module Travis::API::V3
     def by_slug
       owner_name, repo_name = slug.split('/')
       Models::Repository.where(
-        "LOWER(repositories.owner_name) = ? AND LOWER(repositories.name) = ? AND repositories.invalidated_at IS NULL",
+        "LOWER(repositories.owner_name.downcase) = ? AND LOWER(repositories.name.downcase) = ? AND repositories.invalidated_at IS NULL",
         owner_name,
         repo_name
       ).first
