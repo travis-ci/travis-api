e03bc8b97d580eca13b5f06a89d83fefcddd4958
Pavel Dotsulenko <pavel@travis-ci.com>
2019-06-03 15:05:08 +0300

    Proxy create beta migration requests from .org to .com api (#911)

--- lib/travis/api/v3/queries/beta_migration_request.rb
+++ lib/travis/api/v3/queries/beta_migration_request.rb
@@ -1,13 +1,18 @@
 module Travis::API::V3
   class Queries::BetaMigrationRequest < Query
+    class ComApiRequestFailed < StandardError; end
 
     def create(current_user, organizations)
       Travis::API::V3::Models::BetaMigrationRequest.create({
-          owner_type:    current_user.class.name.demodulize,
-          owner_id:      current_user.id,
-          owner_name:    current_user.login,
-          organizations: organizations
-        })
+        owner_type:    current_user.class.name.demodulize,
+        owner_id:      current_user.id,
+        owner_name:    current_user.login,
+        organizations: organizations
+      })
+    end
+
+    def send_create_request(current_user, organizations)
+      ComApiClient.new.create_beta_migration_request(current_user, organizations)
     end
   end
 end
