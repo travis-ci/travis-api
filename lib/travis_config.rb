eb1b25abea599df02dc006b7f035131c254402bb
Michał Gajowiak <m.gajowiak@nomtek.com>
2019-07-04 21:51:07 +0200

    Allign code

--- lib/travis_config.rb
+++ lib/travis_config.rb
@@ -3,26 +3,26 @@ require 'travis/config'
 class TravisConfig < Travis::Config
   define admins: [],
          enterprise: false,
-	 host: 'localhost:3000',
-	 service_hook_url: '',
-	 api_endpoint: '',
-	 become_endpoint: '',
-	 log_level: 'info',
-	 gdpr: {
-			 endpoint: 'https://gdpr.travis-ci.com',
-			 auth_token: 'token'
-	 },
-	 slack: {
-	   url: '',
-	   username: "Travis Admin v2 (OSS)",
-	   icon_emoji: ":travis:"
+         host: 'localhost:3000',
+         service_hook_url: '',
+         api_endpoint: '',
+         become_endpoint: '',
+         log_level: 'info',
+         gdpr: {
+           endpoint: 'https://gdpr.travis-ci.com',
+           auth_token: 'token'
          },
-	 settings: {
-	   timeouts: {
-	     maximums: {
+         slack: {
+           url: '',
+           username: 'Travis Admin v2 (OSS)',
+           icon_emoji: ':travis:'
+         },
+         settings: {
+           timeouts: {
+             maximums: {
                hard_limit: 240
              }
            }
          },
-	 yml_checker: { url: '' }
+         yml_checker: { url: '' }
 end
