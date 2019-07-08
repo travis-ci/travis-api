66774a0e315eb1bba83434984b06aaeb2a6b42fe
altimi-repo <eveguras@gmail.com>
2019-07-08 14:56:18 +0200

    Add validation of input for canonical_route and repository actions in UnknownController

--- app/controllers/unknown_controller.rb
+++ app/controllers/unknown_controller.rb
@@ -1,10 +1,11 @@
 class UnknownController < ApplicationController
   def canonical_route
-    redirect_to "/?q=#{params['other']}"
+    valid_login = User.find_by(login: params['other']).present? || Organization.find_by(login: params['other']).present?
+    redirect_to valid_login ? "/?q=#{params['other']}" : :not_found
   end
 
   def repository
-    repository_id = Repository.by_slug("#{params['owner']}/#{params['repo']}").first
-    redirect_to repository_id&.id ? "/repositories/#{repository_id}" : :not_found
+    repository = Repository.by_slug("#{params['owner']}/#{params['repo']}").first
+    redirect_to repository ? "/repositories/#{repository.id}" : :not_found
   end
 end
---
+++ spec/features/unknown_controller_redirects_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature 'Unknown controller redirects spec', js: true, type: :feature do
+  let!(:user)         { create(:user) }
+  let!(:organization) { create(:organization) }
+  let!(:repository)   { create(:repository) }
+
+  describe 'canonical_route' do
+    scenario 'displays /?q=owner for valid owner' do
+      visit "/#{user.login}"
+      expect(page.current_url).to have_text("/?q=#{user.login}")
+    end
+
+    scenario 'redirects to :not_found for invalid user or organization' do
+      visit '/fake_user_organization'
+      expect(page).to have_text('Not Found!')
+    end
+  end
+
+  describe 'repository' do
+    scenario 'displays repository page for valid repository path' do
+      visit "/#{repository.owner_name}/#{repository.name}"
+      expect(page).to have_text("#{repository.name}")
+    end
+
+    scenario 'redirects to :not_found for invalid repository path' do
+      visit '/fake_owner/fake_repo'
+      expect(page).to have_text('Not Found!')
+    end
+  end
+end
