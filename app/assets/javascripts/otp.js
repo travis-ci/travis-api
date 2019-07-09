df64fb1d6a928cf2e93ac2930cbd2f5bb5867c6b
Michał Gajowiak <m.gajowiak@nomtek.com>
2019-07-09 23:22:17 +0200

    Fix typo and double opt popup

--- app/assets/javascripts/otp.js
+++ app/assets/javascripts/otp.js
@@ -3,7 +3,21 @@ $(document).ready(function(){
     $('.otp').on('click', function(ev) {
       ev.preventDefault();
       $('.popup.is-hidden').removeClass('is-hidden');
+
+      if ($('.export').length && $('.purge').length) {
+        var container = ev.currentTarget.parentElement.parentElement.parentElement;
+        var className = container.className;
+
+        if (className === 'export') {
+          $('.purge .popup').addClass('is-hidden');
+        }
+
+        if (className === 'purge') {
+          $('.export .popup').addClass('is-hidden');
+        }
+      }
     });
+
     $('.popup-close').on('click', function() {
       $('.popup').addClass('is-hidden');
     });
