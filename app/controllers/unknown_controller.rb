24d046ce84fb9a4a56c52b92a4d0d7bec5ca3fba
altimi-repo <eveguras@gmail.com>
2019-07-11 14:11:28 +0200

    Fix Build/Job redirects

--- app/controllers/unknown_controller.rb
+++ app/controllers/unknown_controller.rb
@@ -1,7 +1,15 @@
 class UnknownController < ApplicationController
+  def build
+    redirect_to "/builds/#{params['id']}"
+  end
+
   def canonical_route
     valid_login = User.find_by(login: params['other']).present? || Organization.find_by(login: params['other']).present?
     redirect_to valid_login ? "/?q=#{params['other']}" : :not_found
+  end
+
+  def job
+    redirect_to "/jobs/#{params['id']}"
   end
 
   def repository
