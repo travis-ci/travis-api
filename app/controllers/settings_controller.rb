654b4e13b81498587d078a601ac78fbb3d3a48df
altimi-repo <eveguras@gmail.com>
2019-07-12 21:58:11 +0200

    save not supported settings outside of api scope

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,8 +3,9 @@ class SettingsController < ApplicationController
     @repository = Repository.find_by(id: params[:id])
     current_settings = Settings.new(@repository.settings)
     settings = Settings.new(settings_params)
+    not_in_api = %w[timeout_hard_limit timeout_log_silence api_build_rate_limit]
 
-    settings.attributes.each do |setting_name, setting_value|
+    settings.attributes.except(*not_in_api).each do |setting_name, setting_value|
       if current_settings.attributes[setting_name] != setting_value
         response = Services::Settings::Update.new(@repository, setting_name, setting_value).call
 
@@ -14,6 +15,18 @@ class SettingsController < ApplicationController
           flash[:error] = "Error: #{response.headers[:status]}"
           break
         end
+      end
+    end
+
+    settings.attributes.slice(*not_in_api).each do |setting_name, setting_value|
+      if current_settings.attributes[setting_name] != setting_value
+        if setting_name == 'api_build_rate_limit' && setting_value.to_i > 200
+          flash[:warning] = "API builds rate limit can't execeed 200"
+          break
+        end
+        @repository.settings[setting_name] = setting_value.to_i
+        @repository.update!(settings: @repository.settings)
+        flash[:notice] = "Updated settings for #{@repository.slug}"
       end
     end
 
