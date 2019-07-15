e2e2a5ce9c184fd481a51ecf67ebb1e006f66754
altimi-repo <eveguras@gmail.com>
2019-07-15 15:11:45 +0200

    Change routing in canonical route to search controller

--- app/controllers/unknown_controller.rb
+++ app/controllers/unknown_controller.rb
@@ -5,7 +5,7 @@ class UnknownController < ApplicationController
 
   def canonical_route
     valid_login = User.find_by(login: params['other']).present? || Organization.find_by(login: params['other']).present?
-    redirect_to valid_login ? "/?q=#{params['other']}" : :not_found
+    redirect_to valid_login ? "/search/?q=#{params['other']}" : :not_found
   end
 
   def job
