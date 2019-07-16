ffeb6482be66e5f4572efe0f81d3a0f15bcfd574
Piotr Sarnacki <drogus@gmail.com>
2019-07-16 17:25:46 +0200

    Send application/json content type when sending request to merge

--- spec/v3/models/repository_migration_spec.rb
+++ spec/v3/models/repository_migration_spec.rb
@@ -15,7 +15,8 @@ describe Travis::API::V3::Models::RepositoryMigration do
       before { Travis::Features.expects(:owner_active?).with(:allow_migration, repository.owner).returns(true) }
 
       it 'migrates repository' do
-        request = stub_request(:post, %r{/api/repo/by_github_id/\d+/migrate})
+        request = stub_request(:post, %r{/api/repo/by_github_id/\d+/migrate}).
+          with(headers: { 'Content-Type' => 'application/json' })
         subject.migrate!
         expect(request).to have_been_requested
       end
