8beae9910ed3eb9c8dc449c9ec30f3d17c8e3fe4
Sergio Gil <sgilperez@gmail.com>
2019-07-24 15:55:57 +0200

    Expose invoice amount_due

--- lib/travis/api/v3/renderer/invoice.rb
+++ lib/travis/api/v3/renderer/invoice.rb
@@ -1,5 +1,5 @@
 module Travis::API::V3
   class Renderer::Invoice < ModelRenderer
-    representation(:standard, :id, :created_at, :url)
+    representation(:standard, :id, :created_at, :url, :amount_due)
   end
 end
