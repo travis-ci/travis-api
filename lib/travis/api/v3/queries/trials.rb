a26d6a77206d34d4af2fff5a620ecc31520d187c
Piotr Milcarz <piotrm@travis-ci.org>
2019-08-30 15:34:28 +0200

    Activate trial subscription

--- lib/travis/api/v3/queries/trials.rb
+++ lib/travis/api/v3/queries/trials.rb
@@ -4,5 +4,15 @@ module Travis::API::V3
       client = BillingClient.new(user_id)
       client.trials
     end
+
+    def create(user_id)
+      client = BillingClient.new(user_id)
+      login = client.create_trial(params['type'], params['owner'])
+      client.trials.select { |trial| 
+        trial.owner.id.to_s == params['owner'] && 
+        trial.owner.class.to_s.split('::').last.downcase == params['type'] &&
+        trial.owner.login == login
+      }
+    end
   end
 end
