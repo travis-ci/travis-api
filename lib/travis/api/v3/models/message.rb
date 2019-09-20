405a80aed9aa63dc7f0cf7715d978cac07797032
Sven Fuchs <me@svenfuchs.com>
2019-09-20 15:15:58 +0200

    add msg level alert to msg ordering

--- lib/travis/api/v3/models/message.rb
+++ lib/travis/api/v3/models/message.rb
@@ -1,10 +1,11 @@
 module Travis::API::V3
   class Models::Message < Model
-    belongs_to :subject, polymorphic: true  
+    belongs_to :subject, polymorphic: true
 
     scope :ordered, -> do
       order(%Q{
         CASE
+        WHEN level = 'alert' THEN '0'
         WHEN level = 'error' THEN '1'
         WHEN level = 'warn' THEN '2'
         WHEN level = 'info' THEN '3'
