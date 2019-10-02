bb0b1131fb071dae0280e43011bbc94a2751b00a
Michal Gajowiak <michal.gajowiak@softiti.com>
2019-10-02 22:17:52 +0200

    Refactor specs

--- spec/features/organization/update_offender_spec.rb
+++ spec/features/organization/update_offender_spec.rb
@@ -6,15 +6,15 @@ RSpec.feature 'Update Offender', js: true, type: :feature do
   before { visit "/organizations/#{organization.id}" }
 
   scenario 'Update abuse status of organization' do
-    find('#offender_offenders').trigger('click')
+    find('#offender_abuse_offenders').trigger('click')
     find_button('update-abuse-status').trigger('click')
 
     expect(page).to have_text('Abuse settings for Travis (travis-pro) updated.')
-    expect(page.has_checked_field?('offender[offenders]')).to be true
+    expect(page.has_checked_field?('offender_abuse_offenders')).to be true
   end
 
   scenario 'Update offender reason' do
-    find('#offender_offenders').trigger('click')
+    find('#offender_abuse_offenders').trigger('click')
     fill_in('offender_reason', with: 'Test Reason')
     find_button('update-abuse-status').trigger('click')
 
