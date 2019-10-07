205e2698d15ae4b6245d812e6e616c955ff30600
Sophie Orta <sophie@travis-ci.org>
2019-10-07 16:18:52 -0400

    Add support for utm fields to lead create

--- lib/travis/api/v3/services/leads/create.rb
+++ lib/travis/api/v3/services/leads/create.rb
@@ -4,11 +4,11 @@ require 'closeio'
 module Travis::API::V3
   class Services::Leads::Create < Service
     result_type :leads
-    params :name, :email, :team_size, :phone, :message, :utm_source
+    params :name, :email, :team_size, :phone, :message, :lead_source, :utm_fields
 
     def run!
       # Get params
-      name, email, team_size, phone, message, utm_source = params.values_at('name', 'email', 'team_size', 'phone', 'message', 'utm_source')
+      name, email, team_size, phone, message, lead_source, utm_fields = params.values_at('name', 'email', 'team_size', 'phone', 'message', 'lead_source', 'utm_fields')
       team_size = team_size.to_i unless team_size.nil?
       name = name.strip unless name.nil?
       message = message.strip unless message.nil?
@@ -22,23 +22,31 @@ module Travis::API::V3
       # Prep data for request
       api_client = Closeio::Client.new(Travis.config.closeio.key)
       custom_fields = api_client.list_custom_fields
-      team_size_field = custom_fields['data'].find { |field| field['name'] == 'team_size' }
-      utm_source_field = custom_fields['data'].find { |field| field['name'] == 'utm_source' }
+      team_size_field = fetch_custom_field(custom_fields, 'team_size')
+      lead_source_field = fetch_custom_field(custom_fields, 'lead_source')
 
       phones = []
       phones.push({ type: "office", phone: phone }) unless phone.nil?
 
       lead_data = {
         name: name,
-        "custom.#{utm_source_field['id']}": utm_source || 'Travis API',
         contacts: [{
           name: name,
           emails: [{ type: "office", email: email }],
           phones: phones
-        }]
+        }],
+        "custom.#{lead_source_field['id']}": lead_source || 'Travis API',
       }
 
       lead_data["custom.#{team_size_field['id']}"] = team_size if team_size
+
+      # Handle UTM fields
+      supported_utm_fields = ['utm_source', 'utm_campaign', 'utm_medium', 'utm_term', 'utm_content']
+      supported_utm_fields.each do |field_name|
+        field = fetch_custom_field(custom_fields, field_name)
+        field_data = utm_fields[field_name]
+        lead_data["custom.#{field['id']}"] = field_data if field && field_data
+      end
 
       # Send request
       lead = api_client.create_lead(lead_data)
@@ -47,6 +55,12 @@ module Travis::API::V3
       # Return result
       model = Travis::API::V3::Models::Leads.new(lead)
       result model
+    end
+
+    private
+  
+    def fetch_custom_field(custom_fields, field_name)
+      custom_fields['data'].find { |field| field['name'] == field_name }
     end
   end
 end
