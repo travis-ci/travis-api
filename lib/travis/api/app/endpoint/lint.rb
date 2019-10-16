a20cda8371f416493bc5ddafa153bf764ea2e910
Sven Fuchs <me@svenfuchs.com>
2019-10-16 16:32:13 +0200

    make /lint proxy travis-yml

--- lib/travis/api/app/endpoint/lint.rb
+++ lib/travis/api/app/endpoint/lint.rb
@@ -1,19 +1,25 @@
 require 'travis/api/app'
-require 'travis/yaml'
 
 class Travis::Api::App
   class Endpoint
     class Lint < Endpoint
-      def lint
-        request.body.rewind
-        content  = params[:content] || request.body.read
-        parsed   = Travis::Yaml.parse(content)
-        warnings = parsed.nested_warnings.map { |k, m| { key: k, message: m } }
-        { lint: { warnings: warnings } }.to_json
-      end
+      WARNING = /\[(alert|error|warn)\]/
 
       post('/', scope: :public) { lint }
       put('/',  scope: :public) { lint }
+
+      def lint
+        request.body.rewind
+        content = params[:content] || request.body.read
+        query = Travis::API::V3::Queries::Lint.new({}, :lint)
+        msgs = query.lint(content)
+        { lint: { warnings: warnings(msgs) } }.to_json
+      end
+
+      def warnings(msgs)
+        msgs = msgs.select { |msg| WARNING =~ msg }
+        msgs.map { |msg| { key: [], message: msg } }
+      end
     end
   end
 end
---
+++ lib/travis/api/v3/queries/lint.rb
@@ -0,0 +1,56 @@
+require 'json'
+require 'faraday'
+
+module Travis::API::V3
+  class Queries::Lint < RemoteQuery
+    MIME_TYPE = 'application/vnd.travis-ci.configs+json'
+
+    def lint(content)
+      response = client.post([config: content, source: 'lint'])
+      handle_errors(response)
+      body = JSON.parse(response.body)
+      body['full_messages']
+    end
+
+    def client
+      Client.new(config)
+    end
+
+    def handle_errors(response)
+      case response.status
+      when 400 then raise Error, 'bad request'
+      when 500 then raise Error, 'travis-yml application error'
+      end
+    end
+
+    class Client < Struct.new(:config)
+      extend Forwardable
+
+      def post(content)
+        client.post do |r|
+          r.url '/v1/parse'
+          r.headers['Content-Type'] = MIME_TYPE
+          r.body = JSON.dump(content)
+        end
+      end
+
+      private
+
+        def client
+          @client ||= Faraday.new(config[:url], ssl: ssl) do |client|
+            client.basic_auth 'admin', config[:auth_key]
+            client.headers['Accept'] = 'application/json'
+            client.adapter :net_http
+          end
+        end
+
+        def ssl
+          { ca_path: '/usr/lib/ssl/certs' }
+        end
+
+        def config
+          super[:yml]
+        end
+    end
+  end
+end
