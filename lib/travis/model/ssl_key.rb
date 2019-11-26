b1563abdfc9adc79ed2e1ac24ea97a6edd7e0fb9
Hiro Asari <asari.ruby@gmail.com>
2019-11-26 11:35:09 -0500

    Use `OpenSSL::PKey::RSA::PKCS1_OAEP_PADDING` for secrets

--- lib/travis/model/ssl_key.rb
+++ lib/travis/model/ssl_key.rb
@@ -23,10 +23,12 @@ class SslKey < Travis::Model
   end
 
   def encrypt(string)
-    build_key.public_encrypt(string)
+    build_key.public_encrypt(string, OpenSSL::PKey::RSA::PKCS1_OAEP_PADDING)
   end
 
   def decrypt(string)
+    build_key.private_decrypt(string, OpenSSL::PKey::RSA::PKCS1_OAEP_PADDING)
+  rescue OpenSSL::PKey::RSAError
     build_key.private_decrypt(string)
   end
 
