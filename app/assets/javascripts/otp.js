f790db239957dad2e14617d86425e2db8db7a303
Rafael <51964575+rafael-travis@users.noreply.github.com>
2019-11-27 16:39:29 -0300

    Fix reset2fa (#293)

--- app/assets/javascripts/otp.js
+++ app/assets/javascripts/otp.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   if ($('.popup').length) {
     $('.otp').on('click', function(ev) {
       ev.preventDefault();
-      $('.popup.is-hidden').removeClass('is-hidden');
+      $(this).siblings('.popup').removeClass('is-hidden');
 
       if ($('.export').length && $('.purge').length) {
         var container = ev.currentTarget.parentElement.parentElement.parentElement;
