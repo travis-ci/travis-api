1e1e22c1b49b278b50a94914b897d17c858396a9
Piotr Milcarz <piotrm@travis-ci.org>
2019-11-29 15:11:23 +0100

    debug

--- lib/travis/api/v3/access_control/generic.rb
+++ lib/travis/api/v3/access_control/generic.rb
@@ -9,7 +9,7 @@ module Travis::API::V3
 
     def visible?(object, type = nil)
       Travis.logger.info("full_access?: " + full_access?.to_s)
-      Travis.logger.info("access_control.visible?: " + access_control.visible?(object).to_s)
+      Travis.logger.info("dispatch(object, :visible?, type): " + dispatch(object, :visible?, type))
       full_access? or dispatch(object, :visible?, type)
     end
 
