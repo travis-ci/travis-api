7f754659d42651b811d33f9af44b1b3d5eafad92
Piotr Milcarz <piotrm@travis-ci.org>
2019-12-18 17:19:32 +0100

    Logout endpoint for v2

---
+++ lib/travis/api/app/endpoint/logout.rb
@@ -0,0 +1,18 @@
+require 'travis/api/app'
+require 'travis/api/enqueue/services/restart_model'
+require 'travis/api/enqueue/services/cancel_model'
+require 'travis/api/app/responders/base'
+
+class Travis::Api::App
+  class Endpoint
+    class Logout < Endpoint
+      include Helpers::Accept
+
+      before { authenticate_by_mode! }
+
+      get '/' do
+        respond_with current_user if Travis.redis.del("t:#{access_control.token}") == 1
+      end
+    end
+  end
+end
