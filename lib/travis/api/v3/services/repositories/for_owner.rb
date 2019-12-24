86a70b28a1c838fe6c83eca5f255f142db65ea59
Piotr Milcarz <piotrm@travis-ci.org>
2019-12-24 12:11:11 +0100

    ForOwner refactored

--- lib/travis/api/v3/services/repositories/for_owner.rb
+++ lib/travis/api/v3/services/repositories/for_owner.rb
@@ -5,6 +5,10 @@ module Travis::API::V3
     paginate(default_limit: 100)
 
     def run!
+      raise LoginRequired unless access_control.logged_in?
+      raise InstallationMissing unless access_control.user.installation
+      puts '===================== installation ========================='
+      puts access_control.user.installation.to_h
       unfiltered = query.for_owner(find(:owner), user: access_control.user)
       result access_control.visible_repositories(unfiltered)
     end
