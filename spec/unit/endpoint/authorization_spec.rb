b77601a7f94af4d47fc145f9cc63e4af7569cf6a
Pavel Dotsulenko <pavel@travis-ci.com>
2020-01-14 15:32:08 +0200

    Clean up spec

--- spec/unit/endpoint/authorization_spec.rb
+++ spec/unit/endpoint/authorization_spec.rb
@@ -62,16 +62,20 @@ describe Travis::Api::App::Endpoint::Authorization do
             'client_secret' => 'client-secret'}))
           .to_return(status: 200, body: 'access_token=token&token_type=bearer')
 
-        WebMock.stub_request(:get, "https://api.github.com/user?per_page=100").
-          to_return(status: 200, body: JSON.dump(name: 'Piotr Sarnacki', login: 'drogus', gravatar_id: '123', id: 456, foo: 'bar'), headers: {'X-OAuth-Scopes' => 'repo, user, new_scope'})
+        WebMock.stub_request(:get, "https://api.github.com/user?per_page=100")
+          .to_return(
+            status: 200,
+            body: JSON.dump(name: 'Piotr Sarnacki', login: 'drogus', gravatar_id: '123', id: 456, foo: 'bar'), headers: {'X-OAuth-Scopes' => 'repo, user, new_scope'}
+          )
+
+        cookie_jar['travis.state-github'] = state
+        Travis.redis.sadd('github:states', state)
       end
 
       context 'when redirect uri is not allowed' do
         let(:state) { 'github-state:::https://dark-corner-of-web.com/' }
 
-        it 'using wrong redirect_uri' do
-          cookie_jar['travis.state-github'] = state
-          Travis.redis.sadd('github:states', state)
+        it 'does not allow redirect' do
           response = get "/auth/handshake?code=1234&state=#{URI.encode(state)}"
           response.status.should be == 401
           response.body.should be == "target URI not allowed"
@@ -82,9 +86,6 @@ describe Travis::Api::App::Endpoint::Authorization do
         let(:state) { 'github-state:::https://travis-ci.com/<sCrIpt' }
 
         it 'does not allow redirect' do
-          cookie_jar['travis.state-github'] = state
-          Travis.redis.sadd('github:states', state)
-
           response = get "/auth/handshake?code=1234&state=#{URI.encode(state)}"
           response.status.should be == 401
           response.body.should be == "target URI not allowed"
