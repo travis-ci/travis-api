f49ddde74e98be36e0506e8e6fc7e64dbd4f8346
Tahsin Hasan <tahsin@travis-ci.org>
2020-01-20 17:36:34 +0600

    set pgsql lock

--- lib/travis/api/app/schedulers/schedule_cron_jobs.rb
+++ lib/travis/api/app/schedulers/schedule_cron_jobs.rb
@@ -28,24 +28,39 @@ class Travis::Api::App
         }
       end
 
+      def self.pgsql_options
+        @pgsql_options ||= {
+          strategy: :postgresql,
+          try: true,
+          transactional: false
+        }
+      end
+
       def self.enqueue
-        scheduled = Travis::API::V3::Models::Cron.scheduled
-        count = scheduled.count
+        begin
+          Travis::Lock.exclusive("pgsql_enqueue_cron_jobs", pgsql_options) do
+            scheduled = Travis::API::V3::Models::Cron.scheduled
+            count = scheduled.count
 
-        Travis.logger.info "Found #{count} cron jobs to enqueue" if count > 0
-        Metriks.gauge("api.v3.cron_scheduler.upcoming_jobs").set(count)
+            Travis.logger.info "Found #{count} cron jobs to enqueue" if count > 0
+            Metriks.gauge("api.v3.cron_scheduler.upcoming_jobs").set(count)
 
-        scheduled.each do |cron|
-          begin
-            cron.needs_new_build? ? cron.enqueue : cron.skip_and_schedule_next_build
-            Travis.logger.info "Enqueued cron id: #{cron.try(:id)}"
-          rescue => e
-            Metriks.meter("api.v3.cron_scheduler.enqueue.error").mark
-            Raven.capture_exception(e, tags: { 'cron_id' => cron.try(:id) })
-            Travis.logger.error "Cron id: #{cron.try(:id)} with message:#{e.message}"
-            next
+            scheduled.each do |cron|
+              begin
+                cron.needs_new_build? ? cron.enqueue : cron.skip_and_schedule_next_build
+                Travis.logger.info "Enqueued cron id: #{cron.try(:id)}"
+              rescue => e
+                Metriks.meter("api.v3.cron_scheduler.enqueue.error").mark
+                Raven.capture_exception(e, tags: { 'cron_id' => cron.try(:id) })
+                Travis.logger.error "Cron id: #{cron.try(:id)} with message:#{e.message}"
+                next
+              end
+            end
           end
+        rescue => e
+          Travis.logger.error "Pgsql lock error with message:#{e.message}"
         end
+        
       end
     end
   end
