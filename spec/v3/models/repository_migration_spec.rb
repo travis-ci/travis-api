acb42674e00fc4476a82726161219ed01b0d8b6c
Curtis Ekstrom <code@fauxton.io>
2020-02-13 13:02:26 +0100

    Remove irrelevant feature flags
    This was previously failing as part of this bisected result:
    bundle exec rspec './spec/lib/model/job/queue_spec.rb[1:6:1]'
    './spec/v3/models/repository_migration_spec.rb[1:1:1:1,1:1:2:1,1:2:1]'
    './spec/v3/service_index_spec.rb[1:1:1,1:2:3:1:11:4:1]' --seed 50388

--- spec/v3/models/repository_migration_spec.rb
+++ spec/v3/models/repository_migration_spec.rb
@@ -3,15 +3,9 @@ describe Travis::API::V3::Models::RepositoryMigration do
 
   subject { Travis::API::V3::Models::RepositoryMigration.new(repository) }
 
-  before do
-    allow(Travis::Features).to receive(:feature_active?).with(:multi_os).and_return(true)
-    allow(Travis::Features).to receive(:feature_active?).with(:dist_group_expansion).and_return(true)
-    allow(Travis::Features).to receive(:feature_active?).with(:education).and_return(true)
-  end
-
   context 'when migration is enabled globally' do
     context 'when migration is enabled for owner' do
-      before { expect(Travis::Features).to receive(:owner_active?).with(:allow_migration, repository.owner).and_return(true) }
+      before { allow(Travis::Features).to receive(:owner_active?).with(:allow_migration, repository.owner).and_return(true) }
 
       it 'migrates repository' do
         request = stub_request(:post, %r{/api/repo/by_github_id/\d+/migrate}).
@@ -22,7 +16,7 @@ describe Travis::API::V3::Models::RepositoryMigration do
     end
 
     context 'when migration is disabled for owner' do
-      before { expect(Travis::Features).to receive(:owner_active?).with(:allow_migration, repository.owner).and_return(false) }
+      before { allow(Travis::Features).to receive(:owner_active?).with(:allow_migration, repository.owner).and_return(false) }
 
       it 'raises error' do
         expect { subject.migrate! }.to raise_error(Travis::API::V3::Models::RepositoryMigration::MigrationDisabledError)
