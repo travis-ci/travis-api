e50b4b4d0f44f2464bbc694110339c32f7250168
Piotr Milcarz <piotrm@travis-ci.org>
2020-02-14 10:05:43 +0100

    Repository representation in Branch endpoint restored to minimal

--- lib/travis/api/v3/renderer/branch.rb
+++ lib/travis/api/v3/renderer/branch.rb
@@ -4,10 +4,6 @@ module Travis::API::V3
     representation(:standard, :name, :repository, :default_branch, :exists_on_github, :last_build)
     representation(:additional, :recent_builds)
 
-#    def repository
-#      Renderer.render_model(model.repository, mode: :standard)
-#    end
-
     def recent_builds
       return unless include_recent_builds?
       access_control.visible_builds(model.builds.limit(10))
