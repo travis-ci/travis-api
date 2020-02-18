a63060bc222e599b909f6dcc8d3478732ac2ac9e
Curtis Ekstrom <code@fauxton.io>
2020-02-18 10:31:24 +0100

    Fix repository model AR deprecation warnings
    This should be fine to mark as safe as it would be very, very weird for us to allow users to set timestamps, but will make a note to double-check to be sure.

--- lib/travis/model/repository.rb
+++ lib/travis/model/repository.rb
@@ -54,7 +54,7 @@ class Repository < Travis::Model
     end
   }
   scope :timeline, -> {
-    active.order('last_build_finished_at IS NULL AND last_build_started_at IS NOT NULL DESC, last_build_started_at DESC NULLS LAST, id DESC')
+    active.order(Arel.sql('last_build_finished_at IS NULL AND last_build_started_at IS NOT NULL DESC, last_build_started_at DESC NULLS LAST, id DESC'))
   }
   scope :with_builds, -> {
     where(arel_table[:last_build_id].not_eq(nil))
