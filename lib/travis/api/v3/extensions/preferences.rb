12c210993ce31b9c920ee33f2c32ca2c252ea06f
Curtis Ekstrom <code@fauxton.io>
2020-03-03 11:19:42 +0100

    Tentative commit, not sure if this is correct

--- lib/travis/api/v3/extensions/preferences.rb
+++ lib/travis/api/v3/extensions/preferences.rb
@@ -1,10 +1,15 @@
+require 'json'
+
 module Travis::API::V3
   module Extensions
     module Preferences
       module ClassMethods
         def has_preferences(klass, column: :preferences, method_name: :preferences)
           define_method method_name do
-            klass.new(self[column]).tap { |prefs| prefs.sync(self, column) }
+            # Try to fix setting nil attributes
+            data = self[column] if column.is_a?(Hash)
+            data = JSON.parse(self[column]) if column.is_a?(String)
+            klass.new(self[data]).tap { |prefs| prefs.sync(self, data) }
           end
         end
       end
