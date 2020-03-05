ab286ed5643865db5c8e727e67e8d8e362a28b47
Piotr Milcarz <piotrm@travis-ci.org>
2020-03-05 19:15:13 +0100

    branch fix

--- lib/travis/api/v3/remote_query.rb
+++ lib/travis/api/v3/remote_query.rb
@@ -62,7 +62,6 @@ module Travis::API::V3
       objects = []
       s3_objects.each { |object| objects << object } if s3_config
       gcs_objects.each { |object| objects << object } if gcs_config
-      puts "objects in repote_query: #{objects}"
       objects
     end
 
