09cbaa4b0fb9ca61a0de3cafabd7b622492a15ee
gabriel-arc <57348209+GbArc@users.noreply.github.com>
2020-03-09 09:03:41 +0100

    otp fix for multiple popups on page (#309)

--- app/assets/javascripts/otp.js
+++ app/assets/javascripts/otp.js
@@ -2,8 +2,14 @@ $(document).ready(function(){
   if ($('.popup').length) {
     $('.otp').on('click', function(ev) {
       ev.preventDefault();
-      $(this).siblings('.popup').removeClass('is-hidden');
-      $('.popup.is-hidden').removeClass('is-hidden');
+      // checking for siblings - lame workaround for gdpr otp vs admin list otp
+      // if has siblings(admin page) need to use it otherwise last popup o page will be shown
+      if ($(this).siblings('.popup').length) {
+        $(this).siblings('.popup').removeClass('is-hidden');
+      }
+      else {
+        $('.popup.is-hidden').removeClass('is-hidden');
+      }
       if ($('.export').length && $('.purge').length) {
         var container = ev.currentTarget.parentElement.parentElement.parentElement;
         var className = container.className;
