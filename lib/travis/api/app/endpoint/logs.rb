82933d6000351a9d2f072d637e2fa82e56f92f77
Hiro Asari <asari.ruby@gmail.com>
2020-03-12 11:33:15 -0400

    Fix accepts MIME-type string

--- lib/travis/api/app/endpoint/logs.rb
+++ lib/travis/api/app/endpoint/logs.rb
@@ -18,7 +18,7 @@ class Travis::Api::App
           # automatically here, so we need to check it explicitly
           if accepts?('text/plain') || request.user_agent.to_s.start_with?('Travis')
             redirect resource.archived_url, 307
-          elsif accepts?('application.json') || accepts?('application/vnd.travis-ci.2+json')
+          elsif accepts?('application/json') || accepts?('application/vnd.travis-ci.2+json')
             {
               "log" => {
                 "id" => resource.id,
