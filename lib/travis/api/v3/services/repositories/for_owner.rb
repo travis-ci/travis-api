d0b01992b2688ea7a8286c9f70833bf3e47cf0a0
Piotr Milcarz <piotrm@travis-ci.org>
2020-03-17 10:45:00 +0100

    debug

--- lib/travis/api/v3/services/repositories/for_owner.rb
+++ lib/travis/api/v3/services/repositories/for_owner.rb
@@ -6,6 +6,7 @@ module Travis::API::V3
 
     def run!
       unfiltered = query.for_owner(find(:owner), user: access_control.user)
+      puts "owner: #{find(:owner).inspect} unfiltered: #{unfiltered.inspect}"
       result access_control.visible_repositories(unfiltered)
     end
   end
