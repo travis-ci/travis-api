9d6cafeb8deb5cd0e0b24c5a1311ffbb7a776b46
gabriel-arc <57348209+GbArc@users.noreply.github.com>
2020-03-24 11:26:41 +0100

    Add builds to ended trials (#313)
    Feature requested in https://travisci.assembla.com/spaces/admin-v2/tickets/23

--- app/models/trial.rb
+++ app/models/trial.rb
@@ -2,5 +2,5 @@ class Trial < ActiveRecord::Base
   belongs_to :owner, polymorphic: true
   has_many :trial_allowances
 
-  scope :underway, -> { where(status: %w{new started}) }
+  scope :underway, -> { where(status: %w{new started ended}) } #'ended' added due to possibility to add trials to expired license (feature was requested )
 end
