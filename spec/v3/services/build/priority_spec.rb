913d3a5d8668268d8f698c322800a296d8758e7e
Debasish <dmaity@celestialsys.com>
2020-05-30 10:45:55 +0530

    change code with proper format

--- spec/v3/services/build/priority_spec.rb
+++ spec/v3/services/build/priority_spec.rb
@@ -1,7 +1,7 @@
 describe Travis::API::V3::Services::Build::Priority, set_app: true do
   let(:repo) { Travis::API::V3::Models::Repository.where(owner_name: 'svenfuchs', name: 'minimal').first }
   let(:build) { repo.builds.first }
-  let(:payload) { { 'id'=> "#{build.id}", 'user_id' => 1, 'source' => 'api' } }
+  let(:payload) { { "id"=> "#{build.id}", "user_id" => 1, "source" => "api" } }
 
   before do
     allow(Travis::Features).to receive(:owner_active?).and_return(true)
@@ -17,42 +17,42 @@ describe Travis::API::V3::Services::Build::Priority, set_app: true do
   end
 
   describe "not authenticated" do
-    before  { post("/v3/build/#{build.id}/priority")      }
+    before { post("/v3/build/#{build.id}/priority") }
     example { expect(last_response.status).to be == 403 }
-    example { expect(JSON.load(body)).to      be ==     {
+    example { expect(JSON.load(body)).to be == {
       "@type"         => "error",
       "error_type"    => "login_required",
       "error_message" => "login required"
-    }}
+    } }
   end
 
   describe "missing build, authenticated" do
     let(:token)   { Travis::Api::App::AccessToken.create(user: repo.owner, app_id: 1) }
-    let(:headers) {{ 'HTTP_AUTHORIZATION' => "token #{token}"                        }}
-    before        { post("/v3/build/9999999999/priority", {}, headers)                 }
+    let(:headers) { { "HTTP_AUTHORIZATION" => "token #{token}" } }
+    before        { post("/v3/build/9999999999/priority", {}, headers) }
 
     example { expect(last_response.status).to be == 404 }
-    example { expect(JSON.load(body)).to      be ==     {
+    example { expect(JSON.load(body)).to be == {
       "@type"         => "error",
       "error_type"    => "not_found",
       "error_message" => "build not found (or insufficient access)",
       "resource_type" => "build"
-    }}
+    } }
   end
 
   describe "private repository, no access" do
-    let(:token)   { Travis::Api::App::AccessToken.create(user: repo.owner, app_id: 1) }
-    let(:headers) {{ 'HTTP_AUTHORIZATION' => "token #{token}"                        }}
-    before        { repo.update_attribute(:private, true)                             }
-    before        { post("/v3/build/#{build.id}/priority", {}, headers)                 }
-    after         { repo.update_attribute(:private, false)                            }
+    let(:token) { Travis::Api::App::AccessToken.create(user: repo.owner, app_id: 1) }
+    let(:headers) { { "HTTP_AUTHORIZATION" => "token #{token}" } }
+    before { repo.update_attribute(:private, true) }
+    before { post("/v3/build/#{build.id}/priority", {}, headers) }
+    after { repo.update_attribute(:private, false) }
 
     example { expect(last_response.status).to be == 404 }
-    example { expect(JSON.load(body)).to      be ==     {
+    example { expect(JSON.load(body)).to be == {
       "@type"         => "error",
       "error_type"    => "not_found",
       "error_message" => "build not found (or insufficient access)",
       "resource_type" => "build"
-    }}
+    } }
   end
 end
