17ecfec43d288379f17fb833a69bc6972c6c387b
Debasish <dmaity@celestialsys.com>
2020-05-31 13:09:42 +0530

    Removed comments from priority.rb file

--- lib/travis/api/v3/services/build/priority.rb
+++ lib/travis/api/v3/services/build/priority.rb
@@ -2,8 +2,6 @@ module Travis::API::V3
   class Services::Build::Priority < Service
 
     def run
-      #Need to add code for permission
-      #Add code to set query from here.
       build = check_login_and_find(:build)
       accepted(build: build, priority: true)
     end
