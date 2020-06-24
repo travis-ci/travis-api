7b5977e99c7413990813d87fcac4e55faa994999
Tahsin Hasan <51903216+Tahsin-travis-ci@users.noreply.github.com>
2020-06-24 23:51:35 +0600

    logout for enterprise (#1119)

---
+++ spec/v3/services/user/logout_spec.rb
@@ -0,0 +1,12 @@
+describe Travis::API::V3::Services::User::Logout, set_app: true do
+    let(:user) { Travis::API::V3::Models::User.find_by_login('svenfuchs') }
+  
+    let(:token)   { Travis::Api::App::AccessToken.create(user: user, app_id: 1) }
+    let(:headers) {{ 'HTTP_AUTHORIZATION' => "token #{token}"                  }}
+  
+    describe "logout user" do
+      before  { get("/v3/logout", {}, headers)    }
+      before  { get("/v3/user", {}, headers)      }
+      example { puts last_response; expect(last_response).not_to be_ok }
+    end
+end
