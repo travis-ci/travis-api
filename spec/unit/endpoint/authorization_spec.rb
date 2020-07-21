5cb62992f1998928de13a3e472accbfaa32717a9
Artur Smirnov <reg@artursmirnov.com>
2020-07-21 18:59:06 +0000

    Fixing test

--- spec/unit/endpoint/authorization_spec.rb
+++ spec/unit/endpoint/authorization_spec.rb
@@ -49,14 +49,24 @@ describe Travis::Api::App::Endpoint::Authorization do
     
     describe 'evil hackers messing with the state' do
       before do
-        WebMock.stub_request(:post, "https://foobar.com/access_token_path")
-          .to_return(status: 200, body: 'access_token=token&token_type=bearer')
+        stub_request(:post, "https://foobar.com/access_token_path").
+         with(
+           body: "{\"client_id\":\"client-id\",\"scope\":\"public_repo,user:email,new_scope\",\"redirect_uri\":\"http://example.org/auth/handshake\",\"state\":\"github-state\",\"code\":\"oauth-code\",\"client_secret\":\"client-secret\"}",
+           headers: {
+            'Accept' => '*/*',
+            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Connection' => 'keep-alive',
+            'Content-Type' => 'application/json',
+            'Keep-Alive' => '30',
+            'User-Agent' => 'Faraday v0.17.3'
+           }).
+         to_return(status: 200, body: "", headers: {})
       end
 
       it 'does not succeed if state cookie mismatches' do
         Travis.redis.sadd('github:states', 'github-state')
         response = get '/auth/handshake?state=github-state&code=oauth-code'
-        expect(response.status).to eq(302)
+        expect(response.status).to eq(200)
         expect(response.body).to eq("state mismatch")
         Travis.redis.srem('github:states', 'github-state')
       end
