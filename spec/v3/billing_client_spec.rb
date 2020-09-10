c19e2111fd46743ed22115aa93f734d4606b0e02
Piotr Milcarz <piotr.milcarz@gmail.com>
2020-09-10 09:32:30 +0200

    V2 Subscription - change to free plan

--- spec/v3/billing_client_spec.rb
+++ spec/v3/billing_client_spec.rb
@@ -232,6 +232,20 @@ describe Travis::API::V3::BillingClient, billing_spec_helper: true do
     end
   end
 
+  describe '#changetofree_v2_subscription' do
+    let(:plan_data) { { 'plan' => 'enterprise_tier_plan' } }
+    subject { billing.changetofree_v2_subscription(subscription_id, plan_data) }
+
+    it 'requests the update' do
+      stubbed_request = stub_billing_request(:patch, "/v2/subscriptions/#{subscription_id}/changetofree", auth_key: auth_key, user_id: user_id)
+        .with(body: JSON.dump(plan_data))
+        .to_return(status: 204)
+
+      expect { subject }.to_not raise_error
+      expect(stubbed_request).to have_been_made
+    end
+  end
+
   describe '#update_v2_subscription' do
     let(:plan_data) { { 'plan' => 'enterprise_tier_plan' } }
     subject { billing.update_v2_subscription(subscription_id, plan_data) }
