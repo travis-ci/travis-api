487f9d8446a2a2f87669091eb27a659d1db7b189
Pavel Dotsulenko <pavel@travis-ci.com>
2020-10-15 13:00:16 +0300

    Updates for change request

--- lib/travis/api/v3/models/v2_subscription.rb
+++ lib/travis/api/v3/models/v2_subscription.rb
@@ -2,7 +2,8 @@ module Travis::API::V3
   class Models::V2Subscription
     include Models::Owner
 
-    attr_reader :id, :plan, :permissions, :source, :billing_info, :credit_card_info, :owner, :client_secret, :payment_intent, :addons, :available_standalone_addons, :created_at
+    attr_reader :id, :plan, :permissions, :source, :billing_info, :credit_card_info, :owner, :status, :valid_to, :canceled_at,
+                :client_secret, :payment_intent, :addons, :available_standalone_addons, :created_at
 
     def initialize(attributes = {})
       @id = attributes.fetch('id')
@@ -16,6 +17,9 @@ module Travis::API::V3
       @client_secret = attributes.fetch('client_secret')
       @addons = attributes['addons'].select { |addon| addon['current_usage']['status'] != 'expired' }.map { |addon| Models::V2Addon.new(addon) }
       @created_at = attributes.fetch('created_at')
+      @status = attributes.fetch('status')
+      @valid_to = attributes.fetch('valid_to')
+      @canceled_at = attributes.fetch('canceled_at')
     end
   end
 
