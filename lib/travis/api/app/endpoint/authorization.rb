f0f50eb97c07e00bb9514cedaea2cf6a32150a41
Piotr Milcarz <piotr.milcarz@gmail.com>
2020-10-16 14:42:29 +0200

    create_initial_subscription removed (#1155)
    create_initial_subscription removed

--- lib/travis/api/app/endpoint/authorization.rb
+++ lib/travis/api/app/endpoint/authorization.rb
@@ -106,7 +106,6 @@ class Travis::Api::App
       get '/handshake/?:provider?' do
         method = org? ? :handshake : :vcs_handshake
         params[:provider] ||= 'github'
-
         send(method) do |user, token, redirect_uri|
           if target_ok? redirect_uri
             content_type :html
@@ -178,7 +177,6 @@ class Travis::Api::App
             token                  = generate_token(user: user, app_id: 0)
             payload                = params[:state].split(":::", 2)[1]
             update_first_login(user)
-            user.create_initial_subscription
             yield serialize_user(user), token, payload
           else
             values[:state]         = create_state
@@ -213,7 +211,6 @@ class Travis::Api::App
 
             user = User.find(vcs_data['user']['id'])
             update_first_login(user)
-            user.create_initial_subscription
             yield serialize_user(user), vcs_data['token'], payload(params[:provider])
           else
             state = vcs_create_state(params[:origin] || params[:redirect_uri])
--- lib/travis/model/user.rb
+++ lib/travis/model/user.rb
@@ -170,13 +170,6 @@ class User < Travis::Model
     vcs_type == 'GithubUser'
   end
 
-  def create_initial_subscription
-    return if Travis.config.org?
-
-    client = Travis::API::V3::BillingClient.new(id)
-    client.create_initial_v2_subscription
-  end
-
   protected
 
     def track_previous_changes
