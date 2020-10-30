0d46d56204635c78f015374fa5d241c1d8deab42
Piotr Milcarz <piotr.milcarz@gmail.com>
2020-10-30 11:26:45 +0100

    default values in allowance model

--- lib/travis/api/v3/models/allowance.rb
+++ lib/travis/api/v3/models/allowance.rb
@@ -5,11 +5,11 @@ module Travis::API::V3
     def initialize(subscription_type, owner_id, attributes = {})
       @subscription_type = subscription_type
       @id = owner_id
-      @public_repos = attributes.fetch('public_repos')
-      @private_repos = attributes.fetch('private_repos')
-      @concurrency_limit = attributes.fetch('concurrency_limit')
-      @user_usage = attributes.fetch('user_usage')
-      @pending_user_licenses = attributes.fetch('pending_user_licenses')
+      @public_repos = attributes.fetch('public_repos', nil)
+      @private_repos = attributes.fetch('private_repos', nil)
+      @concurrency_limit = attributes.fetch('concurrency_limit', nil)
+      @user_usage = attributes.fetch('user_usage', nil)
+      @pending_user_licenses = attributes.fetch('pending_user_licenses', nil)
     end
   end
 end
