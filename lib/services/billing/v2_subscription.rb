03289ccdddc9b1d0db226631f728f2980eb670ab
travis-architect <72377796+travis-architect@users.noreply.github.com>
2021-03-31 11:45:30 +0500

    Added new config key for billing request block (#353)

--- lib/services/billing/v2_subscription.rb
+++ lib/services/billing/v2_subscription.rb
@@ -11,25 +11,35 @@ module Services
       end
 
       def subscriptions
+        return unless travis_pro?
+
         @client.v2_subscriptions(@owner_id)
       end
 
       def subscription
+        return unless travis_pro?
+
         subscriptions = @client.v2_subscriptions(@search_by_owner_id) if @search_by_owner_id.present?
 
         subscriptions.select { |subscription| subscription.owner_id == @owner_id.to_i && subscription.owner_type == @owner_type }.first  if @search_by_owner_id.present?
       end
 
       def invoices
+        return unless travis_pro?
+
         sub = subscription
         sub ? @client.v2_invoices(@search_by_owner_id.to_i, sub.id) : [] if @search_by_owner_id.present?
       end
 
       def plans
+        return unless travis_pro?
+
         @client.v2_plans(@search_by_owner_id.to_s) if @search_by_owner_id.present?
       end
 
       def update_subscription(id, attributes)
+        return unless travis_pro?
+
         @client.update_v2_subscription(@search_by_owner_id, id, attributes) if @search_by_owner_id.present?
 
         nil
@@ -38,15 +48,27 @@ module Services
       end
 
       def create_subscription(attributes)
+        return unless travis_pro?
+
         @client.create_v2_subscription(@search_by_owner_id, attributes) if @search_by_owner_id.present?
       end
 
       def create_addon(id, attributes)
+        return unless travis_pro?
+
         @client.create_v2_addon(@search_by_owner_id, id, attributes) if @search_by_owner_id.present?
 
         nil
       rescue => e
         e.message
+      end
+
+      def travis_pro?
+        travis_config&.travis_pro
+      end
+
+      def travis_config
+        Rails.configuration.travis_config
       end
     end
   end
