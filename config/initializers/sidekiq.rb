3d3744a8e50bd56ab5106af7493b49a8744418fe
Artur Kremens <artur.kremens@gmail.com>
2021-03-31 08:46:16 +0200

    Add additional Sidekiq config after rails update

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,10 +4,10 @@ if Rails.env.production?
   travis_config = TravisConfig.load
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: travis_config.redis.url }
+    config.redis = { url: travis_config.redis.url, id: nil }
   end
 
   Sidekiq.configure_server do |config|
-    config.redis = { url: travis_config.redis.url }
+    config.redis = { url: travis_config.redis.url, id: nil }
   end
 end
