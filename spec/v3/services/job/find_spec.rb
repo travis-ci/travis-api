c73e9ee401cf2082effa5854e1d47af31f5eb16a
gabriel-arc <57348209+GbArc@users.noreply.github.com>
2021-04-21 14:55:12 +0200

    gem updates (#1192)

--- spec/v3/services/job/find_spec.rb
+++ spec/v3/services/job/find_spec.rb
@@ -324,7 +324,7 @@ describe Travis::API::V3::Services::Job::Find, set_app: true do
           'Authorization'=>'token notset',
           'Connection'=>'keep-alive',
           'Keep-Alive'=>'30',
-          'User-Agent'=>'Faraday v0.17.3'
+          'User-Agent'=>'Faraday v0.17.4'
            }).
          to_return(status: 200, body: "{}", headers: {})
     end
@@ -346,7 +346,7 @@ describe Travis::API::V3::Services::Job::Find, set_app: true do
           'Authorization'=>'token notset',
           'Connection'=>'keep-alive',
           'Keep-Alive'=>'30',
-          'User-Agent'=>'Faraday v0.17.3'
+          'User-Agent'=>'Faraday v0.17.4'
            }).
          to_return(status: 200, body: "{}", headers: {})
 
