b041387e41361cf3bc19b72d870655ace13e2846
AndriiMysko <usandrewmisko@gmail.com>
2021-12-23 17:53:50 +0200

    Add artifacts image config warnings

--- lib/travis/api/v3/renderer/artifacts_config.rb
+++ lib/travis/api/v3/renderer/artifacts_config.rb
@@ -2,7 +2,7 @@ module Travis::API::V3
   module Renderer::ArtifactsConfig
     extend self
 
-    AVAILABLE_ATTRIBUTES = [:id, :image_name, :is_valid, :is_pushed, :push_sha]
+    AVAILABLE_ATTRIBUTES = [:id, :image_name, :is_valid, :is_pushed, :push_sha, :warnings]
 
     def available_attributes
       AVAILABLE_ATTRIBUTES
@@ -15,7 +15,8 @@ module Travis::API::V3
         image_name: object['image_name'],
         is_valid: object['is_valid'],
         is_pushed: object['is_pushed'],
-        push_sha: object['push_sha']
+        push_sha: object['push_sha'],
+        warnings: object['warnings']
       }
     end
   end
