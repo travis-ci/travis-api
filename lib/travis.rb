f84538e8fa75d1eb6080595b54c2d43ed1285697
gabriel-arc <gabriel@travis-ci.org>
2022-09-13 15:59:30 +0200

    ship patches

--- lib/travis.rb
+++ lib/travis.rb
@@ -25,6 +25,7 @@ module Travis
   require 'travis/github'
   require 'travis/notification'
   require 'travis/services'
+  require 'patches/gh/token_check'
 
   class UnknownRepository < StandardError; end
   class GithubApiError    < StandardError; end
