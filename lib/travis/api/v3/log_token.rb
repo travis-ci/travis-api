952cfd15754e38c8e237f6e5e943cbd02faa5c76
AndriiMysko <usandrewmisko@gmail.com>
2022-10-10 14:43:17 +0300

    Fix log token working with new log settings

--- lib/travis/api/v3/log_token.rb
+++ lib/travis/api/v3/log_token.rb
@@ -1,14 +1,17 @@
 module Travis::API::V3
   class LogToken
-    attr_accessor :job_id
+    attr_accessor :job_id, :repo_can_write
 
     def self.find(token)
-      new(redis.get("l:#{token}").to_i)
+      values = redis.smembers("l:#{token}")
+      new(values[0].to_i, !!values[1])
     end
 
-    def self.create(job)
+    def self.create(job, user_id)
+      repo_can_write = !!job.repository.users.where(id: user_id, permissions: { push: true }).first
+
       token = SecureRandom.urlsafe_base64(16)
-      redis.set("l:#{token}", job.id)
+      redis.sadd("l:#{token}", [job.id, repo_can_write])
       redis.expire("l:#{token}", 1.day)
       token
     end
@@ -17,8 +20,9 @@ module Travis::API::V3
       Travis.redis
     end
 
-    def initialize(job_id)
+    def initialize(job_id, repo_can_write)
       self.job_id = job_id
+      self.repo_can_write = repo_can_write
     end
 
     def matches?(job)
--- lib/travis/api/v3/services/log/find.rb
+++ lib/travis/api/v3/services/log/find.rb
@@ -4,9 +4,14 @@ module Travis::API::V3
 
     def run!
       job = Models::Job.find(params['job.id'])
-      repo_can_write = access_control.user ? !!job.repository.users.where(id: access_control.user.id, permissions: { push: true }).first : false
-
       log = query.find(job)
+      repo_can_write = false
+      if access_control.is_a?(Travis::API::V3::AccessControl::LogToken)
+        repo_can_write = access_control.repo_can_write
+      elsif access_control.user
+        repo_can_write = !!job.repository.users.where(id: access_control.user.id, permissions: { push: true }).first
+      end
+
       raise(NotFound, :log) unless access_control.visible? log
       raise LogExpired if job.repository.user_settings.job_log_time_based_limit && job.started_at && job.started_at < Time.now - job.repository.user_settings.job_log_access_older_than_days.days
       raise LogAccessDenied if job.repository.user_settings.job_log_access_based_limit && !repo_can_write
