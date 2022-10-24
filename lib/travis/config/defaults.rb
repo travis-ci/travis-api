c76149a7ba8ee5e196fe103970183a99ea1a7260
Andrii Mysko <usandrewmisko@gmail.com>
2022-10-24 11:23:21 +0300

    Active Scanning and Replacing (AWS) Tokens

--- lib/travis/config/defaults.rb
+++ lib/travis/config/defaults.rb
@@ -41,13 +41,9 @@ module Travis
             auth:                 { target_origin: nil },
             assets:               { host: HOSTS[Travis.env.to_sym] },
             amqp:                 { username: 'guest', password: 'guest', host: 'localhost', prefetch: 1 },
-            billing:              {},
             closeio:              { key: 'key' },
             gdpr:                 {},
-            insights:             { endpoint: 'https://insights.travis-ci.dev/', auth_token: 'secret' },
             database:             { adapter: 'postgresql', database: "travis_#{Travis.env}", encoding: 'unicode', min_messages: 'warning', variables: { statement_timeout: 10_000 } },
-            fallback_logs_api:    { url: fallback_logs_api_auth_url, token: fallback_logs_api_auth_token },
-            logs_api:             { url: logs_api_url, token: logs_api_auth_token },
             db:                   { max_statement_timeout_in_seconds: 15, slow_host_max_statement_timeout_in_seconds: 60},
             log_options:          { s3: { access_key_id: '', secret_access_key: ''}},
             s3:                   { access_key_id: '', secret_access_key: ''},
@@ -86,10 +82,15 @@ module Travis
             build_backup_options: ENV['GCE_BUILD_BACKUP_OPTIONS'] ? JSON.parse(ENV['GCE_BUILD_BACKUP_OPTIONS']) : { gcs: { bucket_name: 'fillme', json_key: JSON.parse("{\n  \"type\": \"service_account\",\n  \"project_id\": \"fillme\",\n  \"private_key_id\": \"b1c57117b4a0b8ae2af2f45b19a1cf9727bc6caf\",\n  \"private_key\": \"-----BEGIN PRIVATE KEY-----\\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDRaHA5z0vXNVSr\\nlLVd/smJpNkpzk4BoHq+zcuuzvKTf1ZY1LnrAhldUbDTKY67c06eOYwVrQc3tEIp\\nJCNhIDNY1lRfGJag6t2v5C710WY+X7qVnRhSpgthvWFX/Rm9KIv3be8AJvUTMUDQ\\nAL10eYrIWJOI/J59khuKvr7khIlysASwGoZc8UgcufxGuwCziNyEIfH1nxiBKILR\\nM1+LdYi/Avyb4bQth5x03THVEmdRDjV7Yoo2c17XElIXtkl03nUce4w8BCu+T1G2\\nKRkApHcQ8R0BDTjjBzaQuXTtTpLvkmZzJ1i0/kPNdnT70lv6N2nI1AN2DVkXDCkG\\nNlZ283W7AgMBAAECggEAC/W6CyMywqzSFCafISovGoRmvsOAowkmWYVpb6d0JUZt\\niQ9FOw3YowLKZZUHCN+yCslgndBPDDhoWu8schyjshwzn2bJG5GubaBLqlB2VXOk\\nNW1OeVHwbnmheKQE90+8hropn0maT6lNeVPBfkh+y6h7bKR47NUOa6MvRd/n9bvL\\nT7pP5ZAHoPoTcbUftOX0gDq0u+uRULe/rduxB0S2EHDEtZEH+ioUOP9AomnaRDSy\\n0spH1s2FUZxIbKBQzsrqMCai4MSjeUrJMTR3ZlpfXePirettvilSWqEXDLvvwaak\\ngehELuM5lH4T49wf4PmEYZ8Jqkh9ku+oNYJdJvGR2QKBgQD7KjhJx9usl0afrIH3\\nw7saHELluWGqHNa6j+TJDpY7N5lLLIym/br9d+cuLTF5CBEHJ502coDR9cyrLVZX\\na05CGmEfSVrSrLUyAU+mHHdsn8n6CCATmlgtPyzzt2c29J7dHUZL94zW/yG6Btg6\\nm+nY4eBKreLpj0+3KbhI/q0q1wKBgQDVcG8Ek3Kt2buOrpDBqxcwB31QljntT+7+\\nYcTZctYL/y7Lm2VcTjserNa3AjG59Z5iaQjKFPhbAvMHfppklyiVSVBRfn4bLTcx\\nSM9I+lntODtGI/BiHVE7hfoYKzwz/3Aj3npiOO9xnOfAgEubGn9DrOzLXPsvWN7E\\nz+/iSr4zvQKBgHFVB7kjGZizWgbKzIqEI3UQs479K3ibMrlUHKQslNV7rQwiugTQ\\nEQQ2inZnph866JQV5/adjEsxYn0LJB6mKNXjGVgIvZa6n7hEpzAJQEoff//2kqLF\\nzmv8SchfRY+iqdyUTRgSR9broMhUNlWb7NUUdyS7edxx8kJv7NvjLzhZAoGAMre9\\n2bOD26XSeKwof6y9HM+ayox4BVkqLE5lLVqpXD5uCznI0y9PwxFFEEW4NT0VPsNA\\nsGxdO5suzsgZve9hWGAMcuEA7EpJRC/N+cRrm//xrdAabeYTiHZkoFudualoJ03V\\nfQOUekXTmB2kWZ3pQdaUihp1IaIXhWL32Kj0G20CgYAfGInHUZf5mqxRK/id8B5Q\\nYmpnLDXsNu1I4qFKeaBo4dF2SGByNW7fVbK/BdCSg5Ov3Ui6m3QWBbJXh1a8QVYA\\nICvwJWqm53bNpocrFPAeXLy9xL5/5CEeVGQNcxFvUF3QgaPVmjbTsZk8vjbidUZk\\nOU8bArrUjGTxNJOe7GebhA==\\n-----END PRIVATE KEY-----\\n\",\n  \"client_email\": \"service@fillme.iam.gserviceaccount.com\",\n  \"client_id\": \"100937792194965642651\",\n  \"auth_uri\": \"https://accounts.google.com/o/oauth2/auth\",\n  \"token_uri\": \"https://oauth2.googleapis.com/token\",\n  \"auth_provider_x509_cert_url\": \"https://www.googleapis.com/oauth2/v1/certs\",\n  \"client_x509_cert_url\": \"https://www.googleapis.com/robot/v1/metadata/x509/service%40fillme.iam.gserviceaccount.com\"\n}\n") } },
             merge:                { auth_token: 'merge-auth-token', api_url: 'https://merge.localhost' },
             force_authentication: false,
-            yml:                  { url: 'https://yml.travis-ci.org', token: 'secret', auth_key: 'abc123' },
             read_only:            ENV['READ_ONLY'] || false,
-            vcs: {},
-            job_log_access_permissions: { time_based_limit: false, access_based_limit: false, older_than_days: 365, max_days_value: 730, min_days_value: 30 }
+            job_log_access_permissions: { time_based_limit: false, access_based_limit: false, older_than_days: 365, max_days_value: 730, min_days_value: 30 },
+            billing:              {},
+            vcs:                  {},
+            yml:                  { url: 'https://yml.travis-ci.org', token: 'secret', auth_key: 'abc123' },
+            logs_api:             { url: logs_api_url, token: logs_api_auth_token },
+            fallback_logs_api:    { url: fallback_logs_api_auth_url, token: fallback_logs_api_auth_token },
+            scanner:              {},
+            insights:             { endpoint: 'https://insights.travis-ci.dev/', auth_token: 'secret' }
 
     default :_access => [:key]
 
---
+++ spec/v3/services/scan_result/find_spec.rb
@@ -0,0 +1,98 @@
+describe Travis::API::V3::Services::ScanResult::Find, set_app: true, scanner_spec_helper: true do
+  let(:parsed_body) { JSON.load(last_response.body) }
+  let(:scanner_url) { 'http://scanner.travis-ci.com' }
+  let(:scanner_auth_key) { 'secret' }
+  let(:scan_result_id) { 1 }
+
+  before do
+    Travis.config.scanner.url = scanner_url
+    Travis.config.scanner.token = scanner_auth_key
+  end
+
+  context 'unauthenticated' do
+    it 'responds 403' do
+      get('/v3/scan_results')
+
+      expect(last_response.status).to eq(403)
+    end
+  end
+
+  context 'authenticated' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:repository) { FactoryBot.create(:repository) }
+    let(:job) { FactoryBot.create(:job) }
+    let(:token) { Travis::Api::App::AccessToken.create(user: user, app_id: 1) }
+    let(:headers) {{ 'HTTP_AUTHORIZATION' => "token #{token}" }}
+    let(:offset) { '0' }
+    let(:limit) { '25' }
+    let(:expected_json) do
+      {
+        "@type"=>"scan_result",
+        "@representation"=>"standard",
+        "id"=>1,
+        "created_at"=>"2022-10-20T08:55:20.522Z",
+        "formatted_content"=>
+         "travis_fold:start:trivy\r\e[0K\e[33;1mIn line 1 of your build job log trivy found\e[0m\n" +
+         "AWS Access Key ID\n" +
+         "travis_fold:end:trivy\n" +
+         "\n" +
+         "\n" +
+         "travis_fold:start:detect_secrets\r\e[0K\e[33;1mIn line 1 of your build job log detect_secrets found\e[0m\n" +
+         "AWS Access Key\n" +
+         "travis_fold:end:detect_secrets\n" +
+         "\n" +
+         "\n" +
+         "\n" +
+         "\n" +
+         "Our backend build job log monitoring uses:\n" +
+         " \u2022 trivy\n" +
+         " \u2022 detect_secrets\n" +
+         "Called via command line and under respective permissive licenses.",
+        "issues_found"=>1,
+        "job_id"=>job.id,
+        "build_id"=>nil,
+        "job_number"=>"1",
+        "build_number"=>"1",
+        "job_finished_at"=>nil,
+        "commit_sha"=>"SHA",
+        "commit_compare_url"=>nil,
+        "commit_branch"=>nil,
+        "build_created_by"=>nil
+      }
+    end
+
+    before do
+      stubbed_request = stub_scanner_request(:get, "/scan_results/#{scan_result_id}", auth_key: scanner_auth_key)
+        .to_return(status: 201, body: JSON.dump(scanner_scan_result_response(job.id, repository.id)))
+    end
+
+    context 'with push access to repository' do
+      before { repository.permissions.create(user: user, push: true) }        
+
+      it 'responds with list of plugins' do
+        get("/v3/scan_result/#{scan_result_id}", {}, headers)
+        expect(last_response.status).to eq(200)
+        expect(parsed_body).to eql_json(expected_json)
+      end
+    end
+
+    context 'without push access to repository' do
+      before { repository.permissions.create(user: user, push: false) }        
+
+      it 'responds with list of plugins' do
+        get("/v3/scan_result/#{scan_result_id}", {}, headers)
+        expect(last_response.status).to eq(403)
+        expect(JSON.load(body).to_s).to include(
+          "@type",
+          "error_type",
+          "insufficient_access",
+          "error_message",
+          "operation requires check_scan_results access to repository",
+          "resource_type",
+          "repository",
+          "permission",
+          "check_scan_results")
+      end
+    end
+  end
+end
