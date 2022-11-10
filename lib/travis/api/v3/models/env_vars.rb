e3ffe60b72ef386f577ca486d05effdc55a5a415
ShahzaibAwan <shahzaibawan@Shahzaibs-MacBook-Pro.local>
2022-11-10 06:34:40 +0500

    [BSFY-181] Add env vars changes to changelog for admin

--- lib/travis/api/v3/models/env_vars.rb
+++ lib/travis/api/v3/models/env_vars.rb
@@ -2,21 +2,24 @@ require_relative './json_sync'
 
 module Travis::API::V3
   class Models::EnvVars < Travis::Settings::Collection
-    include Models::JsonSync, ActiveSupport::Callbacks
+    include Models::JsonSync
+    include ActiveSupport::Callbacks
     extend ActiveSupport::Concern
-    model Models::EnvVar
-    define_callbacks :after_save
 
+    model Models::EnvVar
+
+    define_callbacks :after_save
     set_callback :after_save, :after, :save_audit
 
     attr_accessor :user, :change_source
+
     # See Models::JsonSync
     def to_h
       { 'env_vars' => map(&:to_h).map(&:stringify_keys) }
     end
 
     def create(attributes)
-      @changes = { :"env_vars" => { created: "#{attributes.except("value")}" } }
+      @changes = { env_vars: { created: "#{attributes.except("value")}" } }
       env_var = super(attributes).tap { sync! }
       run_callbacks :after_save
       env_var
@@ -29,7 +32,7 @@ module Travis::API::V3
 
     def destroy(id)
       env_var = find(id)
-      @changes = { :"env_vars" => { deleted: "#{env_var.attributes.delete("value")}" } }
+      @changes = { env_vars: { deleted: "#{env_var.attributes.delete("value")}" } }
       deleted_env_var = super(id).tap { sync! }
       run_callbacks :after_save
       deleted_env_var
@@ -38,7 +41,7 @@ module Travis::API::V3
     def repository
       @repository ||= Models::Repository.find(additional_attributes[:repository_id])
     end
-    
+
     def changes
       @changes
     end
@@ -47,7 +50,14 @@ module Travis::API::V3
 
     def save_audit
       if self.change_source
-        Travis::API::V3::Models::Audit.create!(owner: self.user, change_source: self.change_source, source: self.repository, source_changes: { settings: self.changes })
+        Travis::API::V3::Models::Audit.create!(
+          owner: self.user,
+          change_source: self.change_source,
+          source: self.repository,
+          source_changes: {
+            settings: self.changes
+          }
+        )
         @changes = {}
       end
     end
