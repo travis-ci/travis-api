1ac5018312c8e11185170be12dc5d87c3ff8c276
ShahzaibAwan <shahzaibawan@Shahzaibs-MacBook-Pro.local>
2022-12-16 01:26:51 +0500

    Add reason for manual cancel in build cancellation

--- lib/travis/api/app/endpoint/builds.rb
+++ lib/travis/api/app/endpoint/builds.rb
@@ -42,7 +42,7 @@ class Travis::Api::App
           status 422
           respond_with json
         else
-          payload = { id: params[:id], user_id: current_user.id, source: 'api' }
+          payload = { id: params[:id], user_id: current_user.id, source: 'api', reason: 'Build Cancelled manually by User' }
 
           service.push("build:cancel", payload)
 
