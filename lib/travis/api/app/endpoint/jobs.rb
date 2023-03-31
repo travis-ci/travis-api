364e9ba95ce7d9761678eb0fc1c74278f88644da
Gabriel Rat <rat.gabriel@outlook.com>
2023-03-31 20:33:05 +0300

    Added logs for job cancellation

--- lib/travis/api/app/endpoint/jobs.rb
+++ lib/travis/api/app/endpoint/jobs.rb
@@ -50,7 +50,7 @@ class Travis::Api::App
           status 422
           respond_with json
         else
-          payload = { id: params[:id], user_id: current_user.id, source: 'api' }
+          payload = { id: params[:id], user_id: current_user.id, source: 'api', reason: "Job Cancelled manually by User with id: #{user.login}" }
           service.push("job:cancel", payload)
 
           Metriks.meter("api.v2.request.cancel_job.success").mark
