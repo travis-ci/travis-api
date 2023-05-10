bcbf31aa695fd8996c8ae3d1a23398e41e5adbf3
Vitalie D <vitalie@travis-ci.org>
2023-05-10 13:17:21 +0300

    Remove .org (#1286)

--- lib/travis/api/v3/routes.rb
+++ lib/travis/api/v3/routes.rb
@@ -173,7 +173,6 @@ module Travis::API::V3
 
       post :activate, '/activate'
       post :deactivate, '/deactivate'
-      post :migrate, '/migrate'
       post :star, '/star'
       post :unstar, '/unstar'
       hide(patch :update)
