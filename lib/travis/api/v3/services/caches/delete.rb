feec5500b9a28d1e9621d50f08e37eec50de1555
GbArc <gabriel.arczynski@devtactics.net>
2023-07-07 09:18:12 +0200

    typo

--- lib/travis/api/v3/services/caches/delete.rb
+++ lib/travis/api/v3/services/caches/delete.rb
@@ -4,7 +4,7 @@ module Travis::API::V3
 
     def run!
       repo = check_login_and_find(:repository)
-      access_control.permissions(repository).cache_delete!
+      access_control.permissions(repo).cache_delete!
 
       raise InsufficientAccess unless access_control.user.permission?(:push, repository_id: repo.id)
       return repo_migrated if migrated?(repo)
--- lib/travis/api/v3/services/caches/find.rb
+++ lib/travis/api/v3/services/caches/find.rb
@@ -4,7 +4,7 @@ module Travis::API::V3
 
     def run!
       repo = check_login_and_find(:repository)
-      access_control.permissions(repository).cache_view!
+      access_control.permissions(repo).cache_view!
 
       raise InsufficientAccess unless access_control.user.permission?(:push, repository_id: repo.id)
       result query.find(repo)
