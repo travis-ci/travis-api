575812f289545d478a3268910aeb25d51ac8cda9
AndriiMysko <andrii.mysko@devtactics.net>
2024-02-27 17:10:29 +0200

    Add authorizer repo build permissions patch

--- lib/travis/api/v3/queries/build_permissions.rb
+++ lib/travis/api/v3/queries/build_permissions.rb
@@ -13,6 +13,16 @@ module Travis::API::V3
     end
 
     def update_for_repo(repository, user_ids, permission)
+      roles = ['repository_builds_triggerer', 'repository_builds_restarter']
+      user_ids.each do |user_id|
+        authorizer = Authorizer::new(user_id)
+        if (bool(permission))
+          authorizer.patch_repo_roles(repository.id, roles)
+        else
+          authorizer.delete_repo_roles(repository.id, roles)
+        end
+      end
+
       Models::Permission.where(repository_id: repository.id, user_id: user_ids).update_all(build: bool(permission))
     end
   end
