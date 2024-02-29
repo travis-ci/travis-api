edfd45e9718b6458e1a377feeb1eb19492099a37
Dominik <dominik.alberski@gmail.com>
2024-02-29 08:39:46 +0100

    revert: to previous version with comment

--- lib/patches/active_record/predicate_builder.rb
+++ lib/patches/active_record/predicate_builder.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-EXPECTED_AR_VERSION = '7.0.8'.freeze
+EXPECTED_AR_VERSION = '7.0.6'.freeze # gh gem requires 7.0.8
 ACTUAL_AR_VERSION = "#{ActiveRecord::VERSION::MAJOR}.#{ActiveRecord::VERSION::MINOR}.#{ActiveRecord::VERSION::TINY}"
 
 if EXPECTED_AR_VERSION != ACTUAL_AR_VERSION
