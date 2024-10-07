95c249a8f3139fabee83c212fcff79dec3f5b930
nikolaTrichkovski <nikola.trichkovski@devtactics.net>
2024-10-07 03:57:11 +0200

    add audit log for regenerate token

--- lib/travis/api/v3/queries/access_token.rb
+++ lib/travis/api/v3/queries/access_token.rb
@@ -2,7 +2,18 @@ module Travis::API::V3
   class Queries::AccessToken < Query
     def regenerate_token(user, token, app_id, expires_in: nil)
       Travis.redis.del("t:#{token}")
-      Travis::Api::App::AccessToken.create(user: user, app_id: app_id, expires_in:, force: true).token
+      new_token = Travis::Api::App::AccessToken.create(user: user, app_id: app_id, expires_in:, force: true).token
+      Travis::API::V3::Models::Audit.create!(
+        owner: user,
+        change_source: 'travis-api',
+        source: user,
+        source_changes: {
+          action: 'regenerate_token',
+          old_api_token: token,
+          new_api_token: new_token,
+        }
+      )
+      new_token
     end
 
     def remove_token(user, token, app_id)
