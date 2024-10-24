eff26ac90b877a1d8ed4692257e59f4572775e78
nikolaTrichkovski <nikola.trichkovski@devtactics.net>
2024-10-24 11:47:20 +0200

    [TBT-46] Handle new web token with expiry (#1340)
    * handle new web token with expiry
    
    * handle expired auth token on regenerate fix specs
    
    * add audit log for regenerate token
    
    * set the auth cli token to expire
    
    * fix job/jobs find_specs vm_size
    
    * add user_id to find web_token for expiry reset
    
    * add puts
    
    * set default expiries to 5 mins, more puts
    
    * remove puts
    
    * remove user_id
    
    * return user_id and defaults for api and cli tokens
    
    * add unlimited expiry two years if env set
    
    * rename env variable for web token
    
    * change default expiries for testing
    
    * refactor reset_expiry
    
    * bring back default expiries

--- spec/v3/services/access_token/regenerate_token_spec.rb
+++ spec/v3/services/access_token/regenerate_token_spec.rb
@@ -1,13 +1,17 @@
 describe Travis::API::V3::Services::AccessToken::RegenerateToken, set_app: true do
   let(:user)    { FactoryBot.create(:user) }
   let(:token)   { Travis::Api::App::AccessToken.create(user: user, app_id: 0) }
-  let(:headers) {{ 'HTTP_AUTHORIZATION' => "token #{token}" }}
-  let(:parsed_body) { JSON.load(body) }
+  let(:headers) {{ 'HTTP_AUTHORIZATION' => "token #{token}", 'CONTENT_TYPE' => 'application/json' }}
+  let(:params)  {{ token: token.token }.to_json }
+  let(:parsed_body) { JSON.load(last_response.body) }
 
   describe "regenerating the API access token" do
-    before     { patch('/v3/access_token', {}, headers) }
-    example    { expect(last_response.status).to eq 200 }
-    example    { expect(Travis.redis.exists?("t:#{token}")).to be_falsey }
-    example    { expect(Travis.redis.exists?("t:#{parsed_body['token']}")).to be_truthy }
+    before do
+      patch('/v3/access_token', params, headers)
+    end
+
+    example { expect(last_response.status).to eq 200 }
+    example { expect(Travis.redis.exists?("t:#{token.token}")).to be_falsey }
+    example { expect(Travis.redis.exists?("t:#{parsed_body['token']}")).to be_truthy }
   end
 end
