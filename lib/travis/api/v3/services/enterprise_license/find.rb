2c1c3bbf38656344e62eea1f7e0e3ced83cd98fd
piccadilly circus <baize.aspect0t@icloud.com>
2024-11-25 16:50:37 +0400

    Debug logs added ship:docker

--- lib/travis/api/v3/services/enterprise_license/find.rb
+++ lib/travis/api/v3/services/enterprise_license/find.rb
@@ -1,31 +1,39 @@
 module Travis::API::V3
   class Services::EnterpriseLicense::Find < Service
     def run!
-      if replicated_endpoint
-        # We turn off verification because this is an internal IP and a self signed cert so it will always fail
-        http_options = {url: replicated_endpoint, ssl: Travis.config.ssl.to_h.merge(verify: false)}.compact
-        conn = Faraday.new(http_options) do |conn|
-          conn.use OpenCensus::Trace::Integrations::FaradayMiddleware if Travis::Api::App::Middleware::OpenCensus.enabled?
-          conn.adapter Faraday.default_adapter
-        end
-        response = conn.get("license/v1/license")
-        replicated_response = JSON.parse(response.body)
-        license_id = replicated_response["license_id"]
-        license_type = replicated_response["license_type"]
-        seats = get_seats(replicated_response)
-        expiration_time = replicated_response["expiration_time"]
-        active_users = query.active_users
+      begin
+        Travis.logger.info("Fetching enterprise license")
+        if replicated_endpoint
+          # We turn off verification because this is an internal IP and a self signed cert so it will always fail
+          http_options = {url: replicated_endpoint, ssl: Travis.config.ssl.to_h.merge(verify: false)}.compact
+          conn = Faraday.new(http_options) do |conn|
+            conn.use OpenCensus::Trace::Integrations::FaradayMiddleware if Travis::Api::App::Middleware::OpenCensus.enabled?
+            conn.adapter Faraday.default_adapter
+          end
+          response = conn.get("license/v1/license")
+          replicated_response = JSON.parse(response.body)
+          Travis.logger.info("Fetched enterprise license: #{replicated_response}")
+          license_id = replicated_response["license_id"]
+          license_type = replicated_response["license_type"]
+          seats = get_seats(replicated_response)
+          expiration_time = replicated_response["expiration_time"]
+          active_users = query.active_users
 
-        result({
-          license_id: license_id,
-          license_type: license_type,
-          seats: seats,
-          active_users: active_users.count,
-          expiration_time: expiration_time
-        })
-      else
-        raise InsufficientAccess
+          result({
+                   license_id: license_id,
+                   license_type: license_type,
+                   seats: seats,
+                   active_users: active_users.count,
+                   expiration_time: expiration_time
+                 })
+        else
+          Travis.logger.error("REPLICATED_INTEGRATIONAPI not set")
+          raise InsufficientAccess
+        end
+      rescue => e
+        Travis.logger.error("Error fetching enterprise license: #{e.message}")
       end
+
     end
 
     private
