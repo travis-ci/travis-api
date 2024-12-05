7568f394941af329f82812b0947771269b956609
nikolaTrichkovski <nikola.trichkovski@devtactics.net>
2024-12-05 10:22:06 +0100

    syntax

--- lib/travis/api/app/middleware/error_handler.rb
+++ lib/travis/api/app/middleware/error_handler.rb
@@ -22,12 +22,11 @@ class Travis::Api::App
         Sentry.capture_exception(e)
         raise if Travis.testing
 
-        body = "Sorry, we experienced an error."
+        body = "Sorry, we experienced an error.\n"
         if env['HTTP_X_REQUEST_ID']
           body += "\n"
           body += "request_id:#{env['HTTP_X_REQUEST_ID']}\n"
         end
-
         [500, {'Content-Type' => 'text/plain'}, [body]]
       end
     end
