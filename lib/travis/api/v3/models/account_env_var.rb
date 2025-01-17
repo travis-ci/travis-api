86a392ee0327612b6f9cd705d1b19e23d0c1f97f
tonoyansergey-devt <ssergey.tonoyan@devtactics.net>
2025-01-17 04:07:44 +0400

    TBT-137 Add encryption for env value

--- lib/travis/api/v3/models/account_env_var.rb
+++ lib/travis/api/v3/models/account_env_var.rb
@@ -2,6 +2,8 @@ module Travis::API::V3
   class Models::AccountEnvVar < Model
     belongs_to :owner, polymorphic: true
 
+    serialize :value, Travis::Model::EncryptedColumn.new
+
     def save_account_env_var!(owner_type, owner_id, name, value, public)
       self.owner_type = owner_type
       self.owner_id = owner_id
