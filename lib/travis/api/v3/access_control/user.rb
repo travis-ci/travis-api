c6ee79735ea8980da19e27bc48f579bb0010042c
GbArc <gabriel.arczynski@devtactics.net>
2025-02-06 10:59:31 +0100

    recording user last activity timestamp

--- lib/travis/api/v3/access_control/user.rb
+++ lib/travis/api/v3/access_control/user.rb
@@ -7,6 +7,7 @@ module Travis::API::V3
     def initialize(user)
       user                = Models::User.find(user.id) if user.is_a? ::User
       @user               = user
+      user.touch()
       @access_permissions = user.permissions.where(user_id: user.id)
       super()
     end
