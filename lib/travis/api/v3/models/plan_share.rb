a7d7cafcfb9109eac35ca6709f93eb98f2b250e6
GbArc <gabriel.arczynski@devtactics.net>
2025-04-24 12:28:24 +0200

    added credits_consumed to plan_share

--- lib/travis/api/v3/models/plan_share.rb
+++ lib/travis/api/v3/models/plan_share.rb
@@ -1,6 +1,6 @@
 module Travis::API::V3
   class Models::PlanShare
-    attr_reader :plan_id, :donor, :receiver, :shared_by, :created_at, :admin_revoked
+    attr_reader :plan_id, :donor, :receiver, :shared_by, :created_at, :admin_revoked, :credits_consumed
     def initialize(attributes = {})
       @plan_id = attributes.fetch('plan_id')
       @donor = attributes.fetch('donor')
@@ -8,6 +8,7 @@ module Travis::API::V3
       @shared_by = attributes.fetch('shared_by')
       @created_at = attributes.fetch('created_at')
       @admin_revoked = attributes.fetch('admin_revoked')
+      @credits_consumed = attributes.fetch('credits_consumed')
     end
   end
 end
--- lib/travis/api/v3/renderer/plan_share.rb
+++ lib/travis/api/v3/renderer/plan_share.rb
@@ -1,6 +1,6 @@
 module Travis::API::V3
   class Renderer::PlanShare < ModelRenderer
-    representation(:standard, :plan_id, :donor, :receiver, :shared_by, :created_at, :admin_revoked)
-    representation(:minimal, :plan_id, :donor, :receiver, :shared_by, :created_at, :admin_revoked)
+    representation(:standard, :plan_id, :donor, :receiver, :shared_by, :created_at, :admin_revoked, :credits_consumed)
+    representation(:minimal, :plan_id, :donor, :receiver, :shared_by, :created_at, :admin_revoked, :credits_consumed)
   end
 end
