bce0e10ada09ae677e2fe2ace0caf3681a7a09c1
AndriiMysko <andrii.mysko@devtactics.net>
2025-05-12 11:23:46 +0300

    Address review comments

--- lib/travis/api/v3/models/custom_image_log.rb
+++ lib/travis/api/v3/models/custom_image_log.rb
@@ -2,5 +2,6 @@ module Travis::API::V3
   class Models::CustomImageLog < Model
     after_initialize :readonly!
     belongs_to :custom_image
+    enum action: { created: 'created', used: 'used', deleted: 'deleted', other: 'other' }
   end
 end
