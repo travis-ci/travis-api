227561cc45756a69646a4dccd312602e010014ac
Dominik <dominik.alberski@gmail.com>
2025-05-16 13:02:27 +0200

    refactor: use scope to refactor find method

--- lib/travis/api/v3/queries/organization.rb
+++ lib/travis/api/v3/queries/organization.rb
@@ -5,11 +5,7 @@ module Travis::API::V3
     def find
       return Models::Organization.find_by_id(id) if id
       return Models::Organization.find_by(vcs_id: github_id) || Models::Organization.find_by(github_id: github_id) if github_id
-      return Models::Organization.where(
-        'lower(login) = ? and lower(vcs_type) = ?'.freeze,
-        login.downcase,
-        provider.downcase + 'organization'
-      ).order("id DESC").first if login
+      return Models::Organization.by_login(login, provider).first if login
       raise WrongParams, 'missing organization.id or organization.login'.freeze
     end
 
