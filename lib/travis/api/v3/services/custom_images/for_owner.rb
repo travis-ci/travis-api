72ff6dce0c4c88f8cb6d5cd5a9e16416b8f00bda
AndriiMysko <andrii.mysko@devtactics.net>
2025-05-30 10:04:56 +0300

    Fix permissions for custom images list

--- lib/travis/api/v3/services/custom_images/for_owner.rb
+++ lib/travis/api/v3/services/custom_images/for_owner.rb
@@ -10,10 +10,15 @@ module Travis::API::V3
 
       raise NotFound unless owner
 
-      repo = owner.repositories.first
-      raise InsufficientAccess unless repo
+      if owner.is_a?(Travis::API::V3::Models::User)
+        raise InsufficientAccess unless access_control.user.id == owner.id
+      else
+        membership = Models::Membership.where(organization_id: owner.id).joins(:user).includes(:user).first
+        raise NotFound unless membership
 
-      access_control.permissions(repo).build_create!
+        build_permission = membership.build_permission.nil? ? true : membership.build_permission
+        raise InsufficientAccess unless build_permission
+      end
 
       results = query(:custom_images).for_owner(owner)
       result results
