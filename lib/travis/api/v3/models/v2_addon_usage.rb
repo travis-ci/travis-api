0c0df00adf704fb1d2397a3575317cd03b4397cd
AndriiMysko <andrii.mysko@devtactics.net>
2025-06-25 15:33:29 +0300

    Fix quantity_limit_free

--- lib/travis/api/v3/models/v2_addon_usage.rb
+++ lib/travis/api/v3/models/v2_addon_usage.rb
@@ -1,7 +1,7 @@
 module Travis::API::V3
   class Models::V2AddonUsage
     attr_reader :id, :addon_id, :addon_quantity, :addon_usage, :remaining, :purchase_date, :valid_to, :active, :status,
-                :quantity_free_limit, :quantity_limit_type, :quantity_limit_charge, :total_usage
+                :quantity_limit_free, :quantity_limit_type, :quantity_limit_charge, :total_usage
 
     def initialize(attrs)
       @id = attrs.fetch('id')
@@ -13,7 +13,7 @@ module Travis::API::V3
       @valid_to = attrs.fetch('valid_to')
       @active = attrs.fetch('active')
       @status = attrs.fetch('status')
-      @quantity_free_limit = attrs.fetch('quantity_free_limit', 0)
+      @quantity_limit_free = attrs.fetch('quantity_limit_free', 0)
       @quantity_limit_type = attrs.fetch('quantity_limit_type', nil)
       @quantity_limit_charge = attrs.fetch('quantity_limit_charge', nil)
       @total_usage = attrs.fetch('total_usage', nil)
--- lib/travis/api/v3/renderer/v2_addon_usage.rb
+++ lib/travis/api/v3/renderer/v2_addon_usage.rb
@@ -1,8 +1,8 @@
 module Travis::API::V3
   class Renderer::V2AddonUsage < ModelRenderer
     representation(:standard, :id, :addon_id, :addon_quantity, :addon_usage, :remaining, :purchase_date, :valid_to,
-                   :active, :status, :total_usage, :quantity_free_limit, :quantity_limit_type, :quantity_limit_charge)
+                   :active, :status, :total_usage, :quantity_limit_free, :quantity_limit_type, :quantity_limit_charge)
     representation(:minimal, :id, :addon_id, :addon_quantity, :addon_usage, :remaining, :purchase_date, :valid_to,
-                   :active, :status, :total_usage, :quantity_free_limit, :quantity_limit_type, :quantity_limit_charge)
+                   :active, :status, :total_usage, :quantity_limit_free, :quantity_limit_type, :quantity_limit_charge)
   end
 end
